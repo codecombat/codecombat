6ae8464e6d874ea63ba12a5a10d290c7d827620d
Glen De Cauwsemaecker <decauwsemaecker.glen@gmail.com>
2014-03-23 02:33:03 +0100

    Made the get_var system easier and cleaner to use

--- scripts/windows/coco-dev-setup/batch/setup.bat
+++ scripts/windows/coco-dev-setup/batch/setup.bat
@@ -8,11 +8,8 @@ call print_header
 call print_dashed_seperator
 
 call get_config version
-set "version=%temp_var%"
 call get_config author
-set "author=%temp_var%"
 call get_config copyright
-set "copyright=%temp_var%"
 echo Welcome to the automated Installation of the CodeCombat Dev. Environment!
 echo v%version% authored by %author% and published by %copyright%.
 
