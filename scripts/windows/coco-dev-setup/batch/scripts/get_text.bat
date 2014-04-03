6f2cfbf1e8d3975024e6a7691e508dc446db9f80
Glen De Cauwsemaecker <decauwsemaecker.glen@gmail.com>
2014-04-04 00:37:59 +0200

    config system has been refactored and now works with powershell

--- scripts/windows/coco-dev-setup/batch/scripts/get_text.bat
+++ scripts/windows/coco-dev-setup/batch/scripts/get_text.bat
@@ -1,3 +1,3 @@
-call run_script .\get_var.ps1 ..\\localisation\\%1.coco %3 %4 %5 %6 %7 > var.tmp
-set /p %2= < var.tmp
-del /q var.tmp
+for /F "delims=" %%F in ('call run_script .\\get_var.ps1 ..\\localisation\\%1.coco %3 %4 %5 %6') do (
+	set "%2=%%F"
+)
--- scripts/windows/coco-dev-setup/batch/scripts/get_var.ps1
+++ scripts/windows/coco-dev-setup/batch/scripts/get_var.ps1
@@ -1,17 +1,27 @@
 $xml_file = [xml](get-content $args[0])
-if($args.count -eq 3)
+if($args.count -eq 2)
 {
-    cmd /c "set_var.bat" ($args[1]) ($xml_file.variables.($args[2]))
+    $var_output = ($xml_file.variables.($args[1]))
+}
+elseif($args.count -eq 3)
+{
+    $var_output = ($xml_file.variables.($args[1]).($args[2]))
 }
 elseif($args.count -eq 4)
 {
-    cmd /c "set_var.bat" ($args[1]) ($xml_file.variables.($args[2]).($args[3]))
+    $var_output = ($xml_file.variables.($args[1]).($args[2]).($args[3]))
 }
 elseif($args.count -eq 5)
 {
-    cmd /c "set_var.bat" ($args[1]) ($xml_file.variables.($args[2]).($args[3]).($args[4]))
+    $var_output = ($xml_file.variables.($args[1]).($args[2]).($args[3]).($args[4]))
 }
 elseif($args.count -eq 6)
 {
-    cmd /c "set_var.bat" ($args[1]) ($xml_file.variables.($args[2]).($args[3]).($args[4]).($args[5]))
+    $var_output = ($xml_file.variables.($args[1]).($args[2]).($args[3]).($args[4]).($args[5]))
 }
+elseif($args.count -eq 7)
+{
+    $var_output = ($xml_file.variables.($args[1]).($args[2]).($args[3]).($args[4]).($args[5]).($args[6]))
+}
+
+Write-Host "$var_output"
