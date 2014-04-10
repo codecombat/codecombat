5ff4a3379879bc7bae26d7a6dcf0bfa94f22e1c2
Glen De Cauwsemaecker <decauwsemaecker.glen@gmail.com>
2014-04-11 00:00:23 +0200

    Seperated intro-github from npm/brunch and removed zh local option

---
+++ scripts/windows/coco-dev-setup/batch/configuration.bat
@@ -0,0 +1,2 @@
+cd scripts
+configuration.bat
--- scripts/windows/coco-dev-setup/batch/scripts/setup_p2.bat
+++ scripts/windows/coco-dev-setup/batch/scripts/setup_p2.bat
@@ -7,4 +7,16 @@ call github_setup
 
 start cmd /c "setup_p3.bat"
 
+call write_cache.bat
+
+call get_local_text switch_install switch install
+call get_local_text switch_close switch close
+call get_local_text switch_open switch open
+
+echo %switch_install%
+echo %switch_close%
+echo.
+
+set /p "dummy=%switch_open%"
+
 endlocal
