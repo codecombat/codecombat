b95d7d4cb44df45e58440d76471f0b8ffdd2cc00
Glen De Cauwsemaecker <decauwsemaecker.glen@gmail.com>
2014-04-12 20:18:55 +0200

    Fixed grammar error in files

--- scripts/windows/coco-dev-setup/batch/scripts/get_language.bat
+++ scripts/windows/coco-dev-setup/batch/scripts/get_language.bat
@@ -3,28 +3,28 @@ echo but most feedback is sent and localised by us.
 echo Here is a list of languages:
 call print_dashed_seperator
 
-call get_array ..\\localisation\\languages.coco languages language_count
+call get_array ..\\localization\\languages.coco languages language_count
 for /l %%i in (1,1,%language_count%) do (
 	call get_text !languages[%%i]! global_description global description
 	echo   [%%i] !global_description!
 )
 
-goto:get_localisation_id
+goto:get_localization_id
 
-:get_localisation_id
+:get_localization_id
 	call print_dashed_seperator
-	set /p "localisation_id=Enter the language ID of your preference and press <ENTER>: "
+	set /p "localization_id=Enter the language ID of your preference and press <ENTER>: "
 	goto:validation_check
 
 :validation_check
-	set "localisation_is_false="
-	set /a local_id = %localisation_id%
-	if !local_id! EQU 0 set localisation_is_false=1
-	if !local_id! LSS 1 set localisation_is_false=1
-	if !local_id! GTR !language_count! set localisation_is_false=1
-	if defined localisation_is_false (
+	set "localization_is_false="
+	set /a local_id = %localization_id%
+	if !local_id! EQU 0 set localization_is_false=1
+	if !local_id! LSS 1 set localization_is_false=1
+	if !local_id! GTR !language_count! set localization_is_false=1
+	if defined localization_is_false (
 		echo The id you entered is invalid, please try again...
-		goto:get_localisation_id
+		goto:get_localization_id
 	) else (
 		set language_id=!languages[%local_id%]!
 		call print_dashed_seperator
--- scripts/windows/coco-dev-setup/batch/scripts/get_text.bat
+++ scripts/windows/coco-dev-setup/batch/scripts/get_text.bat
@@ -1,3 +1,3 @@
-for /F "delims=" %%F in ('call run_script .\\get_var.ps1 ..\\localisation\\%1.coco %3 %4 %5 %6') do (
+for /F "delims=" %%F in ('call run_script .\\get_var.ps1 ..\\localization\\%1.coco %3 %4 %5 %6') do (
 	set "%2=%%F"
 )
