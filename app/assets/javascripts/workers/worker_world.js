0b2d5c251dcb8dce7cce33e33c9129bd5ad133b8
Nick Winter <livelily@gmail.com>
2014-06-04 15:47:09 -0600

    A bit more Worker hardening.

--- app/assets/javascripts/workers/worker_world.js
+++ app/assets/javascripts/workers/worker_world.js
@@ -60,11 +60,14 @@ self.console = console;
 
 self.importScripts('/javascripts/world.js', '/javascripts/lodash.js', '/javascripts/aether.js');
 
-// We could do way more from this: http://stackoverflow.com/questions/10653809/making-webworkers-a-safe-environment
-Object.defineProperty(self, "XMLHttpRequest", {
-  get: function() { throw new Error("Access to XMLHttpRequest is forbidden."); },
-  configurable: false
-});
+var restricted = ["XMLHttpRequest", "importScripts", "Worker"];
+for(var i = 0; i < restricted.length; ++i) {
+  // We could do way more from this: http://stackoverflow.com/questions/10653809/making-webworkers-a-safe-environment
+  Object.defineProperty(self, restricted[i], {
+    get: function() { throw new Error("Access to that global property is forbidden."); },
+    configurable: false
+  });
+}
 
 self.transferableSupported = function transferableSupported() {
   if (typeof self._transferableSupported !== 'undefined') return self._transferableSupported;
