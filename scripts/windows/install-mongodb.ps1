b4a39c9b04a1df060fcddf59b7c9ed5f4987ef2c
Omar S. <osanseviero@gmail.com>
2014-12-29 14:58:26 -0500

    Update install-mongodb.ps1
    I changed the link, but the link is just for 64 bit systems...I don't know if it will work with 32 bit systems

--- scripts/windows/install-mongodb.ps1
+++ scripts/windows/install-mongodb.ps1
@@ -2,9 +2,9 @@ Set-ExecutionPolicy RemoteSigned
  
 $mongoDbPath = "C:\MongoDB" 
 $mongoDbConfigPath = "$mongoDbPath\mongod.cfg"
-$url = "http://downloads.mongodb.org/win32/mongodb-win32-x86_64-2008plus-2.4.9.zip" 
+$url = "http://downloads.mongodb.org/win32/mongodb-win32-x86_64-2008plus-2.8.0-rc4.zip" 
 $zipFile = "$mongoDbPath\mongo.zip" 
-$unzippedFolderContent ="$mongoDbPath\mongodb-win32-x86_64-2008plus-2.4.9"
+$unzippedFolderContent ="$mongoDbPath\mongodb-win32-x86_64-2008plus-2.8.0-rc4.zip"
  
 if ((Test-Path -path $mongoDbPath) -eq $True) 
 { 
