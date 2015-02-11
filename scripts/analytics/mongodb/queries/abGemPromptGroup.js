6178800d164419534f13563318f54bc6467a9dae
Matt Lott <mattlott@live.com>
2015-02-11 11:07:32 -0800

    Add browser segments to a/b test scripts

--- scripts/analytics/mongodb/queries/abGemPromptGroup.js
+++ scripts/analytics/mongodb/queries/abGemPromptGroup.js
@@ -4,13 +4,6 @@
 // Usage:
 // mongo <address>:<port>/<database> <script file> -u <username> -p <password>
 
-// Inputs to modify below:
-// numDays - number of days into the past to fetch
-// eventFunnel - ordered array of events that define the completion funnel
-// levelSlugs - [optional] array of levels to examine, otherwise fetch all levels
-// testGroupFn - return group value from user testGroupNumber
-
-// Include getFunnelData(), log()
 load('abTestHelpers.js');
 
 var scriptStartTime = new Date();
@@ -29,43 +22,14 @@ try {
 
   var funnelData = getFunnelData(startDay, eventFunnel, testGroupFn);
 
-  log("Day\t\tGroup\t\tStarted\tFinished\tCompletion Rate");
-  var overallCounts = {};
-  for (var i = 0; i < funnelData.length; i++) {
-    var level = funnelData[i].level;
-    var day = funnelData[i].day;
-    var group = funnelData[i].group;
-    var started = funnelData[i].started;
-    var finished = funnelData[i].finished;
-    var rate = started > 0 ? finished / started * 100 : 0.0;
-
-    if (!overallCounts[level]) overallCounts[level] = {};
-    if (!overallCounts[level][group]) overallCounts[level][group] = {started: 0, finished: 0};
-    overallCounts[level][group]['started'] += started;
-    overallCounts[level][group]['finished'] += finished;
-
-    if (group === 'prompt') {
-      log(day + "\t" + group + "\t\t" + started + "\t" + finished + "\t" + rate.toFixed(2));
+  printFunnelData(funnelData, function (day, level, browser, group, started, finished, rate) {
+    if (day && level && browser && group) {
+      log(day + "\t" + group + "\t" + (group === 'prompt' ? "\t": "") + started + "\t" + finished + "\t" + rate.toFixed(2));
     }
-    else {
-      log(day + "\t" + group + "\t" + started + "\t" + finished + "\t" + rate.toFixed(2));
+    else if (group) {
+      log(group + (group === 'prompt' ? "\t": "") + "\t" + started + "\t" + finished + "\t" + rate.toFixed(2));
     }
-  }
-
-  log("Overall totals:");
-  for (level in overallCounts) {
-    for (group in overallCounts[level]) {
-      var started = overallCounts[level][group].started;
-      var finished = overallCounts[level][group].finished;
-      var rate = started > 0 ? finished / started * 100 : 0.0;
-      if (group === 'prompt') {
-        log(group + "\t\t" + started + "\t" + finished + "\t" + rate.toFixed(2));
-      }
-      else {
-        log(group + "\t" + started + "\t" + finished + "\t" + rate.toFixed(2));
-      }
-    }
-  }
+  });
 }
 catch(err) {
   log("ERROR: " + err);
