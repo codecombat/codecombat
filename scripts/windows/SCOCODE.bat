13e2d97c46b675f91eb2735a1bffac1b4674ea93
nixel2007 <nixel2007@gmail.com>
2015-03-22 20:12:31 +0300

    Update scocode.bat sample

--- scripts/windows/SCOCODE.bat
+++ scripts/windows/SCOCODE.bat
@@ -1,4 +1,6 @@
 set "mongo_db_location=MONGO_DB_PATH_HERE"
+call npm update
+call bower update
 start cmd.exe cmd /c call nodemon -w server -w server_config.js
 start cmd.exe cmd /c call brunch w^
   & mongod --setParameter textSearchEnabled=true^
