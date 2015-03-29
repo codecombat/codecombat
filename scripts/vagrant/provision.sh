8bf7a29d9e12066bbd02a72e30fe95b9d2bfed43
Alan Hohn <alanhohn@gmail.com>
2015-03-28 22:24:18 -0400

    vagrant node_modules workaround

--- scripts/vagrant/provision.sh
+++ scripts/vagrant/provision.sh
@@ -5,12 +5,16 @@ sudo apt-get -y install python-software-properties git
 sudo add-apt-repository -y ppa:chris-lea/node.js
 sudo apt-get -y update
 sudo apt-get -y install nodejs
-sudo apt-get -y install g++ make coffeescript
+sudo apt-get -y install g++ make 
+sudo mkdir /node_modules
+sudo chown vagrant:vagrant /node_modules
+sudo mount -o bind /node_modules /vagrant/node_modules
 cd /vagrant
 sudo npm install
 sudo npm install -g bower
 sudo npm install -g brunch
 sudo npm install -g geoip-lite
+sudo npm install -g coffee-script
 bower install --allow-root
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 7F0CEB10
 echo 'deb http://downloads-distro.mongodb.org/repo/ubuntu-upstart dist 10gen' | sudo tee /etc/apt/sources.list.d/mongodb.list
