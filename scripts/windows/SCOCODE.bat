05c765f0b0ed6b8b8e72ecd1d4880c68276c2024
Kevin Avignon <kevin.o.avignon@gmail.com>
2015-06-03 14:41:33 -0400

    Deleted unnecessary changes from translatation.

--- scripts/windows/SCOCODE.bat
+++
@@ -1,7 +0,0 @@
-set "mongo_db_location=C:\Users\Kevin\Documents\codecombat\db"
-call npm update
-call bower update
-start cmd.exe cmd /c call nodemon -w server -w server_config.js
-start cmd.exe cmd /c call brunch w^
-  & mongod --setParameter textSearchEnabled=true^
-    --dbpath %mongo_db_location%
