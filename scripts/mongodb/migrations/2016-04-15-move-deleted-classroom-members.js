50e8000faf57f5e496c8bfeb8997012d419c6717
Scott Erickson <sderickson@gmail.com>
2016-04-29 13:12:22 -0700

    Update move-deleted-classroom-members script

--- scripts/mongodb/migrations/2016-04-15-move-deleted-classroom-members.js
+++ scripts/mongodb/migrations/2016-04-15-move-deleted-classroom-members.js
@@ -1,7 +1,11 @@
+load('bower_components/lodash/dist/lodash.js');
 var classrooms = db.classrooms.find();
 classrooms.forEach(function (classroom) {
     print('classroom', classroom._id);
     classroom.members.forEach(function (userID) {
+        if (_.isString(userID)) {
+            userID = ObjectId(userID);
+        }
         var user = db.users.findOne({ _id: userID }, { deleted: true });
         if (user.deleted) {
             print('\tFOUND ONE', userID);
