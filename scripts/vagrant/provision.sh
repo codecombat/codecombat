19a70c4135a0219cc130700d20a63cf54dbe82e1
duybkict <duytran.bkict@gmail.com>
2016-05-04 11:38:33 +0700

    install nvm, upgrade auto-reload-brunch

--- scripts/vagrant/provision.sh
+++ scripts/vagrant/provision.sh
@@ -2,8 +2,7 @@
 # Original content copyright (c) 2014 dpen2000 licensed under the MIT license
 
 # some defaults
-DISTRO="trusty"
-NODE_VERSION="5.x" # 0.10 | 0.12 | 4.x | 5.x
+NODE_VERSION="5.11.0" # 0.10 | 0.12 | 4.x | 5.x | 6.x
 
 # inform apt that there's no user to answer interactive questions
 export DEBIAN_FRONTEND=noninteractive
@@ -17,9 +16,7 @@ sudo mv /tmp/limits.conf /etc/security/limits.conf
 sudo chown root:root /etc/security/limits.conf
 
 # install prerequisites
-curl -s https://deb.nodesource.com/gpgkey/nodesource.gpg.key | sudo apt-key add -
-echo "deb https://deb.nodesource.com/node_${NODE_VERSION} ${DISTRO} main
-deb-src https://deb.nodesource.com/node_${NODE_VERSION} ${DISTRO} main" | sudo tee /etc/apt/sources.list.d/nodesource.list
+# curl -sL https://deb.nodesource.com/setup_${NODE_VERSION} | sudo -E bash -
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 7F0CEB10
 echo 'deb http://downloads-distro.mongodb.org/repo/ubuntu-upstart dist 10gen' | sudo tee /etc/apt/sources.list.d/mongodb.list
 
@@ -27,17 +24,22 @@ echo "updating apt sources..."
 sudo apt-get update
 
 echo "installing prerequisites..."
-sudo apt-get -y install --no-install-recommends build-essential git g++ make curl wget python2.7 mongodb-org dos2unix
+sudo apt-get -y install --no-install-recommends build-essential git g++ make curl wget python2.7 dos2unix
+
+# install nvm
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.31.0/install.sh | bash
+source ~/.nvm/nvm.sh
 
 # install node.js
 echo "installing node.js..."
-sudo apt-get -y install nodejs
+nvm install ${NODE_VERSION}
+n=$(which node);n=${n%/bin/node}; chmod -R 755 $n/bin/*; sudo cp -r $n/{bin,lib,share} /usr/local
+
+# sudo apt-get -y install nodejs
 echo "upgrading npm..."
 sudo npm install -g npm@latest # upgrade npm
 sudo npm install -g geoip-lite
-sudo npm install -g nodemon
 sudo npm install -g bower
-sudo npm install -g brunch
 
 # bind /vagrant/node_modules so that it does not leak through to the host file system
 # which triggers symlink and path size issues on Windows hosts
@@ -47,13 +49,14 @@ sudo chown -R vagrant:vagrant /node_modules
 sudo mount --bind /node_modules /vagrant/node_modules
 
 # prepare
-find /vagrant -type f -exec dos2unix {} \;
+find /vagrant/app -type f -exec dos2unix {} \;
+find /vagrant/vendor -type f -exec dos2unix {} \;
 sudo chown -R vagrant:vagrant /home/vagrant
 
 # install npm modules
 echo "installing modules..."
 cd /vagrant
-npm install --no-bin-links
+npm install
 bower install
 
 # install mongo
