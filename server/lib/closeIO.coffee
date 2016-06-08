codecombat/codecombat

commit a6bb706cf295e2680fe77ff13e91d3da59487b53

Update licenses needed form email contacts to include NL

server/lib/closeIO.coffee
@@ -65,7 +65,7 @@ module.exports =
           activities = JSON.parse(body)
           return done("Unexpected activities format: " + body) unless activities.data?
           for activity in activities.data when activity._type is 'Email'
-            if /@codecombat\.com/ig.test(activity.sender) and not activity.sender?.indexOf(config.mail.username) >= 0
+            if /@codecombat\.(?:com)|(?:nl)$/ig.test(activity.sender) and not activity.sender?.indexOf(config.mail.username) >= 0
               return done(null, activity.sender, lead.id)
           return done(null, config.mail.supportSchools, lead.id)
     catch error

commit 6019beac25e5929f5ca7e58e0fd66e8e29a476cd

:bug:Fix license request email parsing
Caused by
https://github.com/codecombat/codecombat/commit/a6bb706cf295e2680fe77ff1
3e91d3da59487b53

server/lib/closeIO.coffee
@@ -65,7 +65,7 @@ module.exports =
           activities = JSON.parse(body)
           return done("Unexpected activities format: " + body) unless activities.data?
           for activity in activities.data when activity._type is 'Email'
-            if /@codecombat\.(?:com)|(?:nl)$/ig.test(activity.sender) and not activity.sender?.indexOf(config.mail.username) >= 0
+            if /@codecombat\.(?:com)|(?:nl)/ig.test(activity.sender) and not activity.sender?.indexOf(config.mail.username) >= 0
               return done(null, activity.sender, lead.id)
           return done(null, config.mail.supportSchools, lead.id)
     catch error