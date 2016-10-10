140405e97ce48b61c672abeb8137924ed58358ba
Nick Winter <livelily@gmail.com>
2016-10-10 16:34:16 -0700

    Update subscribersByCountry.js

--- scripts/analytics/mongodb/queries/subscribersByCountry.js
+++ scripts/analytics/mongodb/queries/subscribersByCountry.js
@@ -3,20 +3,32 @@
 // Usage:
 // mongo <address>:<port>/<database> <script file> -u <username> -p <password>
 
-var countries = ['brazil', 'china'];
+function displayMoney(amount) {
+    return '$' + (amount / 100).toFixed(2);
+};
+
+var countries = ['brazil', 'china', 'israel'];
 countries.forEach(function(country) {
-  print('---' + country.toUpperCase() + '---');
-  var cursor = db.users.find({country: country, stripe: {$exists: true}}, {email: 1, country: 1, stripe: 1, name: 1});
-  var users = [];
-  var inactiveUsers = [];
-  while (cursor.hasNext()) {
-    var user = cursor.next();
-    if (!user.stripe.subscriptionID) {
-      inactiveUsers.push(user);
-      continue;
+    print('---' + country.toUpperCase() + '---');
+    var cursor = db.users.find({country: country, stripe: {$exists: true}}, {email: 1, country: 1, stripe: 1, name: 1, dateCreated: 1});
+    var users = [];
+    var total = 0;
+    var activeCount = 0;
+    while (cursor.hasNext()) {
+        var user = cursor.next();
+	var payments = db.payments.find({recipient: user._id});
+        var amount = 0;
+	payments.forEach(function(payment) {
+	    amount += payment.amount;
+	});
+        total += amount;
+	if (!amount) {
+            continue;
+	}
+        var active = !!user.stripe.subscriptionID;
+        if (active) ++activeCount;
+	users.push(user);
+	print([displayMoney(amount), active ? 'Active' : '', user.dateCreated, user._id, user.country, user.email, user.name, JSON.stringify(user.stripe, null, 0)].join('\t'));
     }
-    users.push(user);
-    print([user._id, user.country, user.email, user.name, JSON.stringify(user.stripe, null, 0)].join('\t'));
-  }
-  print('Had', users.length, 'active subscribers and', inactiveUsers.length, 'possible former subscribers in', country, '\n');
+    print('Got', displayMoney(total), 'over', activeCount, 'active subscribers and', users.length - activeCount, 'former subscribers in', country, '\n');
 });
