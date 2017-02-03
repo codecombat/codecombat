4e6f6037a9d20c9741a7179803a0062ad1cf26fa
Matt Lott <mattlott@live.com>
2017-02-03 09:53:23 -0800

    Add mid-event drop offs to teacher form -> create class script

--- scripts/analytics/teacherFormToClassConversion.js
+++ scripts/analytics/teacherFormToClassConversion.js
@@ -2,10 +2,13 @@
 // Find conversion rate between viewing the teacher sign up form and creating a class
 
 // Rough algorithm
-// 1. Find all users that saw teacher form between 7 and 30 days ago
-// 2. Find subset of those users that also created a class in last 30 days
+// 1. Find all users that saw teacher form between 7 and 14 days ago
+// 2. Find subset of those users that also created a class in last 14 days
 
 // NOTE: update the endDay variable below to control the date range
+// NOTE: assumes events happened in correct order, and can't be skipped somehow
+
+// TODO: users that saw teacher form earlier in date range have more time to create a class
 
 if (process.argv.length !== 3) {
   console.log("Usage: node <script> <mongo connection Url analytics>");
@@ -18,17 +21,19 @@ const co = require('co');
 const MongoClient = require('mongodb').MongoClient;
 const mongoose = require('mongoose');
 const mongoConnUrlAnalytics = process.argv[2];
+
 const debugOutput = true;
 const daysToViewForm = 7;
-const daysToCreateClass = 4;
-const endDay = "2017-01-13";
+const daysToCreateClass = 7;
+const endDay = "2017-01-06";
+
 let startDay = new Date(`${endDay}T00:00:00.000Z`);
 startDay.setUTCDate(startDay.getUTCDate() - daysToViewForm - daysToCreateClass);
 startDay = startDay.toISOString().substring(0, 10);
 debug(`Measuring days ${startDay} to ${endDay}`);
 
-const teacherFormEvents = ['Teachers Request Demo Loaded', 'Teachers Create Account Loaded', 'Teachers Convert Account Loaded'];
-const classCreatedEvent = 'Teachers Classes Create New Class Finished';
+const startEvents = ['Teachers Request Demo Loaded', 'Teachers Create Account Loaded', 'Teachers Convert Account Loaded'];
+const remainingOrderedEvents = ['Teachers Classes Loaded', 'Teachers Classes Create New Class Started', 'Teachers Classes Create New Class Finished'];
 
 co(function*() {
   const analyticsDb = yield MongoClient.connect(mongoConnUrlAnalytics, {connectTimeoutMS: 1000 * 60 * 60, socketTimeoutMS: 1000 * 60 * 60});
@@ -36,10 +41,10 @@ co(function*() {
   const startObjectId = objectIdWithTimestamp(new Date(`${startDay}T00:00:00.000Z`));
   const formEndDate = new Date(`${endDay}T00:00:00.000Z`);
   formEndDate.setUTCDate(formEndDate.getUTCDate() - daysToCreateClass);
-  let endObjectId = objectIdWithTimestamp(formEndDate);
-  debug(`Finding view teacher form events between ${startDay} and ${formEndDate.toISOString().substring(0, 10)}..`);
+  const midObjectId = objectIdWithTimestamp(formEndDate);
+  debug(`Finding view teacher form events between ${startDay} and ${formEndDate.toISOString().substring(0, 10)}...`);
 
-  let query = {$and: [{_id: {$gte: startObjectId}}, {_id: {$lt: endObjectId}}, {event: {$in: teacherFormEvents}}]};
+  let query = {$and: [{_id: {$gte: startObjectId}}, {_id: {$lt: midObjectId}}, {event: {$in: startEvents}}]};
   const formEvents = yield analyticsDb.collection('log').find(query, {user: 1}).toArray();
   debug(`Teacher form events found ${formEvents.length}`);
   const userFormMap = {};
@@ -49,25 +54,29 @@ co(function*() {
   const userIds = Object.keys(userFormMap);
   debug(`Unique users ${userIds.length}`);
 
-  debug(`Finding created class events between ${startDay} and ${endDay}..`);
-  endObjectId = objectIdWithTimestamp(new Date(`${endDay}T00:00:00.000Z`));
-  query = {$and: [{_id: {$gte: startObjectId}}, {_id: {$lt: endObjectId}}, {event: classCreatedEvent}, {user: {$in: userIds}}]};
-  const classEvents = yield analyticsDb.collection('log').find(query, {user: 1}).toArray();
+  debug(`Finding rest of create class funnel events between ${startDay} and ${endDay}...`);
+  const endObjectId = objectIdWithTimestamp(new Date(`${endDay}T00:00:00.000Z`));
+  query = {$and: [{_id: {$gte: startObjectId}}, {_id: {$lt: endObjectId}}, {event: {$in: remainingOrderedEvents}}, {user: {$in: userIds}}]};
+  const classEvents = yield analyticsDb.collection('log').find(query, {event: 1, user: 1}).toArray();
   debug(`Class created events found ${classEvents.length}`);
-  const userClassMap = {};
+  const userFunnelEventsMap = {};
   for (const event of classEvents) {
-    userClassMap[event.user] = true;
+    if (!userFunnelEventsMap[event.event]) userFunnelEventsMap[event.event] = {};
+    userFunnelEventsMap[event.event][event.user] = true;
   }
 
   const usersSawTeacherForm = userIds.length;
   let teachersCreatedClass = 0;
-  for (const userId in userFormMap) {
-    if (userClassMap[userId]) {
-      teachersCreatedClass++;
+  console.log(`Looking at events from ${startDay} to ${endDay}:`);
+  console.log(`100% ${usersSawTeacherForm} users saw teacher form`);
+  for (const event of remainingOrderedEvents) {
+    const currentEventCount = Object.keys(userFunnelEventsMap[event] || {}).length;
+    console.log(`${(currentEventCount * 100.0 / usersSawTeacherForm).toFixed(2)}% ${currentEventCount} users saw event ${event}`);
+    if (event === 'Teachers Classes Create New Class Finished') {
+      teachersCreatedClass = currentEventCount;
     }
   }
-  console.log(`Looking at data from ${startDay} to ${endDay}:`);
-  console.log(`Saw teacher signup form= ${usersSawTeacherForm} Created class= ${teachersCreatedClass} Conversion= ${(teachersCreatedClass / usersSawTeacherForm).toFixed(4)}`);
+  console.log(`Saw teacher signup form= ${usersSawTeacherForm} Created class= ${teachersCreatedClass} Conversion= ${teachersCreatedClass / usersSawTeacherForm} ${teachersCreatedClass * 100.0 / usersSawTeacherForm}%`);
 
   analyticsDb.close();
   debug(`Script runtime: ${new Date() - scriptStartTime}`);
