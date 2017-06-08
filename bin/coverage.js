edb7b8d5a1827df6de678185f8f2c6cbb0f139ba
Oge Nnadi <oge@nnadi.org>
2017-06-08 18:42:12 +0100

    Refactored out manual fs traversal in coverage.js
    See issue #4335

--- bin/coverage.js
+++ bin/coverage.js
@@ -1,38 +1,21 @@
 fs = require('fs')
+_ = require('lodash')
 child_process = require('child_process')
 
 // Have to convert all CS server files to JS for coverage to work.
 // Walk through folders in server/ and compile each .coffee file,
 // keeping a list of generated files so they can be deleted later.
 
-var directories = ['./server']
 var convertedFiles = [];
 console.log('Convert server coffeescript files.')
 
-while(directories.length) {
-  directory = directories.pop()
-  console.log('*', directory)
+try{
+  var find_output = child_process.execSync('coffee --compile ./server && find ./server -name "*.js"');
+  var splits      = find_output.toString().split("\n")
+  convertedFiles  = _.compact(splits);
 
-  fs.readdirSync(directory).forEach((fileOrDir) => {
-    absPath = directory + '/' + fileOrDir
-    stat = fs.statSync(absPath)
-
-    // .coffee => .js
-    if(stat.isFile() && fileOrDir.endsWith('.coffee')) {
-      child_process.execSync(`coffee -c ${absPath}`)
-      convertedFiles.push(absPath.replace('.coffee', '.js'))
-    }
-
-    // Add to list of directories to walk
-    if(stat.isDirectory()) {
-      directories.push(absPath);
-    }
-  })
-}
-
-// Run Istanbul
-console.log(`Converted ${convertedFiles.length} server coffeescript files. Running tests...`)
-try {
+  // Run Istanbul
+  console.log(`Converted ${convertedFiles.length} server coffeescript files. Running tests...`)
   child_process.execSync('istanbul cover ./node_modules/jasmine/bin/jasmine.js')
 }
 catch (e) {
