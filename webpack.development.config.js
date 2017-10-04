aa2b1956082c7fd308b446ce0df1a20802c20261
Phoenix Eliot <phoenixjeliot@gmail.com>
2017-10-04 14:09:39 -0700

    Always show progress bar in development build

--- webpack.development.config.js
+++ webpack.development.config.js
@@ -15,6 +15,7 @@ module.exports = (env) => {
       contentBase: './public'
     },
     plugins: baseConfig.plugins.concat([
+      new webpack.ProgressPlugin({ profile: false }),
       new webpack.BannerPlugin({ // Label each module in the output bundle
         banner: "hash:[hash], chunkhash:[chunkhash], name:[name], filebase:[filebase], query:[query], file:[file]"
       }),
