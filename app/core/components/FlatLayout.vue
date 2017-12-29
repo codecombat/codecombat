abf088bcde197b93859e09eb0f0968e09af054c9
Scott Erickson <sderickson@gmail.com>
2017-12-28 16:37:00 -0800

    Move chunking to Router, have Router handle Vue components
    * Move dynamicRequire content to bottom of Router, organize by chunk
    * Set up Router to handle loading, wrapping Vue components
    * Have Router directly load
      * CLAsComponent
      * AdminClassroomLevelsComponent
      * FilesComponent
      * OutcomesReportComponent
    * Refactor FlatLayout, PageErrors to .vue files
    * Add .gitignore for stats.json

---
+++ app/core/components/FlatLayout.vue
@@ -0,0 +1,22 @@
+<template lang="jade">
+
+div
+  page-errors(v-if="errors.length")
+  slot(v-else)
+
+</template>
+
+<script lang="coffee">
+
+PageErrors = require('core/components/PageErrors.vue').default
+
+module.exports = {
+  name: 'flat-layout'
+  components:
+    'page-errors': PageErrors
+  computed: Vuex.mapState({
+    errors: (state) -> state.pageErrors
+  })
+}
+
+</script>
