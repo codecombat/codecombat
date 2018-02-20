f0f359046a1b607563a1b577a4a96a6c51184c35
Nick Winter <livelily@gmail.com>
2018-02-20 11:00:44 -0800

    Post good problem levels to Slack

--- scripts/analytics/checkProblemLevels.js
+++ scripts/analytics/checkProblemLevels.js
@@ -9,8 +9,14 @@ if (process.argv.length !== 4) {
   process.exit();
 }
 
+require('coffee-script');
+require('coffee-script/register');
 const scriptStartTime = new Date();
 const co = require('co');
+const _ = require('lodash');
+_.str = require('underscore.string')
+_.mixin(_.str.exports())
+const slack = require('../../server/slack');
 const MongoClient = require('mongodb').MongoClient;
 const mongoose = require('mongoose');
 const mongoConnUrl = process.argv[2];
@@ -18,37 +24,37 @@ const mongoConnUrlLevelSessions = process.argv[3];
 
 const debugOutput = true;
 const daysToCheck = 7;  // Warn about completion problems arising in the past N days; should be a multiple of 7 to avoid weekend effects
-const daysBeforeRepeating = 7;  // Don't warn about this level if we would have warned in the past week
-const maxProblemsPerDay = 5;
+//const daysBeforeRepeating = 7;  // Don't warn about this level if we would have warned in the past week
+const daysBeforeRepeating = 0;  // First run
+const maxProblemLevelsPerDay = 5;
 
 const endDay = new Date(new Date().toISOString().substring(0, 10) + 'T00:00:00.000Z');
 const endDayStr = endDay.toISOString().substring(0, 10)
 let startDay = new Date(endDay);
-startDay.setUTCDate(startDay.getUTCDate() - 2 * daysToCheck - daysBeforeRepeating);
+startDay.setUTCDate(startDay.getUTCDate() - 2 * daysToCheck - daysBeforeRepeating - 1);
 const startDayStr = startDay.toISOString().substring(0, 10)
 debug(`Measuring days ${startDayStr} to ${endDayStr}; looking at last ${daysToCheck} days compared to past.`);
 
 // TODO: cron this up
 
+let prodDb, levelSessionsDb;
+
 co(function*() {
-  const prodDb = yield MongoClient.connect(mongoConnUrl, {connectTimeoutMS: 1000 * 60 * 60, socketTimeoutMS: 1000 * 60 * 60});
-  const levelSessionsDb = yield MongoClient.connect(mongoConnUrlLevelSessions, {connectTimeoutMS: 1000 * 60 * 60, socketTimeoutMS: 1000 * 60 * 60});
+  prodDb = yield MongoClient.connect(mongoConnUrl, {connectTimeoutMS: 1000 * 60 * 60, socketTimeoutMS: 1000 * 60 * 60});
+  levelSessionsDb = yield MongoClient.connect(mongoConnUrlLevelSessions, {connectTimeoutMS: 1000 * 60 * 60, socketTimeoutMS: 1000 * 60 * 60});
   debug(`Connected to databases`);
 
   const startObjectId = objectIdWithTimestamp(startDay);
   const endObjectId = objectIdWithTimestamp(endDay);
   debug(`Finding LevelSessions created and changed between ${startDayStr} and ${endDayStr}...`);
 
-  // TODO: find ways to make more performant if they take too long
-  //let query = {$and: [{_id: {$gte: startObjectId}}, {_id: {$lt: endObjectId}}]};
-  let query = {$and: [{_id: {$gte: startObjectId}}, {_id: {$lt: endObjectId}}, {levelID: {$in: ['cell-commentary', 'kithgard-gates', 'the-raised-sword', 'robot-ragnarok', 'signal-corpse']}}]};
-  //let query = {$and: [{_id: {$gte: startObjectId}}, {_id: {$lt: endObjectId}}, {levelID: 'signal-corpse'}]};
+  const query = {$and: [{_id: {$gte: startObjectId}}, {_id: {$lt: endObjectId}}]};
   const sessions = yield levelSessionsDb.collection('level.sessions').find(query, {creator: 1, 'stats.gamesCompleted': 1, playtime: 1, 'state.complete': 1, isForClassroom: 1, levelID: 1, heroConfig: 1, team: 1, codeLanguage: 1, created: 1}).toArray();
   debug(`Sessions found: ${sessions.length}`);
 
   const levelSessionMap = {};
   const userSessionMap = {};
-  for (let session of sessions) {
+  for (const session of sessions) {
     if (!session.levelID) continue;
     if (!levelSessionMap[session.levelID]) levelSessionMap[session.levelID] = [];
     levelSessionMap[session.levelID].push(session);
@@ -63,22 +69,22 @@ co(function*() {
   const users = yield prodDb.collection('users').find({_id: {$in: userObjectIDs}}, {country: 1, role: 1, permissions: 1, stripe: 1, paypal: 1, anonymous: 1, preferredLanguage: 1, clientCreator: 1, createdOnHost: 1}).toArray();
   debug(`... fetched ${users.length} users`);
 
-  const levels = yield prodDb.collection('levels').find({slug: {$in: levelIDs}}, {slug: 1, type: 1, kind: 1, practice: 1, replayable: 1, assessment: 1, adventurer: 1, adminOnly: 1, created: 1, requiresSubscription: 1}).toArray();
-  let levelsBySlug = {};
-  for (let level of levels) {
+  const levels = yield prodDb.collection('levels').find({slug: {$in: levelIDs}}, {slug: 1, type: 1, kind: 1, practice: 1, replayable: 1, assessment: 1, shareable: 1, adventurer: 1, adminOnly: 1, created: 1, requiresSubscription: 1}).toArray();
+  const levelsBySlug = {};
+  for (const level of levels) {
     if (level.adminOnly || level.replayable) continue;
     levelsBySlug[level.slug] = level;
   }
   debug(`... fetched ${levels.length} levels, kept ${Object.keys(levelsBySlug).length}`);
 
-  let statsByLevel = {};
-  let datesByIndex = [];
-  let currentDay = startDay;
-  let allProblemsByDay = [];
-  let topProblemsByDay = [];
+  const statsByLevel = {};
+  const datesByIndex = [];
+  const currentDay = startDay;
+  const allProblemsByDay = [];
+  const topProblemsByDay = [];
   while (currentDay < endDay) {
     for (const levelID of levelIDs) {
-      let level = levelsBySlug[levelID];
+      const level = levelsBySlug[levelID];
       if (!level) continue;
       if (!statsByLevel[levelID]) statsByLevel[levelID] = {home: {javascript: [], python: []}, classroom: {javascript: [], python: []}};
       statsByLevel[levelID].home.javascript.push(statsPlaceholder(currentDay));
@@ -98,18 +104,18 @@ co(function*() {
     if (user.country && user.country != 'united-states') continue;  // TODO: upgrade to look at how different languages/countries are performing?
     if (user.preferredLanguage && user.preferredLanguage != 'en-US') continue;
     if (user.clientCreator) continue;
-    let userSessions = userSessionMap[user._id + ''];
-    userSessions.sort((a, b) => b.created - a.created);
-    for (let session of userSessions) {
+    const userSessions = userSessionMap[user._id + ''];
+    userSessions.sort((a, b) => a.created - b.created);
+    for (const session of userSessions) {
       if (!session.playtime) continue;
       if (session.codeLanguage != 'python' && session.codeLanguage != 'javascript') continue;
-      let level = levelsBySlug[session.levelID];
+      const level = levelsBySlug[session.levelID];
       if (!level) continue;
-      let product = session.isForClassroom ? 'classroom' : 'home';
+      const product = session.isForClassroom ? 'classroom' : 'home';
       let sessionStartIndex = 0;
       for (sessionStartIndex = datesByIndex.length - 1; datesByIndex[sessionStartIndex] > session.created; --sessionStartIndex) {}
       // TODO: ignore weekends, since those players are different from our main players?
-      let stats = statsByLevel[session.levelID][product][session.codeLanguage][sessionStartIndex];
+      const stats = statsByLevel[session.levelID][product][session.codeLanguage][sessionStartIndex];
       ++stats.started;
       if (session.state && session.state.complete)
 	++stats.completed;
@@ -118,22 +124,21 @@ co(function*() {
     }
   }
 
-  for (let levelID in statsByLevel) {
-    for (let product in statsByLevel[levelID]) {
-      for (let codeLanguage in statsByLevel[levelID][product]) {
-        let statDays = statsByLevel[levelID][product][codeLanguage];
+  for (const levelID in statsByLevel) {
+    for (const product in statsByLevel[levelID]) {
+      for (const codeLanguage in statsByLevel[levelID][product]) {
+        const statDays = statsByLevel[levelID][product][codeLanguage];
 	for (let startDateIndex = 0; startDateIndex < datesByIndex.length - daysToCheck; ++startDateIndex) {
-          let aggregateDateIndex = startDateIndex + daysToCheck;
-          let aggregateStats = statsPlaceholder(datesByIndex[aggregateDateIndex]);
-          let dateIndex;
-	  for (dateIndex = startDateIndex; dateIndex < aggregateDateIndex; ++dateIndex) {
-            let stats = statDays[dateIndex];
+          const aggregateDateIndex = startDateIndex + daysToCheck;
+          const aggregateStats = statsPlaceholder(datesByIndex[aggregateDateIndex]);
+	  for (let dateIndex = startDateIndex; dateIndex < aggregateDateIndex; ++dateIndex) {
+            const stats = statDays[dateIndex];
             aggregateStats.started += stats.started;
             aggregateStats.completed += stats.completed;
             aggregateStats.playtime += stats.playtime;
             aggregateStats.leftGame += stats.leftGame;
 	  }
-          let stats = statDays[aggregateDateIndex];
+          const stats = statDays[aggregateDateIndex];
           stats.aggregateStarted = aggregateStats.started;
           stats.aggregateCompleted = aggregateStats.completed;
           stats.aggregatePlaytime = aggregateStats.playtime;
@@ -155,66 +160,170 @@ co(function*() {
         // ------+                ... and if so, skip the warning.
 
 	for (let dateIndex = datesByIndex.length - daysToCheck; dateIndex < datesByIndex.length; ++dateIndex) {
-          let previousStats = statDays[dateIndex - daysToCheck];
-	  let currentStats = statDays[dateIndex];
-          let problems = calculateProblems(levelID, product, codeLanguage, previousStats, currentStats);
+          const previousStats = statDays[dateIndex - daysToCheck];
+	  const currentStats = statDays[dateIndex];
+          const level = levelsBySlug[levelID];
+          const problems = calculateProblems(level, product, codeLanguage, previousStats, currentStats);
           allProblemsByDay[dateIndex] = allProblemsByDay[dateIndex].concat(problems);
 	}
       }
     }
   }
-  //console.log(JSON.stringify(statsByLevel, null, 2));
+  //debug(JSON.stringify(statsByLevel, null, 2));
 
   for (let dateIndex = datesByIndex.length - daysToCheck; dateIndex < datesByIndex.length; ++dateIndex) {
-    let previouslyWarnedTopProblemsByDay = topProblemsByDay.slice(Math.max(0, dateIndex - daysBeforeRepeating), dateIndex);
+    const previouslyWarnedTopProblemsByDay = topProblemsByDay.slice(Math.max(0, dateIndex - daysBeforeRepeating), dateIndex);
     topProblemsByDay[dateIndex] = filterTopProblems(allProblemsByDay[dateIndex], previouslyWarnedTopProblemsByDay);
   }
-  console.log(JSON.stringify(topProblemsByDay, null, 2));
+  //debug(JSON.stringify(topProblemsByDay, null, 2));
   logProblems(topProblemsByDay[datesByIndex.length - 1]);
 
   prodDb.close();
   levelSessionsDb.close();
-  debug(`Script runtime: ${new Date() - scriptStartTime}`);
-})
+  debug(`Script runtime: ${Math.round((new Date() - scriptStartTime) / 1000)}s`);
+}).then(function (value) {
+  
+}, function (err) {
+  console.error(err.stack);
+  prodDb.close();
+  levelSessionsDb.close();
+  process.exit()
+});
 
-function calculateProblems(levelID, product, codeLanguage, old, now) {
-  let problems = [];
-  if (now.aggregateStarted < 20 || old.aggregateStarted < 20) return problems;
-  let nowCompletion = now.aggregateCompleted / now.aggregateStarted;
-  let oldCompletion = old.aggregateCompleted / old.aggregateStarted;
-  if ((1 - oldCompletion) / (1 - nowCompletion) < 0.8) {
-    problems.push({levelID, product, codeLanguage, old, now, type: 'completion rate drop', oldCompletion, nowCompletion, severity: (oldCompletion - nowCompletion) * now.aggregateStarted});
+// Detect absolute problems with poorly performing levels
+const levelThresholdsByType = {
+  'hero': {completion: 0.9, playtime: 300, leftGame: 0.1},
+  'course': {completion: 0.9, playtime: 300, leftGame: 0.1},
+  'hero-ladder': {completion: 0.85, playtime: 450, leftGame: 0.1},
+  'course-ladder': {completion: 0.8, playtime: 600, leftGame: 0.1},
+  'game-dev': {completion: 0.9, playtime: 300, leftGame: 0.1},
+  'web-dev': {completion: 0.9, playtime: 300, leftGame: 0.1},
+}
+
+function calculateProblems(level, product, codeLanguage, old, now) {
+  const problems = [];
+  if (now.aggregateStarted < 30 || old.aggregateStarted < 30) return problems;
+
+  // Detect relative problems compared to last time window
+  const nowCompletion = now.aggregateCompleted / now.aggregateStarted;
+  const oldCompletion = old.aggregateCompleted / old.aggregateStarted;
+  const nowFailureRate = 1 - nowCompletion;
+  const oldFailureRate = 1 - oldCompletion;
+  let severity, message;
+  if (nowFailureRate > oldFailureRate * 1.25) {
+    severity = (nowFailureRate - oldFailureRate) * Math.log(now.aggregateStarted);
+    message = `${(nowCompletion * 100).toFixed(1)}% completion (was ${(oldCompletion * 100).toFixed(1)}%)`;
+    problems.push({level, product, codeLanguage, old, now, severity, message, type: 'completion rate drop'});
   }
-  // TODO: be able to detect more kinds of problems, of two main kinds: absolute problems, and new problems compared to past time period
+
+  const nowPlaytime = now.aggregatePlaytime / now.aggregateStarted;
+  const oldPlaytime = old.aggregatePlaytime / old.aggregateStarted;
+  if (nowPlaytime > oldPlaytime * 1.25) {
+    severity = (nowPlaytime / oldPlaytime) * Math.log(now.aggregateStarted);
+    message = `${nowPlaytime.toFixed(0)}s completion time (was ${oldPlaytime.toFixed(0)}s)`;
+    problems.push({level, product, codeLanguage, old, now, severity, message, type: 'completion time increase'});
+  }
+
+  const nowLeftGame = now.aggregateLeftGame / now.aggregateStarted;
+  const oldLeftGame = old.aggregateLeftGame / old.aggregateStarted;
+  if (nowLeftGame > oldLeftGame * 1.25) {
+    severity = (nowLeftGame / oldLeftGame) * Math.log(now.aggregateStarted);
+    message = `${(nowLeftGame * 100).toFixed(1)}% left game (was ${(oldLeftGame * 100).toFixed(1)}%)`;
+    problems.push({level, product, codeLanguage, old, now, severity, message, type: 'left game rate increase'});
+  }
+
+  // Detect absolute poor performance problems  
+  const thresholds = levelThresholdsByType[level.type] || levelThresholdsByType.hero;
+  let completionThreshold = thresholds.completion;
+  let playtimeThreshold = thresholds.playtime;
+  let leftGameThreshold = thresholds.leftGame;
+  if (level.practice) {
+    completionThreshold *= 0.8;
+    playtimeThreshold *= 1.5;
+    leftGameThreshold *= 1.5;
+  }
+  if (level.kind == 'mastery') {
+    completionThreshold *= 0.9;
+    playtimeThreshold *= 1.25;
+    leftGameThreshold *= 1.25;
+  }
+  if (level.kind == 'advanced' || level.kind == 'challenge') {
+    completionThreshold *= 0.7;
+    playtimeThreshold *= 2;
+    leftGameThreshold *= 2;
+  }
+  if (level.assessment) {
+    completionThreshold *= 0.8;
+    playtimeThreshold *= 1.5;
+    leftGameThreshold *= 1.5;
+  }
+  if (level.shareable == 'project') {
+    completionThreshold *= 0.8;
+    playtimeThreshold *= 3;
+    leftGameThreshold *= 1.5;
+  }
+
+  if (nowCompletion < completionThreshold) {
+    severity = (nowFailureRate / (1 - completionThreshold)) * Math.log(now.aggregateStarted);
+    message = `${(nowCompletion * 100).toFixed(1)}% completion (threshold: ${(completionThreshold * 100).toFixed(1)}%)`;
+    problems.push({level, product, codeLanguage, old, now, severity, message, type: 'low completion rate'});
+  }
+
+  if (nowPlaytime > playtimeThreshold) {
+    severity = (nowPlaytime / playtimeThreshold) * Math.log(now.aggregateStarted);
+    message = `${nowPlaytime.toFixed(0)}s completion time (threshold ${playtimeThreshold.toFixed(0)}s)`;
+    problems.push({level, product, codeLanguage, old, now, severity, message, type: 'high playtime'});
+  }
+
+  if (nowLeftGame > leftGameThreshold) {
+    severity = (nowLeftGame / leftGameThreshold) * Math.log(now.aggregateStarted);
+    message = `${(nowLeftGame * 100).toFixed(1)}% left game (threshold ${(leftGameThreshold * 100).toFixed(1)}%)`;
+    problems.push({level, product, codeLanguage, old, now, severity, message, type: 'high left game rate'});
+  }
+  
+  for (const problem of problems) {
+    if (product == 'classroom')
+      problem.severity *= 2;
+  }
+
   return problems;
 }
 
 function filterTopProblems(problems, previouslyWarnedTopProblemsByDay) {
-  let previouslyWarnedTopProblemsByLevel = {};
+  // TODO: are we getting multiple days' problems in here sometimes?
+  const previouslyWarnedTopProblemsByLevel = {};
   for (const problemsByDay of previouslyWarnedTopProblemsByDay) {
     for (const problem of problemsByDay) {
-      previouslyWarnedTopProblemsByLevel[problem.levelID] = (previouslyWarnedTopProblemsByLevel[problem.levelID] || []).concat(problem);
+      previouslyWarnedTopProblemsByLevel[problem.level.slug] = (previouslyWarnedTopProblemsByLevel[problem.level.slug] || []).concat(problem);
     }
   }
   let topProblems = [];
-  problems.sort((a, b) => a.severity - b.severity);
-  for (let problem of problems) {
-    if (previouslyWarnedTopProblemsByLevel[problem.levelID]) continue;
-    if (topProblems.filter(p => p.levelID == problem.levelID).length) continue;  // Already did this level
-    let thisLevelProblems = problems.filter(p => p.levelID == problem.levelID);
+  let problemLevelsCount = 0;
+  problems.sort((a, b) => b.severity - a.severity);
+  for (const problem of problems) {
+    if (previouslyWarnedTopProblemsByLevel[problem.level.slug]) continue;
+    if (topProblems.filter(p => p.level.slug == problem.level.slug).length) continue;  // Already did this level
+    const thisLevelProblems = problems.filter(p => p.level.slug == problem.level.slug);
     topProblems = topProblems.concat(thisLevelProblems);
-    if (topProblems.length == maxProblemsPerDay) break;
+    if (++problemLevelsCount == maxProblemLevelsPerDay) break;
   }
   return topProblems;
 }
 
 function logProblems(problems) {
-  debug('New problems for today:');
-  for (let problem of problems) {
-    console.log('\t\t' + JSON.stringify(problem));
+  const logLines = []
+  logLines.push(`New problems for today (${new Date().toISOString().substring(0, 10)}):`);
+  const problemsByLevel = _.groupBy(problems, (problem) => problem.level.slug);
+  for (const levelID in problemsByLevel) {
+    const problems = problemsByLevel[levelID];
+    problems.sort((a, b) => b.severity - a.severity);
+    logLines.push(`\t${levelID}, with ${problems[0].now.aggregateStarted} players in the past ${daysToCheck} days (${problems[0].old.aggregateStarted} in previous window):`);
+    for (const problem of problems) {
+      logLines.push(`\t  ${_.str.lpad(problem.severity.toFixed(1), 8)}  ${_.str.rpad(problem.product, 9)}  ${_.str.rpad(problem.codeLanguage, 10)}  ${_.str.rpad(problem.type, 20)}  ${problem.message}`);
+    }
   }
-  // TODO: format these nicely
-  // TODO: post to Slack
+  debug(logLines.join('\n'));
+  slack.sendSlackMessage(`\`\`\`${logLines.join('\n')}\n\`\`\``, ['#game'], {markdown: true})
 }
 
 // * Helper functions
@@ -237,8 +346,8 @@ function objectIdWithTimestamp(timestamp) {
   // Convert string date to Date object (otherwise assume timestamp is a date)
   if (typeof(timestamp) == 'string') timestamp = new Date(timestamp);
   // Convert date object to hex seconds since Unix epoch
-  let hexSeconds = Math.floor(timestamp/1000).toString(16);
+  const hexSeconds = Math.floor(timestamp/1000).toString(16);
   // Create an ObjectId with that hex timestamp
-  let constructedObjectId = mongoose.Types.ObjectId(hexSeconds + "0000000000000000");
+  const constructedObjectId = mongoose.Types.ObjectId(hexSeconds + "0000000000000000");
   return constructedObjectId;
 }
