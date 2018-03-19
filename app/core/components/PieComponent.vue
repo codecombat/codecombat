427115074436014b51c59177c6938ee39f1cb227
Scott Erickson <sderickson@gmail.com>
2018-03-19 09:22:21 -0700

    Fix classrooms in IE11
    Convert PieComponent from ES6 to ES5
    
    This is separate from commit 79b23d81db27e246be60f11654ee768cd3cb15d9
    on master because there are changes on this branch to the PieComponent.
    This commit will override that commit.

--- app/core/components/PieComponent.vue
+++ app/core/components/PieComponent.vue
@@ -37,6 +37,8 @@
 </template>
 
 <script>
+  'use strict';
+
   module.exports = Vue.extend({
     name: 'pie-chart',
     props: {
@@ -48,7 +50,9 @@
       strokeWidth: {
         type: Number,
         default: 1,
-        validator: (v) => v > 0 && v <= 10
+        validator: function validator(v) {
+          return v > 0 && v <= 10;
+        }
       },
       color: {
         type: String,
@@ -60,45 +64,45 @@
       }
     },
     computed: {
-      width: function () {
-        return 2 + this.strokeWidth / 5
+      width: function width() {
+        return 2 + this.strokeWidth / 5;
       },
-      viewBox: function () {
-        let c = 1 + this.strokeWidth / 10
-        let w = this.width
-        return `${-c} ${-c} ${w} ${w}`
+      viewBox: function viewBox() {
+        var c = 1 + this.strokeWidth / 10;
+        var w = this.width;
+        return -c + ' ' + -c + ' ' + w + ' ' + w;
       },
-      circle: function () {
-        return this._ratio % 1 === 0
+      circle: function circle() {
+        return this._ratio % 1 === 0;
       },
-      _ratio: function () {
-        let r = this.percent / 100
-        return isFinite(this.ratio) ? this.ratio : r
+      _ratio: function _ratio() {
+        var r = this.percent / 100;
+        return isFinite(this.ratio) ? this.ratio : r;
       },
-      arc: function () {
-        let r = this._ratio  // short hand
-        let deg = 2 * Math.PI * r
-        let x = 1 * Math.sin(deg)
-        let y = -1 * Math.cos(deg)
-        let negative = r < 0
-        let large = 0
+      arc: function arc() {
+        var r = this._ratio; // short hand
+        var deg = 2 * Math.PI * r;
+        var x = 1 * Math.sin(deg);
+        var y = -1 * Math.cos(deg);
+        var negative = r < 0;
+        var large = 0;
         // 0 ccw, 1 clock-wise
-        let rotation = negative ? 0 : 1
+        var rotation = negative ? 0 : 1;
         if (negative && x > 0) {
-          large = 1
+          large = 1;
         } else if (!negative && x < 0) {
-          large = 1
+          large = 1;
         }
-        return `A 1 1 0 ${large} ${rotation} ${x} ${y}`
+        return 'A 1 1 0 ' + large + ' ' + rotation + ' ' + x + ' ' + y;
       },
-      path: function () {
-        return `M 0 -1 ${this.arc}`
+      path: function path() {
+        return 'M 0 -1 ' + this.arc;
       }
-    },
-  })
+    }
+  });
 
-  function ratioOK (val) {
-    return
+  function ratioOK(val) {
+    return;
   }
 </script>
 
