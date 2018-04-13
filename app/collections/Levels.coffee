codecombat/codecombat

commit 3542ee580a68abf2df496c4d7be95a5e4d4a1f3a

Add support for multiple solutions in classroom UI (#4637)
Add tabbed solutions in teacher student view and course solutions.
Update solutions to be localized.
Fix a few ace editor syntax highlighting problems for wd2.
Add fingerprinting to student view.

app/collections/Levels.coffee
@@ -1,5 +1,6 @@
 CocoCollection = require 'collections/CocoCollection'
 Level = require 'models/Level'
+utils = require 'core/utils'
 
 module.exports = class LevelCollection extends CocoCollection
   url: '/db/level'
@@ -16,3 +17,19 @@ module.exports = class LevelCollection extends CocoCollection
   fetchForCampaign: (campaignSlug, options={}) ->
     options.url = "/db/campaign/#{campaignSlug}/levels"
     @fetch(options)
+
+  getSolutionsMap: (language) ->
+    @models.reduce((map, level) =>
+      targetLang = level.get('primerLanguage') or language
+      solutions = level.getSolutions().filter((s) => s.language is targetLang)
+      map[level.get('original')] = solutions?.map((s) => @fingerprint(s.source, s.language))
+      map
+    , {})
+
+  fingerprint: (code, language) ->
+    # Add a zero-width-space at the end of every comment line
+    switch language
+      when 'javascript' then code.replace /^(\/\/.*)/gm, "$1​"
+      when 'lua' then code.replace /^(--.*)/gm, "$1​"
+      when 'html' then code.replace /^(<!--.*)-->/gm, "$1​-->"
+      else code.replace /^(#.*)/gm, "$1​"

app/templates/teachers/teacher-student-view.jade
@@ -208,7 +208,9 @@ block content
                   - courseStarted = true;
                   - var levelNumber = view.classroom.getLevelNumber(level.get('original'), index + 1);
                   - var langClass = 'lang-python';
-                  if view.classroom.get('aceConfig')
+                  if level.get('primerLanguage')
+                    - langClass = 'lang-' + level.get('primerLanguage');
+                  else if view.classroom.get('aceConfig')
                     - langClass = 'lang-' + view.classroom.get('aceConfig').language;
                   tr.code-level-row
                     td(colspan=2)
@@ -241,7 +243,8 @@ block content
                           span(data-i18n="teacher.level_in_progress")
                           span )
                       if view.levelStudentCodeMap[level.get('original')]
-                        - wroteCode = true;
+                        if (view.levelSolutionsMap[level.get('original')] || []).length > 1
+                          .student-solution-spacer
                         if view.levelProgressMap[level.get('original')] === 'complete'
                           pre
                             code(class=langClass)= view.levelStudentCodeMap[level.get('original')]
@@ -280,9 +283,30 @@ block content
                           span )
                           =" "
                           span.glyphicon.glyphicon-question-sign(data-html='true', data-title=view.questionMarkHtml("teacher.solution_project_blurb"))
-                      if view.levelSolutionMap[level.get('original')]
-                        pre
-                          code(class=langClass)= view.levelSolutionMap[level.get('original')]
+                      - var solutions = view.levelSolutionsMap[level.get('original')] || [];
+                      if solutions.length === 1
+                        each solution in solutions
+                          pre
+                            code(class=langClass)= solution
+                      else if solutions.length > 0
+                        div(role='tabpanel')
+                          ul.nav.nav-tabs(role='tablist')
+                            each solution, index in solutions
+                              li.nav-item(class=(index === 0 ? 'active' : ''))
+                                - var target = level.get('original') + index;
+                                a.nav-link(id="#{target}-tab", class=(index === 0 ? 'active' : ''), href="##{target}", role="tab", data-toggle="tab", aria-controls=target, aria-selected=(index === 0))
+                                  //- TODO: use solution names from level data
+                                  .text-center Variation #{index + 1}
+                              if index < solutions.length - 1
+                                .tab-spacer
+                              else
+                                .tab-filler
+                          .tab-content
+                            each solution, index in solutions
+                              - var target = level.get('original') + index;
+                              div.tab-pane(id=target, class=(index === 0 ? 'active' : ''), role=tabpanel, aria-labelledby="#{target}-tab")
+                                pre
+                                  code(class=langClass)= solution
                       else
                         i.small(data-i18n="common.coming_soon")
               if !courseStarted

app/models/Level.coffee
@@ -3,6 +3,7 @@ LevelComponent = require './LevelComponent'
 LevelSystem = require './LevelSystem'
 LevelConstants = require 'lib/LevelConstants'
 ThangTypeConstants = require 'lib/ThangTypeConstants'
+utils = require 'core/utils'
 
 # Pure functions for use in Vue
 # First argument is always a raw Level.attributes
@@ -282,8 +283,7 @@ module.exports = class Level extends CocoModel
     solutions = _.cloneDeep plan.solutions ? []
     for solution in solutions
       try
-        # TODO: use preferredlanguage to localize source
-        solution.source = _.template(solution.source)(plan.context)
+        solution.source = _.template(solution?.source)(utils.i18n(plan, 'context'))
       catch e
         console.error "Problem with template and solution comments for '#{@get('slug') or @get('name')}'\n", e
     solutions

app/styles/teachers/teacher-course-solution-view.sass
@@ -49,7 +49,6 @@
       display: none
 
   .ace_content
-    background-color: #f5f5f5
     .ace_marker-layer
       .ace_bracket
         display: none
@@ -58,3 +57,35 @@
         display: none
     .ace_text-layer .ace_invisible_space
       visibility: hidden
+
+  ul.nav-tabs
+    border: none
+    .tab-spacer
+      float: left
+      width: 15px
+      height: 21px
+      border-bottom: thin solid #979797
+
+    .tab-filler
+      // Triggers block formatting context
+      // http://stackoverflow.com/questions/1260122/expand-a-div-to-take-the-remaining-width
+      overflow: hidden
+      height: 21px
+      border-bottom: thin solid #979797
+
+    li > a
+      margin: 0
+      padding: 0px 4px
+      border: thin solid #979797
+      border-bottom: none
+      background: white
+      border-radius: 5px 5px 0 0
+      color: black
+      font-size: 14px
+    li
+      border-bottom: thin solid #979797
+    li.active
+      border-bottom: none
+
+  .student-solution-spacer
+    height: 22px

app/styles/teachers/teacher-student-view.sass
@@ -207,3 +207,45 @@
 
   .glyphicon-question-sign
     margin-left: 5px
+
+  .ace_content
+    .ace_marker-layer
+      .ace_bracket
+        display: none
+    .ace_cursor-layer
+      .ace_cursor
+        display: none
+    .ace_text-layer .ace_invisible_space
+      visibility: hidden
+
+  ul.nav-tabs
+    border: none
+    .tab-spacer
+      float: left
+      width: 15px
+      height: 21px
+      border-bottom: thin solid #979797
+
+    .tab-filler
+      // Triggers block formatting context
+      // http://stackoverflow.com/questions/1260122/expand-a-div-to-take-the-remaining-width
+      overflow: hidden
+      height: 21px
+      border-bottom: thin solid #979797
+
+    li > a
+      margin: 0
+      padding: 0px 4px
+      border: thin solid #979797
+      border-bottom: none
+      background: white
+      border-radius: 5px 5px 0 0
+      color: black
+      font-size: 14px
+    li
+      border-bottom: thin solid #979797
+    li.active
+      border-bottom: none
+
+  .student-solution-spacer
+    height: 22px

app/templates/teachers/teacher-course-solution-view.jade
@@ -39,6 +39,7 @@ block content
       each level, index in view.levels.models
         if level.get('assessment') && !me.hasAssessments()
           - continue
+        - var langClass = 'lang-' + (level.get('primerLanguage') || view.language || 'python');
         h2.page-break-before(id=level.get('slug')) ##{view.levelNumberMap[level.get('original')]}. #{i18n(level.attributes, 'name')}
         h3(data-i18n="teacher.level_overview_solutions")
         i #{i18n(level.attributes, 'description')}
@@ -52,7 +53,7 @@ block content
           h4.text-center(data-i18n="common.default_code")
           if level.get('begin')
             pre
-              code= level.get('begin')
+              code(class=langClass)= level.get('begin')
           else
             .text-center
               i(data-i18n="common.coming_soon")
@@ -67,11 +68,34 @@ block content
           h4.text-center
             span= level.get('name')
             span.spl(data-i18n="common.solution")
-          if level.get('solution')
-            if level.get('assessment') === 'open-ended'
-              em(data-i18n="teacher.explain_open_ended")
-            pre
-              code= level.get('solution')
+          if level.get('assessment') === 'open-ended'
+            em(data-i18n="teacher.explain_open_ended")
+
+          //- TODO: this UI is identical to teacher-student-view multi-solution tabbing
+          - var solutions = view.levelSolutionsMap[level.get('original')] || [];
+          if solutions.length === 1
+            each solution in solutions
+              pre
+                code(class=langClass)= solution
+          else if solutions.length > 0
+            div(role='tabpanel')
+              ul.nav.nav-tabs(role='tablist')
+                each solution, index in solutions
+                  li.nav-item(class=(index === 0 ? 'active' : ''))
+                    - var target = level.get('original') + index;
+                    a.nav-link(id="#{target}-tab", class=(index === 0 ? 'active' : ''), href="##{target}", role="tab", data-toggle="tab", aria-controls=target, aria-selected=(index === 0))
+                      //- TODO: use solution names from level data
+                      .text-center Variation #{index + 1}
+                  if index < solutions.length - 1
+                    .tab-spacer
+                  else
+                    .tab-filler
+              .tab-content
+                each solution, index in solutions
+                  - var target = level.get('original') + index;
+                  div.tab-pane(id=target, class=(index === 0 ? 'active' : ''), role=tabpanel, aria-labelledby="#{target}-tab")
+                    pre
+                      code(class=langClass)= solution
           else
             .text-center
               i(data-i18n="common.coming_soon")

app/views/teachers/TeacherCourseSolutionView.coffee
@@ -1,9 +1,9 @@
 require('app/styles/teachers/teacher-course-solution-view.sass')
 utils = require 'core/utils'
 RootView = require 'views/core/RootView'
-CocoCollection = require 'collections/CocoCollection'
 Course = require 'models/Course'
 Level = require 'models/Level'
+Levels = require 'collections/Levels'
 utils = require 'core/utils'
 ace = require('lib/aceContainer')
 aceUtils = require 'core/aceUtils'
@@ -19,7 +19,7 @@ module.exports = class TeacherCourseSolutionView extends RootView
       @prettyLanguage = @camelCaseLanguage(@language)
       @course = new Course(_id: @courseID)
       @supermodel.trackRequest(@course.fetch())
-      @levels = new CocoCollection([], { url: "/db/course/#{@courseID}/level-solutions", model: Level})
+      @levels = new Levels([], { url: "/db/course/#{@courseID}/level-solutions"})
       @supermodel.loadCollection(@levels, 'levels', {cache: false})
       @levelNumberMap = {}
     super(options)
@@ -35,12 +35,12 @@ module.exports = class TeacherCourseSolutionView extends RootView
       return '' if l isnt @language
       a
 
-
   onLoaded: ->
     @listenTo me, 'change:preferredLanguage', @updateLevelData
     @updateLevelData()
 
   updateLevelData: ->
+    @levelSolutionsMap = @levels.getSolutionsMap(@language)
     for level in @levels?.models
       articles = level.get('documentation')?.specificArticles
       if articles
@@ -61,16 +61,6 @@ module.exports = class TeacherCourseSolutionView extends RootView
         playerCodeTag = utils.extractPlayerCodeTag(translatedDefaultCode)
         finalDefaultCode = if playerCodeTag then playerCodeTag else translatedDefaultCode
         level.set 'begin', finalDefaultCode
-        solution = _.find(programmableMethod.solutions, (x) => x.language is (level.get('primerLanguage') or @language))
-        try
-          solutionText = _.template(solution?.source)(utils.i18n(programmableMethod, 'context'))
-        catch error
-          solutionText = solution?.source
-          console.error "Couldn't create solution template of", solution?.source, "\nwith context", programmableMethod.context, "\nError:", error
-        solutionPlayerCodeTag = utils.extractPlayerCodeTag(solutionText)
-        finalSolutionCode = if solutionPlayerCodeTag then solutionPlayerCodeTag else solutionText
-        finalSolutionCode = @fingerprint finalSolutionCode
-        level.set 'solution',  finalSolutionCode
     levels = []
     for level in @levels?.models when level.get('original')
       continue if @language? and level.get('primerLanguage') is @language
@@ -84,20 +74,11 @@ module.exports = class TeacherCourseSolutionView extends RootView
 
   afterRender: ->
     super()
-    lang = @language
-    @$el.find('pre>code').each ->
-      els = $(@)
-      c = els.parent()
-      aceEditor = aceUtils.initializeACE c[0], lang
+    @$el.find('pre:has(code[class*="lang-"])').each ->
+      codeElem = $(@).first().children().first()
+      lang = mode for mode of aceUtils.aceEditModes when codeElem?.hasClass('lang-' + mode)
+      aceEditor = aceUtils.initializeACE(@, lang or 'python')
       aceEditor.setShowInvisibles false
       aceEditor.setBehavioursEnabled false
       aceEditor.setAnimatedScroll false
       aceEditor.$blockScrolling = Infinity
-
-  fingerprint: (code) ->
-    # Add a zero-width-space at the end of every comment line
-    switch @language
-      when 'javascript' then code.replace /^(\/\/.*)/gm, "$1​"
-      when 'lua' then code.replace /^(--.*)/gm, "$1​"
-      when 'html' then code.replace /^(<!--.*)-->/gm, "$1​-->"
-      else code.replace /^(#.*)/gm, "$1​"

app/views/teachers/TeacherStudentView.coffee
@@ -38,7 +38,7 @@ module.exports = class TeacherStudentView extends RootView
 
     # TODO: fetch only necessary thang data (i.e. levels with student progress, via separate API instead of complicated data.project values)
     @levels = new Levels()
-    @supermodel.trackRequest(@levels.fetchForClassroom(classroomID, {data: {project: 'name,original,i18n,thangs.id,thangs.components.config.programmableMethods.plan.solutions,thangs.components.config.programmableMethods.plan.context'}}))
+    @supermodel.trackRequest(@levels.fetchForClassroom(classroomID, {data: {project: 'name,original,i18n,primerLanguage,thangs.id,thangs.components.config.programmableMethods.plan.solutions,thangs.components.config.programmableMethods.plan.context'}}))
     @urls = require('core/urls')
 
     @singleStudentLevelProgressDotTemplate = require 'templates/teachers/hovers/progress-dot-single-student-level'
@@ -89,18 +89,16 @@ module.exports = class TeacherStudentView extends RootView
     oldEditor.destroy() for oldEditor in @aceEditors ? []
     @aceEditors = []
     aceEditors = @aceEditors
-    lang = @classroom.get('aceConfig')?.language or 'python'
+    classLang = @classroom.get('aceConfig')?.language or 'python'
     @$el.find('pre:has(code[class*="lang-"])').each ->
-      aceEditor = aceUtils.initializeACE(@, lang)
+      codeElem = $(@).first().children().first()
+      lang = mode for mode of aceUtils.aceEditModes when codeElem?.hasClass('lang-' + mode)
+      aceEditor = aceUtils.initializeACE(@, lang or classLang)
       aceEditors.push aceEditor
 
   updateSolutions: ->
     return unless @classroom?.loaded and @sessions?.loaded and @levels?.loaded
-    @levelSolutionMap = {}
-    for level in @levels.models
-      solution = level.getSolutions().find((s) => s.language is @classroom.get('aceConfig')?.language)?.source
-      solution ?= utils.extractPlayerCodeTag(level.getSolutions().find((s) => s.language is 'html')?.source or '')
-      @levelSolutionMap[level.get('original')] = solution
+    @levelSolutionsMap = @levels.getSolutionsMap(@classroom.get('aceConfig')?.language)
     @levelStudentCodeMap = {}
     for session in @sessions.models when session.get('creator') is @studentID
       # Normal level

commit 40bab3a6d7bb78d369a4b5ec271c613264fb57d7

:bug:Hide level solutions boilerplate html in student view
Regression introduced by 3542ee580a68abf2df496c4d7be95a5e4d4a1f3a

app/collections/Levels.coffee
@@ -22,6 +22,11 @@ module.exports = class LevelCollection extends CocoCollection
     @models.reduce((map, level) =>
       targetLangs = if level.get('primerLanguage') then [level.get('primerLanguage')] else languages
       solutions = level.getSolutions().filter((s) => s.language in targetLangs)
+      if 'html' in targetLangs
+        solutions?.forEach (s) =>
+          return unless s.language is 'html'
+          strippedSource = utils.extractPlayerCodeTag(s.source or '')
+          s.source = strippedSource if strippedSource
       map[level.get('original')] = solutions?.map((s) => {source: @fingerprint(s.source, s.language), description: s.description})
       map
     , {})

app/templates/teachers/teacher-student-view.jade
@@ -351,7 +351,6 @@ mixin studentLevelProgressDot(levelProgress, level, levelNumber, course)
   else if level.isProject()
     - labelText = translate('teacher.project');
     - dotClass += ' progress-dot-lg';
-  - console.log('context', context)
   .progress-dot.level-progress-dot(class=dotClass, data-html='true', data-title=view.singleStudentLevelProgressDotTemplate(context), data-level-slug=level.get('slug'), data-level-progress=(levelProgress || 'not started'))
     if link
       a(href=link, data-level-slug=levelSlugNav)