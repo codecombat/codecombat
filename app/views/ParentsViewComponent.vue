949d92fc98ca0ee114c3dac37245db7b19c5d1d5
Andrew Jakubowicz <spyr1014@gmail.com>
2018-08-10 15:41:08 -0700

    Add links and new copy

--- app/views/ParentsViewComponent.vue
+++ app/views/ParentsViewComponent.vue
@@ -15,15 +15,15 @@
       .col-lg-4.col-md-4
         img.img-responsive(src="/images/pages/parents/valueprop1.png")
         h4 The most effective way to learn type based code
-        p Students will cover material aligned with national curriculum standards, covering topics from computational thinking and algorithms, to big data and cybersecurity. They will come out having a grasp on how the internet of things works, and the personal and global impact of computer science.
+        p CodeCombat teaches the two most common coding languages. Students will cover material aligned with national curriculum standards, and cover topics from computational thinking and algorithms, to big data and cybersecurity.
       .col-lg-4.col-md-4
         img.img-responsive(src="/images/pages/parents/valueprop2.png")
         h4 Help them create their best future
-        p Computer Science isn’t just about coding and engineering, but also about learning how to navigate and become citizens in the digital world. The problem-to-solution methods taught by programming are something something blah blah blah just a few more words come on brain think of things
+        p Students will learn how to navigate and become citizens in the digital world. Problem-to-solution methods taught by programming support creative learning and help your child develop resilience and determination.
       .col-lg-4.col-md-4
         img.img-responsive(src="/images/pages/parents/valueprop3.png")
         h4 You've got growing super heroes, so we have heroes too
-        p CodeCombat isn’t just more homework, it’s a game that your kids will love to play. We know how important fun and engagement is for the developing brain, so we’ve packed in as much learning as any other CS classroom, while wrapping it up in a game they love. Your kids are the heroes of the future, they might as well have fun while they’re doing it.
+        p We know how important fun and engagement is for the developing brain, so we’ve packed in as much learning as any other CS classroom, while wrapping it up in a game they love.
     .row.text-center
       .refer-btn
         button.btn.btn-forest.btn-lg(v-on:click="$emit('referTeacher')") Refer a Teacher
@@ -36,16 +36,16 @@
         h2 Coding, not just for Engineers
       .col-lg-2.col-md-4.col-sm-12.pad-down
         div.img-flag
-          h5 Exploration Scientists
-        p.small Apollo got to the moon first through a lot of clever code, and the Curiosity Rover needs a team of exception people. As we explore the known world we are using and communicating with coders to help advance across the final frontier.
+          h5 Medical and Healthcare
+        p.small From mapping of the human genome, to MRI machines, coding allows us to understand the body in ways we’ve never been able to before.
       .col-lg-2.col-md-4.col-sm-12
         div.img-flag
-          h5 Medical and Healthcare
-        p.small From mapping of the human genome, to computational analysis of single neurons, coding allows us to understand the body in ways we’ve never been able to before. Coding knowledge is needed for the interpretation and use of medical imaging, and Google has recently launched an AI to help doctors make better decisions.
+          h5 Exploration Scientists
+        p.small Apollo got to the moon thanks to hardworking human computers, and scientists use computer programs to analyze the gravity of planets and search for new stars.
       .col-lg-2.col-md-4.col-sm-12.pad-down
         div.img-flag
           h5 Filmmaking and Animation
-        p.small From the robotics of Jurassic Park to the incredible Animation of Dreamworks and Pixar, computer knowledge has allowed us to create characters that transport us to another world.
+        p.small From the robotics of Jurassic Park to the incredible Animation of Dreamworks and Pixar, films wouldn’t be the same without computer science and the creatives behind the scenes.
   
     .row
       #dive-graphic-2.col-lg-6.col-md-12.col-sm-12.col-lg-push-6.text-center
@@ -53,15 +53,31 @@
         h2 The Reason For Game Based Learning
         img.img-responsive.deep-img(src="/images/pages/parents/deepdive2.png")
       .col-lg-6.col-md-12.col-sm-12.col-lg-pull-6
-        p There’s a reason we moved away from blackboard learning. Sure it may look like a game, but multiple studies repeatedly found that game based learning promoted cognitive development in kids, while also proving to be more effective in helping students learn and retain knowledge, concentrate and perform at a higher level of achievement.
-        p Game based learning is also good for teaching resilience, cognitive reasoning, and develop executive functions. Early studies have even shown the effectiveness of using games to teach science. What all this means is that educational games can be the most effective method for teaching kids to be the creators of their best future.
+        p
+          span.spr Multiple studies repeatedly found that game based learning promoted
+          a(href="https://www.learntechlib.org/p/19982/") cognitive development
+          span.spl.spr in kids, while also proving to be
+          a(href="http://webcache.googleusercontent.com/search?q=cache:v-UZ4U5_b7cJ:www.ejmste.com/A-Study-of-the-Effects-of-Digital-Learning-on-Learning-Motivation-and-Learning-Outcome,69635,0,2.html+&amp;cd=1&amp;hl=en&amp;ct=clnk&amp;gl=us") more effective
+          span.spl.spr in helping students
+          a(href="https://www.healio.com/nursing/journals/jcen/2016-8-47-8/%7B4c67edb9-1a9c-432d-af17-0f2856f3ce2c%7D/importance-of-gamification-in-increasing-learning") learn and retain knowledge
+          span.spr ,
+          a(href="http://www.sun.ac.za/english/learning-teaching/ctl/Documents/Gamification%20in%20education.pdf") concentrate
+          span.spl and perform at a higher level of achievement.
+        p
+          span.spr Game based learning is also good for teaching
+          a(href="https://mitpress.mit.edu/books/ecology-games") resilience
+          span.spr , cognitive reasoning, and develop
+          a(href="https://link.springer.com/chapter/10.1007/978-3-319-26508-7_20") executive functions
+          span . Science is just telling us what you teachers already know. Kids learn best by playing. 
     .row
       #dive-graphic-3.col-lg-6.col-md-12.col-sm-12.text-center
         div
         img.img-responsive.deep-img(src="/images/pages/parents/deepdive3.png")
         h2 The Reason For Game Based Learning
       .col-lg-6.col-md-12.col-sm-12
-        p We’re in almost 14,000 schools across the work and in 190 many countries because we know that in the future coding is going to be as fundamental a school as learning to read and write. Unfortunately, schools often find it difficult to find computer science teachers who can teach CS to an entire study body. We’ve worked closely with teachers to design and develop our levels, but your kids need <b>you</b> to help them learn. Neuroscience and researchers are in agreement about the positive effects of game based learning for kids, but <b>only</b> when it’s implemented correctly. Teacher participation is critical in ensuring those positive outcomes, so WE NEED YOUR HELP to create that connection between students and teachers.
+        p
+          a(href="https://www.theatlantic.com/technology/archive/2012/05/six-reasons-non-computer-nerd-might-want-learn-code/328102/") In the future
+          span.spl coding is going to be as fundamental a school as learning to read and write. We’ve worked closely with teachers to design and develop our levels, now your kids need you to help them learn. Educational technology works best when the teachers can implement it correctly which means that as a parent, students need your help to create that connection.
     .row.text-center
       .refer-btn
         button.btn.btn-forest.btn-lg(v-on:click="$emit('referTeacher')") Refer a Teacher
@@ -71,18 +87,19 @@
     #mission.row
       .col-md-4.col-sm-12
         img.img-responsive(src="/images/pages/parents/mission1.png")
-        h4 Level up computer science education in and out of the classroom
-        p We’ve built mechanics so that kids learn coding in a way that’s cross translational,  meaning that they know more than just how to pass our levels. These are actual skills that can be applied outside of the game.
-        p We have an amazing education specialist on our team who works closely with teachers to meet children where they are in the educational landscape and CodeCombat is a College Board endorsed provider for AP Computer Science principles.
+        p
+          b Level up computer science education in and out of the classroom
+        p Our education specialist works closely with teachers to meet children where they are in the educational landscape. Kids learn coding skills that can be applied outside of the game because it’s more than just copy and paste. They learn how to solve problems. And because we’ve worked with teachers, we’ve made sure that CodeCombat supports all different learning styles. 
       .col-md-4.col-sm-12
         img.img-responsive(src="/images/pages/parents/mission2.png")
-        h4 How and Why We Began
-        p 30% of teenagers in secondary school fail to reach proficiency in one subject across maths, reading, or science. We believe in doing better for our kids. So we created CodeCombat. CodeCombat was built to be the most engaging way to teach coding to students. A 2016 survey showed that 64% of girls in 3-5th grade want to learn how to code and 2/3rds of all students were asking for more edtech to be available in their school. There were 7 million job openings in 2015 required coding skills and with barriers to entry becoming higher, we built codecombat to because every child should be given a chance to create their best future.
+        p
+          b How and Why We Began
+        p CodeCombat was built to be the most engaging way to teach coding to students. A 2016 survey showed that 64% of girls in 3-5th grade want to learn how to code. There were 7 million job openings in 2015 required coding skills and with barriers to entry becoming higher, we built codecombat to because every child should be given a chance to create their best future. 
       .col-md-4.col-sm-12
         img.img-responsive(src="/images/pages/parents/mission3.png")
-        h4 We’re parents too
-        p Here at CodeCombat, we’re experts in the CS field. Our CEO, Nick, graduated Summa Cum Laude from Oberlin College in 2008. Our CTO, Matt, has been building software for the past 15 years and started off at Microsoft.
-        p But more than that, we’re parents too. Nick and Matt each have two growing superheros. At CodeCombat we’re parents. We’re coders. But most of all, we’re people who believe in giving our kids the best opportunity for success in whatever it is they decide to do.
+        p
+          b We’re parents too
+        p Here at CodeCombat, we’re experts in the CS field. But more than that, we’re parents too. Nick and Matt each have two growing superheros. At CodeCombat we’re parents. We’re coders. But most of all, we’re people who believe in giving our kids the best opportunity for success in whatever it is they decide to do.
 
 </template>
 
@@ -164,6 +181,9 @@ $green: #20572B
     width: 100%
   h3
     margin: 10px 0
+  div h4
+    text-align: center
+    padding: 10px
   div:nth-of-type(1)
     h4
       color: $red
