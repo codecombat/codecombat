63fd64a3f0da9d781782eda00735fa13a43a78ab
chethana Sastry <chethana@codecombat.com>
2018-09-07 12:36:56 -0700

    Fixing non-warrior hero image on courseVictorydialog

--- app/views/play/level/modal/CourseVictoryComponent.vue
+++ app/views/play/level/modal/CourseVictoryComponent.vue
@@ -164,7 +164,7 @@
           slug = heroMap[@$store.state.me.heroConfig.thangType]
           if !slug?
             return "/images/pages/play/modal/captain.png"
-          else
+          else if slug in thangTypeConstants.heroClasses.Warrior 
             return "/images/pages/play/modal/#{slug}.png"
       comboImage: ->
         if @allConceptsUsed
