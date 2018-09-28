891c909ae26f98b8dfb1f95f4ee16118aee34aed
Andrew Jakubowicz <spyr1014@gmail.com>
2018-09-28 12:26:42 -0700

    update webpack to forked v3

--- webpack.production.config.js
+++ webpack.production.config.js
@@ -39,7 +39,7 @@ module.exports = (env) => {
     .concat([
       new UglifyJsPlugin({
         uglifyOptions: {
-          ecma:8,
+          ecma: 8,
           compress: {
             arrows: false,
             booleans: false,
