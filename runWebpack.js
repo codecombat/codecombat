a41199906437381ff288e96104438b956fbb7e19
Shubhangi <shubhangigupta1910@gmail.com>
2018-11-02 12:11:05 -0700

    Small fix in runWebpack.js

--- runWebpack.js
+++ runWebpack.js
@@ -1,6 +1,6 @@
 var spawn = require('child_process').spawn
 
-if (process.env.SKIP_WEBPACK) {
+if (process.env.SKIP_WEBPACK == "true") {
   console.log("Skipping webpack build because SKIP_WEBPACK=true")
 }
 else{
