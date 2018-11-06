31b17bd2fa4bf319fe68deabe6ece3bfab0adaac
Andrew Jakubowicz <spyr1014@gmail.com>
2018-11-06 00:33:05 -0800

    DELETE_DEPENDENCIES flag added.

--- runWebpack.js
+++ runWebpack.js
@@ -1,14 +1,29 @@
-var spawn = require('child_process').spawn
+const cp = require("child_process");
 
-if (process.env.SKIP_WEBPACK == "true") {
-  console.log("Skipping webpack build because SKIP_WEBPACK=true")
-}
-else{
-  var command = spawn("webpack");
-  command.stdout.on('data', function (data) {
+const spawn = cp.spawn;
+const exec = cp.exec;
+
+const remove_dependencies = () => {
+  const rm = exec("rm -rf node_modules && rm -rf bower_components");
+  return rm;
+};
+
+if (process.env.SKIP_WEBPACK === "true") {
+  console.log("Skipping webpack build because SKIP_WEBPACK=true");
+} else {
+  const command = spawn("webpack");
+  command.stdout.on("data", function(data) {
     process.stdout.write(data);
   });
-  command.stderr.on('data', function (data) {
+  command.stderr.on("data", function(data) {
     process.stdout.write(data);
+  });
+
+  command.on("exit", () => {
+    // Use when we only need the public folder.
+    if (process.env.DELETE_DEPENDENCIES === "true") {
+      console.log("Cleaning up dependencies because `DELETE_DEPENDENCIES=true`.");
+      remove_dependencies();
+    }
   });
 }
