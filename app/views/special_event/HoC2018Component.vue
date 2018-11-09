faa193594d8483a185668ea2a799ec352f331991
chethana Sastry <chethana@codecombat.com>
2018-11-09 15:03:01 -0800

    Added all scripts under one script tag

--- app/views/special_event/HoC2018Component.vue
+++ app/views/special_event/HoC2018Component.vue
@@ -47,19 +47,19 @@
 
 </template>
 
-<script>
-  var APP_ID = "zzeo1k6k";
+ <script>
+    var APP_ID = "zzeo1k6k";
 
-  window.intercomSettings = {
-  app_id: APP_ID
-  };
-</script>
-<script>(function(){var w=window;var ic=w.Intercom;if(typeof ic==="function"){ic('reattach_activator');ic('update',intercomSettings);}else{var d=document;var i=function(){i.c(arguments)};i.q=[];i.c=function(args){i.q.push(args)};w.Intercom=i;function l(){var s=d.createElement('script');s.type='text/javascript';s.async=true;s.src='https://widget.intercom.io/widget/' + APP_ID;var x=d.getElementsByTagName('script')[0];x.parentNode.insertBefore(s,x);}if(w.attachEvent){w.attachEvent('onload',l);}else{w.addEventListener('load',l,false);}}})()</script>
+    window.intercomSettings = {
+    app_id: APP_ID
+    };
 
-<script>
-module.exports = Vue.extend({
-});
-</script>
+    (function(){var w=window;var ic=w.Intercom;if(typeof ic==="function"){ic('reattach_activator');ic('update',intercomSettings);}else{var d=document;var i=function(){i.c(arguments)};i.q=[];i.c=function(args){i.q.push(args)};w.Intercom=i;function l(){var s=d.createElement('script');s.type='text/javascript';s.async=true;s.src='https://widget.intercom.io/widget/' + APP_ID;var x=d.getElementsByTagName('script')[0];x.parentNode.insertBefore(s,x);}if(w.attachEvent){w.attachEvent('onload',l);}else{w.addEventListener('load',l,false);}}})()
+    
+    module.exports = Vue.extend({
+    });
+  </script>
+
 
 <style lang="sass">
 @import "../../styles/style-flat-variables"
