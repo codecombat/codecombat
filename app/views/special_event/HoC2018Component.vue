5df2c349c31ae759bce1c7a7fcb208f04e5cf88e
chethana Sastry <chethana@codecombat.com>
2018-11-09 13:30:43 -0800

    Intercom integration script added.

--- app/views/special_event/HoC2018Component.vue
+++ app/views/special_event/HoC2018Component.vue
@@ -48,6 +48,15 @@
 </template>
 
 <script>
+  var APP_ID = "zzeo1k6k";
+
+  window.intercomSettings = {
+  app_id: APP_ID
+  };
+</script>
+<script>(function(){var w=window;var ic=w.Intercom;if(typeof ic==="function"){ic('reattach_activator');ic('update',intercomSettings);}else{var d=document;var i=function(){i.c(arguments)};i.q=[];i.c=function(args){i.q.push(args)};w.Intercom=i;function l(){var s=d.createElement('script');s.type='text/javascript';s.async=true;s.src='https://widget.intercom.io/widget/' + APP_ID;var x=d.getElementsByTagName('script')[0];x.parentNode.insertBefore(s,x);}if(w.attachEvent){w.attachEvent('onload',l);}else{w.addEventListener('load',l,false);}}})()</script>
+
+<script>
 module.exports = Vue.extend({
 });
 </script>
