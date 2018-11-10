eda518ab59f0a5a15a974340e4219bb3cbeb144d
chethana Sastry <chethana@codecombat.com>
2018-11-09 16:40:33 -0800

    Intercom integration for Single Page Apps

--- app/views/special_event/HoC2018Component.vue
+++ app/views/special_event/HoC2018Component.vue
@@ -47,18 +47,18 @@
 
 </template>
 
- <script>
-    var APP_ID = "zzeo1k6k";
+<script>
+  (function(){var w=window;var ic=w.Intercom;if(typeof ic==="function"){ic('reattach_activator');ic('update','intercomSettings');}else{var d=document;var i=function(){i.c(arguments)};i.q=[];i.c=function(args){i.q.push(args)};w.Intercom=i;function l(){var s=d.createElement('script');s.type='text/javascript';s.async=true;
+  s.src='https://widget.intercom.io/widget/{zzeo1k6k}';
+  var x=d.getElementsByTagName('script')[0];x.parentNode.insertBefore(s,x);}if(w.attachEvent){w.attachEvent('onload',l);}else{w.addEventListener('load',l,false);}}})()
 
-    window.intercomSettings = {
-    app_id: APP_ID
-    };
-
-    (function(){var w=window;var ic=w.Intercom;if(typeof ic==="function"){ic('reattach_activator');ic('update',intercomSettings);}else{var d=document;var i=function(){i.c(arguments)};i.q=[];i.c=function(args){i.q.push(args)};w.Intercom=i;function l(){var s=d.createElement('script');s.type='text/javascript';s.async=true;s.src='https://widget.intercom.io/widget/' + APP_ID;var x=d.getElementsByTagName('script')[0];x.parentNode.insertBefore(s,x);}if(w.attachEvent){w.attachEvent('onload',l);}else{w.addEventListener('load',l,false);}}})()
-    
-    module.exports = Vue.extend({
-    });
-  </script>
+  window.Intercom('boot', {
+   app_id: 'zzeo1k6k',
+   custom_launcher_selector: '#my_custom_link' 
+  });
+  module.exports = Vue.extend({
+  });
+</script>
 
 
 <style lang="sass">
