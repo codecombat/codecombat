708b94877727c7110935bce1db38d22848028bc7
Shubhangi <shubhangigupta1910@gmail.com>
2018-12-06 18:08:48 -0800

    Remove DELETE_DEPENDENCIES

--- runWebpack.js
+++ runWebpack.js
@@ -3,18 +3,10 @@
  * By default runs webpack.
  *
  * Set SKIP_WEBPACK=true` to skip the webpack build.
- * Set `DELETE_DEPENDENCIES=true` to delete node_modules and bower_components.
- * Useful when building the client for only the `public` folder.
  */
 const cp = require("child_process");
 
 const spawn = cp.spawn;
-const exec = cp.exec;
-
-const remove_dependencies = () => {
-  const rm = exec("rm -rf node_modules && rm -rf bower_components");
-  return rm;
-};
 
 if (process.env.SKIP_WEBPACK === "true") {
   console.log("Skipping webpack build because SKIP_WEBPACK=true");
@@ -25,15 +17,5 @@ if (process.env.SKIP_WEBPACK === "true") {
   });
   command.stderr.on("data", function(data) {
     process.stdout.write(data);
-  });
-
-  command.on("exit", () => {
-    // Use when we only need the public folder.
-    if (process.env.DELETE_DEPENDENCIES === "true") {
-      console.log(
-        "Cleaning up dependencies because `DELETE_DEPENDENCIES=true`."
-      );
-      remove_dependencies();
-    }
   });
 }
