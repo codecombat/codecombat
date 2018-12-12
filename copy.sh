e3871f0d2e54b9ba67f7c78561ea91a489cc85c9
Andrew Jakubowicz <spyr1014@gmail.com>
2018-12-12 14:07:01 -0800

    Minor changes to install for js to work

--- copy.sh
+++ copy.sh
@@ -1,5 +1,8 @@
-cp bower_components/esper.js/esper-plugin-lang-python.js public/javascripts/app/vendor/aether-python.js
-cp bower_components/esper.js/esper-plugin-lang-lua.js public/javascripts/app/vendor/aether-lua.js
-cp bower_components/esper.js/esper-plugin-lang-coffeescript.js public/javascripts/app/vendor/aether-coffeescript.js
-touch public/javascripts/app/vendor/aether-javascript.js
+# cp bower_components/esper.js/esper-plugin-lang-python.js public/javascripts/app/vendor/aether-python.js
+# cp bower_components/esper.js/esper-plugin-lang-lua.js public/javascripts/app/vendor/aether-lua.js
+# cp bower_components/esper.js/esper-plugin-lang-coffeescript.js public/javascripts/app/vendor/aether-coffeescript.js
 
+# These are empty because we aren't suppoerting them yet or they come included.
+touch bower_components/aether/build/javascript.js
+touch bower_components/aether/build/html.js
+touch bower_components/aether/build/java.js
