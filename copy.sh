bb8eecafa09ef39c79010ce047116eb76eb7ca8b
Andrew Jakubowicz <spyr1014@gmail.com>
2019-01-09 12:29:47 -0800

    Add html parser in setup-aether

--- copy.sh
+++
@@ -1,4 +0,0 @@
-# These are empty because we aren't supporting them yet or they come included.
-# TODO: This is a hack to prevent errors from built in languages.
-touch public/javascripts/app/vendor/aether-html.js
-touch public/javascripts/app/vendor/aether-html.modern.js
