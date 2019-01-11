66b9e5babcdd08de1417901f8e68036f75584cdb
Shubhangi <shubhangigupta1910@gmail.com>
2019-01-10 17:43:51 -0800

    Adding test case for CourseVideosComponent.vue

--- app/views/courses/CourseVideosComponent.vue
+++ app/views/courses/CourseVideosComponent.vue
@@ -3,7 +3,7 @@
     p
       a(href="/students", data-i18n="courses.back_courses")
     div.m-t-2
-      h2(v-if="courseName").text-center
+      h2(v-if="courseName").text-center.course-name
         | {{ courseName }}
       h1.text-center
         | {{ $t('courses.concept_videos') }}
@@ -12,31 +12,31 @@
       .row.m-t-5
         .col-md-6
           iframe.video(src="https://player.vimeo.com/video/310626758" frameborder= "2" webkitallowfullscreen mozallowfullscreen allowfullscreen)
-        .col-md-6.rtl-allowed
-          .semibold.m-l-5.concept-text
+        .col-md-6.rtl-allowed.concept-text
+          .semibold.m-l-5.concept-heading
             span.spr(data-i18n="courses.concept")
             span.spr(data-i18n="courses.basic_syntax")
-          p.m-l-5.m-t-2
+          p.m-l-5.m-t-2.concept-desc
             span(data-i18n="courses.basic_syntax_desc")
       
       .row.m-t-5
         .col-md-6
           iframe.video(src="https://player.vimeo.com/video/310626741" frameborder= "0" webkitallowfullscreen mozallowfullscreen allowfullscreen)
-        .col-md-6.rtl-allowed
-          .semibold.m-l-5.concept-text
+        .col-md-6.rtl-allowed.concept-text
+          .semibold.m-l-5.concept-heading
             span.spr(data-i18n="courses.concept")
             span.spr(data-i18n="courses.while_loops")
-          p.m-l-5.m-t-2
+          p.m-l-5.m-t-2.concept-desc
             span(data-i18n="courses.while_loops_desc")
 
       .row.m-t-5
         .col-md-6
           iframe.video(src="https://player.vimeo.com/video/310626807" frameborder= "0" webkitallowfullscreen mozallowfullscreen allowfullscreen)
-        .col-md-6.rtl-allowed
-          .semibold.m-l-5.concept-text
+        .col-md-6.rtl-allowed.concept-text
+          .semibold.m-l-5.concept-heading
             span.spr(data-i18n="courses.concept")
             span.spr(data-i18n="courses.variables")
-          p.m-l-5.m-t-2
+          p.m-l-5.m-t-2.concept-desc
             span(data-i18n="courses.variables_desc")
 </template>
 
@@ -65,6 +65,6 @@ export default Vue.extend({
     width: 600px
     height: 320px
 
-.concept-text
+.concept-heading
   font-size: 16pt
 </style>
