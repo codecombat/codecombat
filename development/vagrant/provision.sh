7311c4aaa9d560da127367b65a3f610d2b1093cc
Joe Mifsud <joe@mifsud.me>
2019-03-06 15:11:53 -0800

    Small tweaks to vagrant provision script

--- development/vagrant/provision.sh
+++ development/vagrant/provision.sh
@@ -6,11 +6,12 @@ COCO_CLIENT_ROOT=/vagrant
 
 # inform apt that there's no user to answer interactive questions
 export DEBIAN_FRONTEND=noninteractive
+BASHRC=$HOME/.bashrc
 
 echo "Installing system dependencies..."
 sudo apt-get update
 sudo apt-get install -y git curl libssl-dev libreadline-dev zlib1g-dev autoconf bison \
-                        build-essential libyaml-dev libreadline-dev libncurses5-dev \
+                        build-essential libyaml-dev libncurses5-dev \
                         libffi-dev libgdbm-dev python2.7 python-pip build-essential
 
 echo "Installing Ruby..."
@@ -22,14 +23,13 @@ if ! rbenv versions | grep -q $RUBY_VERSION; then
     rbenv install $RUBY_VERSION
     rbenv global $RUBY_VERSION
 
-    echo 'export PATH=$PATH:$HOME/.rbenv/bin:$HOME/.rbenv/shims' >> .bashrc
-    echo 'eval "$(rbenv init -)"' >> .bashrc
+    echo 'export PATH=$PATH:$HOME/.rbenv/bin:$HOME/.rbenv/shims' >> $BASHRC
+    echo 'eval "$(rbenv init -)"' >> $BASHRC
 fi
 
 echo "Installing nvm..."
 curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.34.0/install.sh | bash
 
-BASHRC=$HOME/.bashrc
 if ! grep -q nvm $BASHRC; then
     echo 'NVM_DIR="$HOME/.nvm"' >> $BASHRC
     echo '[ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"' >> $BASHRC
