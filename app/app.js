a8938ae9f73ecdf0b671bbdc25cfe3281ee1b0c2
Andrew Jakubowicz <spyr1014@gmail.com>
2019-03-15 07:24:38 +1100

    Clean up quotes

--- app/app.js
+++ app/app.js
@@ -1,18 +1,18 @@
-global.$ = window.$ = global.jQuery = window.jQuery = require("jquery");
-import "bootstrap";
-import "./app.sass";
-import "babel-polyfill";
+global.$ = window.$ = global.jQuery = window.jQuery = require('jquery');
+import 'bootstrap';
+import './app.sass';
+import 'babel-polyfill';
 
 /**
  * Fonts dynamically imported based on infra location
  */
 if (window.features && window.features.chinaUx) {
-  import(/* webpackChunkName: "ChinaFont" */ "app/styles/common/fontChina.sass");
+  import(/* webpackChunkName: "ChinaFont" */ 'app/styles/common/fontChina.sass');
 } else {
-  import(/* webpackChunkName: "UsFont" */ "app/styles/common/fontUS.sass");
+  import(/* webpackChunkName: "UsFont" */ 'app/styles/common/fontUS.sass');
 }
 
-require("app/vendor.js");
+require('app/vendor.js');
 
 // require.context('app/schemas', true, /.*\.(coffee|jade)/)
 // require.context('app/models', true, /.*\.(coffee|jade)/)
@@ -20,4 +20,4 @@ require("app/vendor.js");
 // require.context('app/core', true, /.*\.(coffee|jade)/)
 // require.context('app/views/core', true, /.*\.(coffee|jade)/)
 
-require("core/initialize");
+require('core/initialize');
