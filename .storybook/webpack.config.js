884c0595860ef94444340de29cd01ad0a202a388
Andrew Jakubowicz <spyr1014@gmail.com>
2019-03-26 16:16:20 -0700

    Add source code documentation to Storybook

---
+++ .storybook/webpack.config.js
@@ -0,0 +1,9 @@
+module.exports = function({ config }) {
+  config.module.rules.push({
+    test: /\.stories\.jsx?$/,
+    loaders: [require.resolve('@storybook/addon-storysource/loader')],
+    enforce: 'pre',
+  });
+
+  return config;
+};
