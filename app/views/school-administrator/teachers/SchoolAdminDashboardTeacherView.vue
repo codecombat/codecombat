504112d333c84e7aa076491fd6a88ed798686544
Joe Mifsud <joe@mifsud.me>
2019-03-31 14:09:52 -0700

    Wire school admin dashboard vuex module to backend.

--- app/views/school-administrator/teachers/SchoolAdminDashboardTeacherView.vue
+++ app/views/school-administrator/teachers/SchoolAdminDashboardTeacherView.vue
@@ -5,7 +5,7 @@
 <template>
     <h3 v-if="loading.teacher || loading.classrooms">{{ $t('common.loading') }}</h3>
     <div v-else>
-        <h4>{{ teacher.get('firstName') }} {{ teacher.get('lastName') }}'s {{ $t('classes') }}</h4> <!-- TODO apply i18n to possessive -->
+        <h4>{{ teacher.firstName }} {{ teacher.lastName }}'s {{ $t('school_administrator.classes') }}</h4> <!-- TODO apply i18n to possessive -->
     </div>
 </template>
 
