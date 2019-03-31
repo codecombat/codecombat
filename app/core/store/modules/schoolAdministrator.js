6041b90580176ab68dc9f712d0254a9f000caac4
Joe Mifsud <joe@mifsud.me>
2019-03-31 10:34:45 -0700

    Final structure of teacher dashboard vue components to work with router.

--- app/core/store/modules/schoolAdministrator.js
+++ app/core/store/modules/schoolAdministrator.js
@@ -1,35 +1,77 @@
+import classroomsApi from 'core/api/classrooms'
+import User from 'models/User'
+
 export default {
   namespaced: true,
 
   state: {
-    loading: false,
+    loading: {
+      teacher: false,
+      classrooms: false,
+      teachers: false
+    },
+
+    teacher: undefined,
+
     isSchoolAdministrator: false,
-    administratedTeachers: []
+    administratedTeachers: [],
+
+    teacherClassrooms: []
   },
 
   mutations: {
-    toggleLoading: (state) => state.loading = !state.loading,
+    toggleLoading: (state, key) => state.loading[key] = !state.loading[key],
+
+    setTeacher: (state, teacher) => state.teacher = teacher,
 
     addTeachers: (state, teachers) => {
       state.administratedTeachers = teachers;
+    },
+
+    addClassrooms: (state, classrooms) => {
+      state.teacherClassrooms = classrooms;
     }
   },
 
   actions: {
-    fetch: ({ commit }) => {
-      commit('toggleLoading')
+    fetchTeachers: ({ commit }) => {
+      commit('toggleLoading', 'teachers')
 
       setTimeout(() => {
         commit(
           'addTeachers',
           [
-            { name: 'Teacher 1', email: 'teacher1@education.com', lastLogin: 'LAST_LOGIN' },
-            { name: 'Teacher 2', email: 'teacher2@education.com', lastLogin: 'LAST_LOGIN' },
+            new User({ _id: '5c94f939b7923904e40fc6e0', name: 'Teacher 1', email: 'teacher1@education.com', lastLogin: 'LAST_LOGIN' }),
+            new User({ _id: '5c99876f08583a0075d5dfc7', name: 'Teacher 2', email: 'teacher2@education.com', lastLogin: 'LAST_LOGIN' }),
           ]
         )
 
-        commit('toggleLoading')
+        commit('toggleLoading', 'teachers')
       }, 1000)
+    },
+
+    fetchTeacher: ({ commit, state }, id) => {
+      commit('toggleLoading', 'teacher')
+
+
+      for (const teacher of state.administratedTeachers) {
+        if (teacher.get('id')) {
+          commit('setTeacher', teacher)
+          break;
+        }
+      }
+
+      commit('toggleLoading', 'teacher')
+    },
+
+    fetchTeacherClassrooms: ({ commit }, id) => {
+      commit('toggleLoading', 'classrooms')
+
+      const res = classroomsApi.fetchByOwner(id)
+      console.log(res)
+      return res.then((res) => commit('addClassrooms', res.body))
+        .then(() => commit('toggleLoading', 'classrooms'))
+        .error((e) => console.error('Classrooms failure', e)) // TODO handle this
     }
   }
 }
