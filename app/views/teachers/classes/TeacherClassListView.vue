6c3a4e275f339772cfc83510129521391dd3757c
Joe Mifsud <joe@mifsud.me>
2019-04-01 12:47:28 -0700

    Start rendering of teacher classrooms

---
+++ app/views/teachers/classes/TeacherClassListView.vue
@@ -0,0 +1,31 @@
+<style scoped>
+    ul.teacher-list {
+        margin: 0;
+        padding: 0;
+    }
+</style>
+
+<template>
+    <ul class="teacher-list">
+        <classroom-list-row
+                v-for="classroom in activeClassrooms"
+                :key="classroom._id"
+                :classroom="classroom">
+        </classroom-list-row>
+    </ul>
+</template>
+
+<script>
+  import ClassroomListRow from './ClassroomListRow'
+
+  export default {
+    components: {
+      'classroom-list-row': ClassroomListRow
+    },
+
+    props: {
+      activeClassrooms: Array
+    }
+  }
+</script>
+
