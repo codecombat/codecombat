bac97a19e352e572e9d8dbf77688848a1a062df9
Shubhangi <shubhangigupta1910@gmail.com>
2019-04-03 14:55:12 -0700

    Set Vue globally in the storybook config

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,5 +1,9 @@
 import { configure } from '@storybook/vue';
 
+// set the global Vue for using it in the vue components
+import Vue from 'vue'
+window.Vue = Vue
+
 // automatically import all files ending in *.stories.js
 const req = require.context('../stories', true, /\.stories\.js$/);
 function loadStories() {
