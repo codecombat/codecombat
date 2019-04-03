7398237cd1ebc46f7ef77fd698fb519185ca3d05
Joe Mifsud <joe@mifsud.me>
2019-04-03 01:04:50 -0700

    Protoype render teacher class view inside vue

---
+++ app/views/teachers/class/TeacherClassView.vue
@@ -0,0 +1,24 @@
+<template>
+    <div></div>
+</template>
+
+<script>
+  import TeacherClassView from 'app/views/courses/TeacherClassView'
+
+  export default {
+    created() {
+      this.teacherClassView = new TeacherClassView({}, this.$route.params.classroomId)
+    },
+
+    mounted() {
+      this.teacherClassView.render()
+      this.$el.appendChild(this.teacherClassView.el)
+    },
+
+    render() {},
+
+    beforeDestroy() {
+      this.teacherClassView.destroy()
+    }
+  }
+</script>
