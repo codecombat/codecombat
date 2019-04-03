d183142f906eb9587ba7fe7165c04fbbb6d20575
Joe Mifsud <joe@mifsud.me>
2019-04-03 16:54:19 -0700

    Integrate new Vue setup

---
+++ app/views/core/SingletonAppVueComponentView.js
@@ -0,0 +1,31 @@
+import VueComponentVue from './VueComponentView'
+
+import store from 'core/store'
+import cocoVueRouter from 'core/CocoVueRouter'
+
+import Root from './Root'
+
+export default class SingletonAppVueComponentView extends VueComponentVue {
+
+  constructor () {
+    // For now we only support the default the default base template
+    super(null, {})
+  }
+
+  buildVueComponent () {
+    this.router = cocoVueRouter()
+
+    return new Vue({
+      el: this.$el.find('#site-content-area')[0],
+
+      store,
+      router: this.router,
+
+      render: (h) => h(Root)
+    })
+  }
+
+  route(path) {
+    this.router.push(path)
+  }
+}
