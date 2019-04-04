dc902557bfea6e66cb55a9a361eace8a8f839cce
Andrew Jakubowicz <spyr1014@gmail.com>
2019-04-04 13:11:09 -0700

    Mouse movement works

--- app/views/CinematicCanvas.vue
+++ app/views/CinematicCanvas.vue
@@ -13,10 +13,19 @@ const Camera = require('lib/surface/Camera')
 
 const ThangType = require('models/ThangType')
 const Lank = require('lib/surface/Lank')
-
+import {controller} from './play/cinematic/cinematicController'
 const CENTER = {
   x: 300,
   y: 175
+}
+controller()
+
+
+// Takes in a lank and a position. Returns lank with new position.
+function moveLank(lank, pos)  {
+  lank.thang.stateChanged = true
+  lank.thang.pos = pos
+  return lank
 }
 
 export default {
@@ -38,8 +47,8 @@ export default {
       acts: true,
       stateChanged: true,
       pos: {
-        x: 0,
-        y: 0,
+        x: 2.5,
+        y: 1,
         z: 1
       },
       shadow: 0,
@@ -53,6 +62,14 @@ export default {
     camera: null,
   }),
   methods: {
+    moveHandler(e) {
+      // We get the X and Y from the event.
+      const {stageX, stageY} = e;
+      // Transform mouse coordinates to world meters.
+      const result = this.camera.canvasToWorld({x: stageX, y: stageY})
+      // we move the lank.
+      moveLank(this.lank, result)
+    },
     createLankFromThang() {
       console.log("call createLankFromThang")
       const lank = this.lank = new Lank(this.thangType, {
@@ -60,15 +77,13 @@ export default {
         preloadSounds: false,
         thang: this.mockThang,
         camera: this.camera,
-        isCinematic: true,
         // This must be passed in as `new Mark` uses a groundLayer.
         // Without this nothing works. In this case I am using a dummy layer.
+        // Cinematics doesn't require Marks
         groundLayer: this.stubRequiredLayer
       })
 
       this.showLank(lank)
-
-      lank.queueAction('idle')
     },
     showLank(lank) {
       this.layerAdapter.resetSpriteSheet()
@@ -78,15 +93,17 @@ export default {
     initStage () {
       const canvas = this.$refs['cinematic-canvas']
       this.stage = new createjs.StageGL(canvas)
-      // Camera requires a jquery object to work
+      
+      // Camera requires a jquery object to work???
       const camera = this.camera = new Camera($(canvas))
       this.stubRequiredLayer = new LayerAdapter({name: 'Ground', webGL: true, camera: camera})
       this.layerAdapter = new LayerAdapter({name: 'Default', webGL: true, camera: camera})
       
-      this.layerAdapter.on("new-spritesheet", (spritesheet) =>{
-        // Creating a new spritesheet changes x and y of layer.
-        this.layerAdapter.container.x = CENTER.x;
-        this.layerAdapter.container.y = CENTER.y;
+      this.layerAdapter.on("new-spritesheet", (spritesheet) => {
+        // Now we have a working Anya that we can move around.
+        // Potentially use this for loading behavior.
+        // By counting how many times this is triggerred by ThangsTypes being loaded.
+        this.stage.addEventListener('stagemousemove', this.moveHandler)
       })
 
       this.stage.addChild(this.layerAdapter.container)
@@ -94,7 +111,6 @@ export default {
       /**
        * Initialize an example Thang.
        */
-
       // https://codecombat.com/db/thang.type/cinematic-anya
       const anya_url = new ThangType({_id: "cinematic-anya"}).getURL()
 
@@ -104,37 +120,28 @@ export default {
           this.thangType = new ThangType(data)
           this.createLankFromThang()
         }).then(this.initTicker)
+        .then(() => {
+          // Set the camera zoom.
+          this.camera.zoomTo({x: 0, y: 0}, 7, 0)
+        })
 
     },
 
     initTicker() {
-      let ticks = 0;
       createjs.Ticker.framerate = 30;
       
       const listener = {
         handleEvent: () => {
-          if (ticks >= 100000) {
-            return;
-          }
-          ticks ++;
           if (!this.lank) {
             return
           }
           this.lank.update(true);
-          // console.log("UPdate", this.lank)
-          // console.log({x: this.lank.sprite.x, y: this.lank.sprite.y})
+
           this.stage.update()
         }
       }
       createjs.Ticker.addEventListener("tick", listener)
 
-      /** INITIATE Camera random movement */
-      setInterval(() => {
-        const pos = {x: Math.random() * CENTER.x * 2, y: Math.random() * CENTER.y * 2}
-        console.log("move camera", pos)
-        this.camera.zoomTo(pos)
-        this.lank.queueAction(['idle', 'bash', 'move', 'die'][Math.floor(Math.random() * 4)])
-      }, 3000)
     }
   }
 }
---
+++ app/views/play/cinematic/cinematicController.js
@@ -0,0 +1,8 @@
+/**
+ * This file contains the Cinematic Controller. 
+ * This system is responsible for orchestrating the Cinematic.
+ */
+
+export const controller = () => {
+  console.log('REGISTERED CONTROLLER')
+}
