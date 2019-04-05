9faa00a6f818aab38a93cf61bde657753690b0b1
codeluggage <matias.forbord@gmail.com>
2019-04-05 23:09:01 +0200

    First few steps of Vue breadcrumbs - with state issues

--- app/views/teachers/classes/TeacherClassListView.vue
+++ app/views/teachers/classes/TeacherClassListView.vue
@@ -6,26 +6,38 @@
 </style>
 
 <template>
-    <ul class="teacher-list">
-        <classroom-list-row
-                v-for="classroom in activeClassrooms"
-                :key="classroom._id"
-                :classroom="classroom">
-        </classroom-list-row>
-    </ul>
+    <div>
+        <breadcrumbs v-bind:links="links"></breadcrumbs>
+        <ul class="teacher-list">
+            <classroom-list-row
+                    v-for="classroom in activeClassrooms"
+                    :key="classroom._id"
+                    :classroom="classroom">
+            </classroom-list-row>
+        </ul>
+    </div>
 </template>
 
 <script>
   import ClassroomListRow from './ClassroomListRow'
+  import Breadcrumbs from '../../common/BreadcrumbComponent'
 
   export default {
     components: {
-      'classroom-list-row': ClassroomListRow
+      'classroom-list-row': ClassroomListRow,
+      Breadcrumbs
     },
 
     props: {
       activeClassrooms: Array
-    }
+    },
+
+    data: () => ({
+      links: [{
+        href: '/school-admin',
+        i18n: 'school_administrator.my_teachers'
+      }]
+    })
   }
 </script>
 
