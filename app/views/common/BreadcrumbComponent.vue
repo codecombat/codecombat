111bcf2b90dc5ba62dcb4e829c3732979fc21605
codeluggage <matias.forbord@gmail.com>
2019-04-05 23:09:01 +0200

    First few steps of Vue breadcrumbs - with state issues

---
+++ app/views/common/BreadcrumbComponent.vue
@@ -0,0 +1,38 @@
+<style scoped>
+    .breadcrumbs > a {
+         color: #065e73;
+     }
+</style>
+
+<template>
+    <div class="breadcrumbs rtl-allowed">
+        Current state of links: {{ links }}
+        <br>
+<!--        TODO: Try a loop checking against length further than current length instead -->
+        <a v-if="links.length > 0" :href="links[0].href">{{ links[0].i18n ? $t(links[0].i18n) : links[0].text }}</a>
+        <span v-if="links.length > 1">
+            <span v-if="links[1].href" class="spl spr">></span>
+            <span v-else class="spl spr">>{{ links[1].i18n ? $t(links[1].i18n) : links[1].text }}</span>
+            <a v-if="links[1].href" :href="links[1].href">{{ links[1].i18n ? $t(links[1].i18n) : links[1].text }}</a>
+        </span>
+        <span v-if="links.length > 2">
+            <span v-if="links[2].href" class="spl spr">></span>
+            <span v-else class="spl spr">>{{ links[2].i18n ? $t(links[2].i18n) : links[2].text }}</span>
+            <a v-if="links[2].href" :href="links[2].href">{{ links[2].i18n ? $t(links[2].i18n) : links[2].text }}</a>
+        </span>
+    </div>
+</template>
+
+<script>
+  export default {
+    props: {
+      /*
+      Each item in the array needs at least one of the following:
+      [Optional String] href: the location for the breadcrumb. Skip this to render the link as text and not a href.
+      [Optional String] i18n: the lookup value to use i18n as the display text
+      [Optional String] text: raw display text
+      */
+      links: Array,
+    },
+  }
+</script>
--- app/views/teachers/class/TeacherClassView.vue
+++ app/views/teachers/class/TeacherClassView.vue
@@ -1,5 +1,6 @@
 <template>
     <loading-progress :loading-status="[ backboneLoadProgress ]" :always-render="true">
+        <breadcrumbs v-if="!loading" v-bind:links="links"></breadcrumbs>
         <backbone-view-harness
                 :backbone-view="backboneViewInstance"
                 :backbone-options="{ vue: true, readOnly: true }"
@@ -11,20 +12,32 @@
 </template>
 
 <script>
+  import { mapState } from 'vuex'
   import TeacherClassView from 'views/courses/TeacherClassView'
   import LoadingProgress from 'views/core/LoadingProgress'
   import BackboneViewHarness from 'views/common/BackboneViewHarness'
+  import Breadcrumbs from '../../common/BreadcrumbComponent'
 
   export default {
     components: {
       LoadingProgress,
-      BackboneViewHarness
+      BackboneViewHarness,
+      Breadcrumbs
     },
 
     data: function () {
       return {
         backboneLoadProgress: 100,
-        backboneViewInstance: TeacherClassView
+        backboneViewInstance: TeacherClassView,
+        links: [{
+          href: '/school-administrator',
+          i18n: 'school_administrator.my_teachers'
+        }, {
+          href: `/school-administrator/teachers/${this.$route.teacherId}`,
+          text: this.teacherName
+        }, {
+          text: this.classroomName()
+        }]
       }
     },
 
@@ -35,7 +48,31 @@
         } else {
           this.backboneLoadProgress = 100
         }
+      },
+      classroomName: function () {
+        console.log('trying to get classroomName:')
+        console.log(this.$route.params.classroomId)
+        const classroom = new Classroom(this.$route.params.classroomId)
+        console.log(classroom)
+        console.log(classroom.get('name'))
+        return classroom.get('name')
       }
     },
+
+    computed: Object.assign({},
+      mapState('schoolAdministrator', [
+        'administratedTeachers'
+      ]),
+
+      {
+        teacherName: () => {
+          console.log('inside teacherName now!')
+          console.log(this.administratedTeachers)
+          const teacher = this.administratedTeachers.find(t => t.id === this.$route.teacherId)
+          console.log(teacher)
+          return teacher.firstName ? `${teacher.firstName} ${teacher.lastName}` : teacher.name
+        }
+      }
+    ),
   }
 </script>
