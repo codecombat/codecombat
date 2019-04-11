d1c3c353fbf4319ddc3a389e108a5d374eb95827
Andrew Jakubowicz <spyr1014@gmail.com>
2019-04-11 16:23:16 -0700

    Add koudashijie to allowed web-dev origins (#5208)

--- app/assets/javascripts/web-dev-listener.js
+++ app/assets/javascripts/web-dev-listener.js
@@ -29,6 +29,7 @@ var allowedOrigins = [
     /^https?:\/\/10.0.2.2:[\d]+$/, // For local virtual machines
     /^https?:\/\/coco\.code\.ninja$/,
     /^https?:\/\/.*codecombat-staging-codecombat\.runnableapp\.com$/,
+    /^https?:\/\/(.*\.)?koudashijie\.com$/ // For china infrastructure
 ];
 
 function receiveMessage(event) {
