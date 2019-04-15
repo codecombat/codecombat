8c1fb11829c24c5a19b326486fea567fcfae327d
Andrew Jakubowicz <spyr1014@gmail.com>
2019-04-15 16:44:52 -0700

    Add AbstractCommand tests

---
+++ test/app/lib/cinematic/AbstractCommand.spec.js
@@ -0,0 +1,44 @@
+/* eslint-env jasmine */
+import AbstractCommand, { run, cancel } from '../../../../app/views/play/cinematic/Command/AbstractCommand'
+import * as PromiseBB from 'bluebird'
+
+PromiseBB.config({
+  cancellation: true
+})
+
+class NaiveExtend extends AbstractCommand {}
+
+class PassInRunCommand extends AbstractCommand {
+  constructor (run) {
+    super()
+    this.run = run
+  }
+}
+
+describe('AbstractCommand', () => {
+  it('abstract class can\'t be constructed', () => {
+    expect(() => new AbstractCommand()).toThrow()
+  })
+  it('constructor works when extended', () => {
+    expect(() => new NaiveExtend()).not.toThrow()
+  })
+  it('run method must be implemented', () => {
+    expect(() => (new NaiveExtend()).run()).toThrow()
+  })
+  it('run must return a promise', () => {
+    const invalidRunFunctions = [
+      () => undefined,
+      () => true,
+      () => false,
+      () => '',
+      () => 'abc',
+      () => {},
+      () => ({ then: () => { } }), // Fake promise
+      () => Promise.resolve() // non cancellable promise
+    ]
+
+    for (const fn of invalidRunFunctions) {
+      expect(() => (new PassInRunCommand(fn))[run]()).toThrow()
+    }
+  })
+})
