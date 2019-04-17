96f31cb2ea841380249ac595e44e76a792f2c2d7
Andrew Jakubowicz <spyr1014@gmail.com>
2019-04-17 14:06:21 -0700

    Deleted comments

--- app/views/play/cinematic/Command/CommandRunner.js
+++ app/views/play/cinematic/Command/CommandRunner.js
@@ -29,9 +29,6 @@ export default class CommandRunner {
    */
   async run () {
     for (const command of this.commands) {
-      // TODO: Make sure cancelling is more robust. Maybe by racing them?
-      // Uncomment below to get cancel behavior.
-      // setTimeout(() => command[cancel](), 60)
       await command[run]()
     }
   }
