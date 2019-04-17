a748f797699a8348e60a189a42d2bb470c73220a
Andrew Jakubowicz <spyr1014@gmail.com>
2019-04-17 13:42:47 -0700

    Improve commandRunner documentation

--- app/views/play/cinematic/Command/CommandRunner.js
+++ app/views/play/cinematic/Command/CommandRunner.js
@@ -1,5 +1,9 @@
 import { run, cancel } from './AbstractCommand'
 
+/**
+ * CommandRunner runs the cinematic and handles command cancellation that the
+ * use may initiate.
+ */
 export default class CommandRunner {
   constructor (commands) {
     this.commands = commands
@@ -8,6 +12,20 @@ export default class CommandRunner {
   /**
    * Runs the commands that are internally stored.
    * The commands must be extended from the `AbstractCommand` class.
+   *
+   * Commands are run with imported unique symbols, allowing protected methods to be run.
+   *
+   * A command is run with:
+   *
+   * ```js
+   * command[run]()
+   * ```
+   *
+   * and cancelled with:
+   *
+   * ```js
+   * command[cancel]()
+   * ```
    */
   async run () {
     for (const command of this.commands) {
