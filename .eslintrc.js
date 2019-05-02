a41323b6ea132f098f7bbbde6cb2cdfd0c0ad840
Joe Mifsud <joe@mifsud.me>
2019-05-02 16:40:48 -0400

    Removed unused globals from eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,11 +9,6 @@ module.exports = {
         'es6': true
     },
 
-    'globals': {
-        'Atomics': 'readonly',
-        'SharedArrayBuffer': 'readonly'
-    },
-
     'parserOptions': {
         'ecmaVersion': 2018,
         'sourceType': 'module'
