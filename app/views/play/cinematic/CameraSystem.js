ed4c913a544e0d3d9599378e6401220ab35ae4ec
Andrew Jakubowicz <spyr1014@gmail.com>
2019-05-01 17:35:48 -0700

    Clean up camera zoom command

--- app/views/play/cinematic/CameraSystem.js
+++ app/views/play/cinematic/CameraSystem.js
@@ -12,16 +12,14 @@ export class CameraSystem {
   /**
    * May return command to reposition camera.
    * @param {Shot} shot - the cinematic shot data.
+   * @returns {AbstractCommand[]} An array of commands to setup the shot.
    */
   parseSetupShot (shot) {
     const commands = []
     const cameraMove = getCamera(shot)
     if (cameraMove) {
       const { pos: { x, y }, zoom } = cameraMove
-      commands.push(new SyncFunction(() => {
-        console.log('camera zooming!')
-        this.camera.zoomTo({ x, y }, zoom, 0)
-      }))
+      commands.push(new SyncFunction(() => this.camera.zoomTo({ x, y }, zoom, 0)))
     }
     return commands
   }
