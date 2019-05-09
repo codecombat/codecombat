7b84ea3f26cfb637c88585cc3d075d0cbd9dae91
Joe Mifsud <joe@mifsud.me>
2019-05-08 19:34:28 -0700

    Add Vue based head tag management and integrate with Backbone (#5311)

---
+++ app/components/Root.vue
@@ -0,0 +1,17 @@
+<script>
+  import MetaManger from './common/MetaManager'
+
+  export default {
+    components: {
+      'meta-manager': MetaManger
+    }
+  }
+</script>
+
+<template>
+  <router-view>
+    <meta-manager>
+      <slot />
+    </meta-manager>
+  </router-view>
+</template>
---
+++ app/components/common/MetaManager.vue
@@ -0,0 +1,21 @@
+<script>
+  /**
+   * Renderless component that manages default head tag configuration for vue-meta.
+   */
+  export default {
+    metaInfo: function () {
+      return {
+        title: this.$t('common.default_title'),
+        titleTemplate: '%s | CodeCombat',
+
+        meta: [
+          { vmid: 'meta-description', name: 'description', content: this.$t('common.default_meta_description') }
+        ]
+      }
+    },
+
+    render () {
+      return this.$slots.default
+    }
+  }
+</script>
