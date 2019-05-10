3ecf9d5effaa768ab1840795936e0c46b7fa4445
Matt Lott <matt@codecombat.com>
2019-05-09 19:09:17 -0700

    StandardJSify webpack.base.config.js

--- webpack.base.config.js
+++ webpack.base.config.js
@@ -1,21 +1,21 @@
 // NOTE: Don't use this config by itself! It is just a parent for the dev and production configs.
 
-const _ = require('lodash');
-const path = require('path');
-const webpack = require('webpack');
-const CopyWebpackPlugin = require('copy-webpack-plugin');
-const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const _ = require('lodash')
+const path = require('path')
+const webpack = require('webpack')
+const CopyWebpackPlugin = require('copy-webpack-plugin')
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
 const glob = require('glob')
-require('coffee-script');
-require('coffee-script/register');
-const CompileStaticTemplatesPlugin = require('./compile-static-templates');
+require('coffee-script')
+require('coffee-script/register')
+const CompileStaticTemplatesPlugin = require('./compile-static-templates')
 const VueLoaderPlugin = require('vue-loader/lib/plugin')
 
-console.log("Starting Webpack...");
+console.log('Starting Webpack...')
 
 // Main webpack config
 module.exports = (env) => {
-  if (!env) env = {};
+  if (!env) env = {}
   return {
     context: path.resolve(__dirname),
     entry: {
@@ -27,10 +27,10 @@ module.exports = (env) => {
         './app/core/utils.coffee',
         './vendor/scripts/string_score.js',
         './bower_components/underscore.string',
-        './vendor/scripts/coffeescript.js',
+        './vendor/scripts/coffeescript.js'
       ]),
       lodash: 'lodash', // For worker_world
-      aether: './bower_components/aether/build/aether.js', // For worker_world
+      aether: './bower_components/aether/build/aether.js' // For worker_world
       // esper: './bower_components/esper.js/esper.js',
       // vendor: './app/vendor.js'
     },
@@ -38,15 +38,15 @@ module.exports = (env) => {
       filename: 'javascripts/[name].js', // TODO: Use chunkhash in layout.static.pug's script tags instead of GIT_SHA
       // chunkFilename is determined by build type
       path: path.resolve(__dirname, 'public'),
-      publicPath: '/', // Base URL path webpack tries to load other bundles from
+      publicPath: '/' // Base URL path webpack tries to load other bundles from
     },
     module: {
-      noParse: function (name){ // These are already built into commonjs bundles
+      noParse: function (name) { // These are already built into commonjs bundles
         return _.any([
           /vendor.*coffeescript/,
           /bower_components.*aether/,
           /bower_components.*jsondiffpatch.*\.js$/,
-          /fuzzaldrin/,
+          /fuzzaldrin/
         ], (regex) => { return regex.test(name) })
       },
       rules: [
@@ -55,11 +55,12 @@ module.exports = (env) => {
         { test: /\.js$/,
           exclude: /(node_modules|bower_components|vendor)/,
           use: [{
-            loader: 'babel-loader',
+            loader: 'babel-loader'
           }]
         },
-        { test: /\.coffee$/, use: [
-          { loader: 'coffee-loader' },
+        { test: /\.coffee$/,
+use: [
+          { loader: 'coffee-loader' }
         ] },
         { test: /\.pug$/,
           oneOf: [
@@ -79,20 +80,25 @@ module.exports = (env) => {
         },
         {
           oneOf: [
-            { test: /jquery-ui.*css$/, use: [ // So we can ignore the images it references that we are missing
+            { test: /jquery-ui.*css$/,
+use: [ // So we can ignore the images it references that we are missing
               { loader: 'style-loader' },
-              { loader: 'css-loader', options: { url: false } },
+              { loader: 'css-loader', options: { url: false } }
             ] },
-            { test: /\.css$/, use: [
+            { test: /\.css$/,
+use: [
               { loader: 'style-loader' },
-              { loader: 'css-loader' }, // TODO Webpack: Maybe use url:false here as well
-            ] },
+              { loader: 'css-loader' } // TODO Webpack: Maybe use url:false here as well
+            ] }
           ]
         },
-        { test: /\.sass$/, enforce: 'pre', use: [ // Allow importing * in app.sass
-          { loader: 'import-glob-loader' },
+        { test: /\.sass$/,
+enforce: 'pre',
+use: [ // Allow importing * in app.sass
+          { loader: 'import-glob-loader' }
         ] },
-        { test: /\.sass$/, use: ExtractTextPlugin.extract({
+        { test: /\.sass$/,
+use: ExtractTextPlugin.extract({
           fallback: 'style-loader',
           use: [
             { loader: 'css-loader' },
@@ -101,22 +107,23 @@ module.exports = (env) => {
               options: {
                 indentedSyntax: true
               }
-            },
+            }
           ]
         }) },
-        { test: /\.scss$/, use: [
+        { test: /\.scss$/,
+use: [
           { loader: 'vue-style-loader' },
           { loader: 'css-loader' },
-          { loader: 'sass-loader' },
-        ] },
-      ],
+          { loader: 'sass-loader' }
+        ] }
+      ]
     },
     resolve: {
       modules: [ // This section denotes what folders you can use as a root in a require statement
         path.resolve('./app'), // eg require('vendor.js') gets /app/vendor.js
         path.resolve('./app/assets'), // eg require('images/favicon.ico') gets /app/assets/images/favicon.ico
         path.resolve('./'), // Or you can use the full path /app/whatever
-        'node_modules'  // Or maybe require('foo') for the Node module "foo".
+        'node_modules' // Or maybe require('foo') for the Node module "foo".
       ],
       extensions: ['.web.coffee', '.web.js', '.coffee', '.js', '.jade', '.pug', '.sass', '.vue'],
       alias: { // Replace Backbone's underscore with lodash
@@ -126,12 +133,12 @@ module.exports = (env) => {
     node: {
       fs: 'empty',
       child_process: 'empty',
-      request: 'empty',
+      request: 'empty'
     },
     plugins: [
       new webpack.ProgressPlugin({ profile: false }), // Always show build progress
       new ExtractTextPlugin({ // Move CSS into external file
-        filename: 'stylesheets/[name].css',
+        filename: 'stylesheets/[name].css'
       }),
       new webpack.ProvidePlugin({ // So Bootstrap can use the global jQuery
         $: 'jquery',
@@ -140,39 +147,39 @@ module.exports = (env) => {
       new webpack.IgnorePlugin(/\/fonts\/bootstrap\/.*$/), // Ignore Bootstrap's fonts
       new webpack.IgnorePlugin(/^memwatch$/), // Just used by the headless client on the server side
       new webpack.IgnorePlugin(/.DS_Store$/),
-      
+
       // Enable IgnorePlugins for development to speed webpack
-      //new webpack.IgnorePlugin(/\!locale/),
-      //new webpack.IgnorePlugin(/\/admin\//),
-      //new webpack.IgnorePlugin(/\/artisan\//),
-      //new webpack.IgnorePlugin(/\/clans\//),
-      //new webpack.IgnorePlugin(/\/contribute\//),
-      //new webpack.IgnorePlugin(/\/courses\//),
-      //new webpack.IgnorePlugin(/\/editor\//),
-      //new webpack.IgnorePlugin(/\/ladder\//),
-      //new webpack.IgnorePlugin(/\/teachers\//),
-      //new webpack.IgnorePlugin(/\/play\//),
-      
+      // new webpack.IgnorePlugin(/\!locale/),
+      // new webpack.IgnorePlugin(/\/admin\//),
+      // new webpack.IgnorePlugin(/\/artisan\//),
+      // new webpack.IgnorePlugin(/\/clans\//),
+      // new webpack.IgnorePlugin(/\/contribute\//),
+      // new webpack.IgnorePlugin(/\/courses\//),
+      // new webpack.IgnorePlugin(/\/editor\//),
+      // new webpack.IgnorePlugin(/\/ladder\//),
+      // new webpack.IgnorePlugin(/\/teachers\//),
+      // new webpack.IgnorePlugin(/\/play\//),
+
       new CopyWebpackPlugin([
         // NOTE: If you add a static asset, consider updating ViewLoadTimer to track its loading.
         { // Static assets
           // Let's use file-loader down the line, but for now, just use URL references.
           from: 'app/assets',
           to: '.'
-        },{ // Ace
+        }, { // Ace
           context: 'bower_components/ace-builds/src-min-noconflict',
           from: '**/*',
           to: 'javascripts/ace'
-        },{ // Esper
+        }, { // Esper
           from: 'bower_components/esper.js/esper.js',
           to: 'javascripts/esper.js'
-        },{
+        }, {
           from: 'bower_components/esper.js/esper-modern.js',
           to: 'javascripts/esper.modern.js'
-        },
+        }
       ]),
       new CompileStaticTemplatesPlugin({
-        locals: {shaTag: process.env.GIT_SHA || 'dev', chinaInfra: process.env.COCO_CHINA_INFRASTRUCTURE || false},
+        locals: { shaTag: process.env.GIT_SHA || 'dev', chinaInfra: process.env.COCO_CHINA_INFRASTRUCTURE || false }
       }),
       new VueLoaderPlugin()
     ]
