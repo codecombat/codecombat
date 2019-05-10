b6cb28a96acdd7c227dca5293cc64f064c3f0a61
Joe Mifsud <joe@mifsud.me>
2019-05-09 19:38:48 -0700

    Make query param filtering work in backbone and vue

--- app/components/common/MetaManager.vue
+++ app/components/common/MetaManager.vue
@@ -3,10 +3,17 @@
    * Renderless component that manages default head tag configuration for vue-meta.
    */
   export default {
+    props: {
+      currentQueryParams: {
+        type: Object,
+        default: () => ({})
+      }
+    },
+
     metaInfo: function () {
       const links = []
       const utmKeys = Object
-        .keys(this.$router.currentRoute.query || {})
+        .keys(this.currentQueryParams || {})
         .filter(k => k.startsWith('utm_'))
 
       if (utmKeys.length > 0) {
