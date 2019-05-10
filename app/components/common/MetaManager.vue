e27802abc35d84daaa6a65c5af726a1b4e0ba87a
Joe Mifsud <joe@mifsud.me>
2019-05-09 19:05:21 -0700

    Remove utm_ management to shared meta manager

--- app/components/common/MetaManager.vue
+++ app/components/common/MetaManager.vue
@@ -4,13 +4,27 @@
    */
   export default {
     metaInfo: function () {
+      const links = []
+      const utmKeys = Object
+        .keys(this.$router.currentRoute.query || {})
+        .filter(k => k.startsWith('utm_'))
+
+      if (utmKeys.length > 0) {
+        const urlWithoutUtm = new URL(window.location.href)
+        utmKeys.forEach(k => urlWithoutUtm.searchParams.delete(k))
+
+        links.push({ vmid: 'rel-canonical', rel: 'canonical', href: urlWithoutUtm.toString() })
+      }
+
       return {
         title: this.$t('common.default_title'),
         titleTemplate: '%s | CodeCombat',
 
         meta: [
           { vmid: 'meta-description', name: 'description', content: this.$t('common.default_meta_description') }
-        ]
+        ],
+
+        link: links
       }
     },
 
