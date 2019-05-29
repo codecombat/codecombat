6c7a40730842093fb30eeae6ca8a19d8c2f5c18c
Andrew Jakubowicz <spyr1014@gmail.com>
2019-05-29 13:58:07 -0700

    Cinematic Canvas View Wrapper (#5366)

--- ozaria/site/components/cinematic/common/CinematicCanvas.vue
+++ ozaria/site/components/cinematic/common/CinematicCanvas.vue
@@ -48,6 +48,7 @@ export default {
       handlers: {
         onPlay: this.handlePlay,
         onPause: this.handleWait,
+        onCompletion: () => this.$emit('completed')
       }})
     window.addEventListener('keypress', this.handleKeyboardCancellation)
   },
