bec8884051c250a45dbeac556e484ff34b05bcd9
Andrew Jakubowicz <spyr1014@gmail.com>
2019-06-03 15:46:28 -0700

    Initial dev art chrome

---
+++ ozaria/site/components/cinematic/common/LayoutCenterCinematic.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="layout-center-cinematic">
+    <div class="middle">
+      <slot></slot>
+    </div>
+  </div>
+</template>
+
+<style lang="sass">
+// Method for centering div:
+// https://stackoverflow.com/questions/396145/how-to-vertically-center-a-div-for-all-browsers#6182661
+#layout-center-cinematic
+  display: table
+  position: absolute
+  top: 0
+  left: 0
+  height: 100%
+  width: 100%
+
+.middle
+  display: table-cell
+  vertical-align: middle
+
+</style>
+
+
