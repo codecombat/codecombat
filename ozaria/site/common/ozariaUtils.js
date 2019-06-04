fd2d41f3a67c31fcdbbfddb01e0983bf5f72d291
Shubhangi <shubhangigupta1910@gmail.com>
2019-06-04 12:57:32 -0700

    Refactoring and linting changes
    - Refactored OzariaUnitMap to ozaria folder
    - Updated route for Ozaria unit map
    - Re-arranged methods in ozariaUtils alphabetically
    - Vue linting in ozaria unit map and intro level pages

--- ozaria/site/common/ozariaUtils.js
+++ ozaria/site/common/ozariaUtils.js
@@ -2,58 +2,7 @@
  Utility functions for ozaria
  */
 
-/**
- * Creates a level status map using the list of level session objects.
- * @param {Object[]} sessions - The list of level session objects.
- * @returns {Object} - Object with key as the level original id, and value as complete/started.
- */
-export const getLevelStatusMap = (sessions) => {
-  const levelStatusMap = {}
-  for (const session of sessions) {
-    const levelOriginal = (session.level || session.get('level') || {}).original
-    if (levelOriginal && levelStatusMap[levelOriginal] !== 'complete') { // Don't overwrite a complete session with an incomplete one
-      if ((session.state || session.get('state') || {}).complete) {
-        levelStatusMap[levelOriginal] = 'complete'
-      } else {
-        levelStatusMap[levelOriginal] = 'started'
-      }
-    }
-  }
-  return levelStatusMap
-}
-
-/**
- * Creates a level data map using the list of level objects.
- * @param {Object[]} levels - The list of level objects.
- * @returns {Object} - Object with key as the level original id, and value as the object containing level's data.
- */
-export const getLevelDataMap = (levels) => {
-  const levelDataMap = {}
-  for (const level of levels) {
-    const levelOriginal = level.original || level.get('original')
-    if (levelOriginal) {
-      levelDataMap[levelOriginal] = level.attributes || level
-    }
-  }
-  return levelDataMap
-}
-
-/**
- * Gets the next level original ids for a given level
- * @param {Object} level - The level object.
- * @param {Object[]} level.nextLevels - The array of nextLevels for the given level.
- * @param {string} level.nextLevels[].levelOriginal - Original id of the next level.
- * @returns {string[]} - Array of next level 'Original' ids.
- */
-export const getNextLevelOriginalForLevel = (level) => {
-  const nextLevels = level.nextLevels
-  const nextLevelOriginals = []
-  if ((nextLevels || []).length > 0) {
-    nextLevelOriginals.push(nextLevels[0].levelOriginal) // assuming that there will be just one next level for ozaria v1 as of now.
-    // TODO: handle logic for 1FH capstone level
-  }
-  return nextLevelOriginals
-}
+export const defaultCodeLanguage = 'python'
 
 /**
  * Calculates all the next levels for a list of levels in a classroom/campaign based on the level sessions.
@@ -96,6 +45,32 @@ export const findNextLevelsBySession = (sessions, levels, levelStatusMap) => {
 }
 
 /**
+ * Returns the options to be used with ajv for json schema validation (used for draft-07 as of now)
+ */
+export const getAjvOptions = () => {
+  const options = {
+    unknownFormats: ['ace', 'hidden', 'i18n'] // list of formats unknown to ajv but need to be supported
+  }
+  return options // If we want to support both draft-04 and draft-06/07 schemas then add { schemaId: 'auto' } to options
+}
+
+/**
+ * Creates a level data map using the list of level objects.
+ * @param {Object[]} levels - The list of level objects.
+ * @returns {Object} - Object with key as the level original id, and value as the object containing level's data.
+ */
+export const getLevelDataMap = (levels) => {
+  const levelDataMap = {}
+  for (const level of levels) {
+    const levelOriginal = level.original || level.get('original')
+    if (levelOriginal) {
+      levelDataMap[levelOriginal] = level.attributes || level
+    }
+  }
+  return levelDataMap
+}
+
+/**
  * Gets the level data given a list of levels and list of level original ids.
  * @param {Object[]|Object} levels - The list of level objects, or an object with keys as level original id and value as level data.
  * @param {string[]} levelOriginals - The list of level original ids.
@@ -112,16 +87,24 @@ export const getLevelsDataByOriginals = (levels, levelOriginals) => {
 }
 
 /**
- * Returns the options to be used with ajv for json schema validation (used for draft-07 as of now)
+ * Creates a level status map using the list of level session objects.
+ * @param {Object[]} sessions - The list of level session objects.
+ * @returns {Object} - Object with key as the level original id, and value as complete/started.
  */
-export const getAjvOptions = () => {
-  const options = {
-    unknownFormats: ['ace', 'hidden', 'i18n'] // list of formats unknown to ajv but need to be supported
+export const getLevelStatusMap = (sessions) => {
+  const levelStatusMap = {}
+  for (const session of sessions) {
+    const levelOriginal = (session.level || session.get('level') || {}).original
+    if (levelOriginal && levelStatusMap[levelOriginal] !== 'complete') { // Don't overwrite a complete session with an incomplete one
+      if ((session.state || session.get('state') || {}).complete) {
+        levelStatusMap[levelOriginal] = 'complete'
+      } else {
+        levelStatusMap[levelOriginal] = 'started'
+      }
+    }
   }
-  return options // If we want to support both draft-04 and draft-06/07 schemas then add { schemaId: 'auto' } to options
+  return levelStatusMap
 }
-
-export const defaultCodeLanguage = 'python'
 
 /**
  * Constructs the next level link
@@ -155,4 +138,21 @@ export const getNextLevelLink = (levelData, options) => {
     link += `?campaignId=${encodeURIComponent(options.campaignId)}`
   }
   return link
+}
+
+/**
+ * Gets the next level original ids for a given level
+ * @param {Object} level - The level object.
+ * @param {Object[]} level.nextLevels - The array of nextLevels for the given level.
+ * @param {string} level.nextLevels[].levelOriginal - Original id of the next level.
+ * @returns {string[]} - Array of next level 'Original' ids.
+ */
+export const getNextLevelOriginalForLevel = (level) => {
+  const nextLevels = level.nextLevels
+  const nextLevelOriginals = []
+  if ((nextLevels || []).length > 0) {
+    nextLevelOriginals.push(nextLevels[0].levelOriginal) // assuming that there will be just one next level for ozaria v1 as of now.
+    // TODO: handle logic for 1FH capstone level
+  }
+  return nextLevelOriginals
 }
