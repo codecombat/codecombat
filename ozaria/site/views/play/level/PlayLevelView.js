858f3b3264318c4ed4c7d1f5d2359ea0465407d2
Matias Forbord <matias.forbord@gmail.com>
2019-06-13 08:08:17 +0800

    Set capstoneStage from query parameters

--- ozaria/site/views/play/level/PlayLevelView.js
+++ ozaria/site/views/play/level/PlayLevelView.js
@@ -91,15 +91,16 @@ class PlayLevelView extends RootView {
     this.levelID = levelID
 
     this.courseID = options.courseID || utils.getQueryVariable('course')
-    this.courseInstanceID =
-      options.courseInstanceID || utils.getQueryVariable('course-instance')
-
+    this.courseInstanceID = options.courseInstanceID || utils.getQueryVariable('course-instance')
     this.isEditorPreview = utils.getQueryVariable('dev')
-    this.sessionID =
-      utils.getQueryVariable('session') || this.options.sessionID
+    this.sessionID = utils.getQueryVariable('session') || this.options.sessionID
     this.observing = utils.getQueryVariable('observing')
-
     this.opponentSessionID = utils.getQueryVariable('opponent') || this.options.opponent
+    this.capstoneStage =
+      utils.getQueryVariable('capstoneStage') ||
+      utils.getQueryVariable('capstonestage') || // Case sensitive, so this is easier to use
+      this.options.capstoneStage
+
     this.gameUIState = new GameUIState()
 
     $('flying-focus').remove() // Causes problems, so yank it out for play view.
@@ -335,6 +336,12 @@ class PlayLevelView extends RootView {
 
   grabLevelLoaderData () {
     this.session = this.levelLoader.session
+    if (this.capstoneStage && me.isSessionless() && (me.isAdmin() || me.isTeacher())) {
+      const state = this.session.get('state') || {}
+      state.capstoneStage = this.capstoneStage
+      this.session.set('state', state)
+    }
+
     this.level = this.levelLoader.level
     store.commit('game/setLevel', this.level.attributes)
     if (this.level.isType('web-dev')) {
@@ -1154,7 +1161,7 @@ class PlayLevelView extends RootView {
 
     if (additionalGoals) {
       const state = this.session.get('state')
-      options.capstoneStage = state ? state.capstoneStage : undefined
+      options.capstoneStage = (state || {}).capstoneStage
       options.remainingGoals = this.goalManager.getRemainingGoals()
       options.levelSlug = this.level.get('slug')
 
