2aa171ecff29b36907d32fb8c8e132b713e5e065
Joe Mifsud <joe@mifsud.me>
2019-07-10 14:08:56 -0700

    Update request a quote experiment to 50 50 split

--- app/core/experiments.js
+++ app/core/experiments.js
@@ -1,7 +1,7 @@
 export function getRequestAQuoteGroup (user) {
   const bucket = user.get('testGroupNumber') % 10
 
-  if (bucket < 7) {
+  if (bucket < 5) {
     return 'request-a-quote-control'
   } else {
     return 'request-a-quote-header'
