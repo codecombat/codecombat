68b9fdc1130552d164711ed23eeef5f53f9e1a2b
Joe Mifsud <joe@mifsud.me>
2019-07-18 18:54:04 -0700

    Finalize draggable ordering ux

--- ozaria/site/components/interactive/PageInteractive/draggableOrdering/index.vue
+++ ozaria/site/components/interactive/PageInteractive/draggableOrdering/index.vue
@@ -204,30 +204,69 @@
   >
     <div class="draggable-ordering-content">
       <div class="draggable-ordering-lists">
+        <ul
+          class="slot-numbers"
+        >
+          <template
+            v-for="(label, index) in labels"
+          >
+            <li
+              :key="`${index}-numbers-content`"
+            >
+              <div>
+                {{ index + 1}}.
+              </div>
+            </li>
+            <li
+              :key="`${index}-numbers-spacer`"
+              class="spacer"
+            />
+          </template>
+        </ul>
+
         <swapping-vue-draggable
           :list="promptSlots"
           class="slots-container prompt-slots"
           tag="ul"
+          :options="{ draggable: '.prompt' }"
         >
-          <li
+          <template
             v-for="prompt in promptSlots"
-            :key="prompt.id"
-            :class="{ 'prompt': true, 'monospaced': (prompt.textStyleCode === true) }"
           >
-            {{ prompt.text }}
-          </li>
+            <li
+              :key="`${prompt.elementId}-slot-content`"
+              :class="{ 'prompt': true, 'monospaced': (prompt.textStyleCode === true) }"
+            >
+              <div>
+                {{ prompt.text }}
+              </div>
+            </li>
+            <li
+              :key="`${prompt.elementId}-slot-spacer`"
+              class="spacer"
+            />
+          </template>
         </swapping-vue-draggable>
 
         <ul
-          class="slots-container"
+          class="slots-container slot-labels"
         >
-          <li
+          <template
             v-for="(label, index) in labels"
-            :key="index"
-            :class="{ 'prompt-label': true, 'monospaced': (label.textStyleCode === true) }"
           >
-            {{ label.text }}
-          </li>
+            <li
+              :key="`${index}-label-content`"
+              :class="{ 'prompt-label': true, 'monospaced': (label.textStyleCode === true) }"
+            >
+              <div>
+                {{ label.text }}
+              </div>
+            </li>
+            <li
+              :key="`${index}-label-spacer`"
+              class="spacer"
+            />
+          </template>
         </ul>
       </div>
 
@@ -253,10 +292,10 @@
 
 <style lang="scss" scoped>
   .draggable-ordering-content {
-    padding: 20px;
-
     display: flex;
     flex-direction: column;
+
+    position: relative;
 
     .draggable-ordering-lists {
       flex-grow: 1;
@@ -276,59 +315,141 @@
   .submit {
     justify-content: flex-end;
 
-    margin: 0px auto;
-    margin-top: auto;
+    margin: 18.69px auto;
   }
 
-  ul.slots-container {
+  ul {
     height: 100%;
-    width: 50%;
-
-    max-width: 500px;
 
     padding: 0;
-
     margin: 0;
-    margin-right: 10px;
 
     display: flex;
     flex-direction: column;
 
     align-items: center;
-    justify-content: space-evenly;
 
     li {
-      margin-bottom: 15px;
-
       width: 100%;
+      height: 100%;
 
-      display: flex;
-
-      justify-content: center;
-      align-items: center;
-
-      text-align: center;
-      font-size: 15px;
+      list-style: none;
 
       min-height: 50px;
+
+      padding: 18.5px 22px;
+
+      color: #000000;
+      font-family: 'Open Sans', sans-serif;
+      font-size: 18px;
+      letter-spacing: 0.75px;
+      line-height: 24px;
+
+      div {
+        width: 100%;
+        height: 100%;
+
+        display: flex;
+
+        justify-content: center;
+        align-items: center;
+
+        text-align: center;
+        font-size: 15px;
+
+        padding: 13.5px
+      }
     }
 
+    li.spacer {
+      width: 100%;
+
+      border: 1px solid #D8D8D8;
+      box-shadow: 1px 1px 2px 0 rgba(155,155,155,0.51);
+      height: 2px;
+
+      min-height: 0;
+
+      margin: 0;
+      padding: 0;
+
+      &:last-of-type {
+        display: none;
+      }
+    }
+  }
+
+  ul.slot-numbers {
+    width: 10%;
+
+    li {
+      padding: 0;
+
+      div {
+        font-family: 'Arvo', serif;
+        font-size: 24px;
+        line-height: 32px;
+        letter-spacing: 0.48px;
+        font-weight: bold;
+        color: #4A4A4A;
+      }
+    }
+  }
+
+  ul.slots-container {
+    width: 45%;
+
     li.prompt {
-      border: 2px solid #acb9fa;
+      cursor: pointer;
+
+      div {
+        height: 100%;
+        background-color: #FFF;
+
+        background-image: url('/images/ozaria/interactives/drag_handle.svg');
+        background-repeat: no-repeat;
+        background-position: right 10px center;
+        background-size: 7px 11px;
+
+        border: 2px solid #acb9fa;
+      }
+
+      &.sortable-ghost {
+        div {
+          position: relative;
+
+          border: 2px solid #D8DBDB;
+
+          &::after {
+            content: ' ';
+
+            position: absolute;
+            top: 0;
+            right: 0;
+            bottom: 0;
+            left: 0;
+
+            background-color: #D8DBDB;
+          }
+        }
+      }
+
+      &.sortable-swap-highlight {
+        div {
+          border: 3px solid #5D73E1;
+        }
+      }
     }
 
     li.prompt-label {
-      background-color: #acb9fa;
-      border: 2px solid #acb9fa;
+      div {
+        background-color: #acb9fa;
+        border: 2px solid #acb9fa;
+      }
     }
 
     li.monospaced {
       font-family: 'Roboto Mono', monospace;
-    }
-
-    li.dragging-slot {
-      // TODO this doesn't work because vue-draggable also uses transforms for positioing
-      transform: rotate(5deg);
     }
   }
 </style>
