9c5e8c7c4093b9350ce5b75ff3051b3e03d4ce2f
Shubhangi <shubhangigupta1910@gmail.com>
2019-07-19 13:56:54 -0700

    Update intro modal padding

--- ozaria/site/views/play/level/modal/LevelIntroComponent.vue
+++ ozaria/site/views/play/level/modal/LevelIntroComponent.vue
@@ -92,83 +92,86 @@
     letter-spacing: 2.58px
     line-height: 62px
 
-.narrative-row
-  padding: 0px 35px
-  height: 246px
+.modal-body
+  padding: 20px
 
-  .vega-face
-    position: relative
-    top: 100px
-    left: -10px
-    width: 17%
+  .narrative-row
+    padding: 0px 35px
+    height: 246px
 
-    .vega-face-image
-      width: 100%
-      height: 100%
-
-  .narrative-div
-    position: relative
-    width: 92%
-    height: 100%
-    left: 40px
-    top: -97px
-
-    .narrative-speech-bubble
-      height: 189px
-      margin-left: 60px
-      background-color: #eaeeee
+    .vega-face
       position: relative
+      top: 100px
+      left: -10px
+      width: 17%
 
-      &::before
+      .vega-face-image
+        width: 100%
+        height: 100%
+
+    .narrative-div
+      position: relative
+      width: 92%
+      height: 100%
+      left: 40px
+      top: -97px
+
+      .narrative-speech-bubble
+        height: 189px
+        margin-left: 60px
+        background-color: #eaeeee
+        position: relative
+
+        &::before
+          position: absolute
+          left: -13px
+          top: 60px
+          content: ""
+          width: 50px
+          height: 40px
+          background: #eaeeee
+          transform: skew(-60deg)
+
+        .narrative-text
+          position: absolute
+          top: 0px
+          width: 442px
+          margin: 13px 15px
+          font-family: $title-font-style
+          font-size: 24px
+          letter-spacing: 0.48px
+          line-height: 32px
+          color: #0e4c60
+
+      .learning-goals-div
+        background-color: #ffffff
+        height: 57px
         position: absolute
-        left: -13px
-        top: 60px
-        content: ""
-        width: 50px
-        height: 40px
-        background: #eaeeee
-        transform: skew(-60deg)
+        width: 88.8%
+        left: 60px
+        padding: 13px 13px
 
-      .narrative-text
-        position: absolute
-        top: 0px
-        width: 442px
-        margin: 13px 15px
-        font-family: $title-font-style
-        font-size: 24px
-        letter-spacing: 0.48px
-        line-height: 32px
-        color: #0e4c60
+        .learning-goals-title
+          color: #1fbab4
+          font-family: $title-font-style
+          font-size: 17px
+          font-weight: bold
+          letter-spacing: 0.59px
+          line-height: 24px
 
-    .learning-goals-div
-      background-color: #ffffff
-      height: 57px
-      position: absolute
-      width: 88.8%
-      left: 60px
-      padding: 13px 13px
+        .learning-goals
+          color: #232323
+          font-family: $body-font-style
+          font-size: 15px
+          font-weight: 600
+          letter-spacing: 0.51px
+          line-height: 20px
 
-      .learning-goals-title
-        color: #1fbab4
-        font-family: $title-font-style
-        font-size: 17px
-        font-weight: bold
-        letter-spacing: 0.59px
-        line-height: 24px
+  .button-row
+    padding: 27px 0px 0px
 
-      .learning-goals
-        color: #232323
-        font-family: $body-font-style
-        font-size: 15px
-        font-weight: 600
-        letter-spacing: 0.51px
-        line-height: 20px
-
-.button-row
-  padding: 27px 0px 0px
-
-  .start-button
-    width: 182px
-    height: 45px
-    margin: 0px 220px
+    .start-button
+      width: 182px
+      height: 45px
+      margin: 0px 220px
 </style>
