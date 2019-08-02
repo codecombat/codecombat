4e0e3f6bafb6e59800fee600bf1173e278df5267
Andrew Jakubowicz <spyr1014@gmail.com>
2019-08-02 16:25:27 -0700

    Update case study pdfs

--- app/views/impact/PageImpact/index.vue
+++ app/views/impact/PageImpact/index.vue
@@ -58,7 +58,7 @@
             .col-xs-9
               h4.text-teal {{ $t('impact.partners_heading_1') }}
               p {{ $t('impact.partners_school_1') }}
-        a.btn.btn-lg.btn-primary(href="/images/pages/impact/pdf/CodeCombat_CaseStudy_PrestonMS.pdf" target="_blank") {{ $t('impact.read_full_story') }}
+        a.btn.btn-lg.btn-primary(href="/images/pages/impact/pdf/CodeCombat_CaseStudy_PrestonHS.pdf" target="_blank") {{ $t('impact.read_full_story') }}
       .col-md-4.partner-story-tile.col-xs-12
         .row
           .row
@@ -69,7 +69,7 @@
             .col-xs-9
               h4.text-teal {{ $t('impact.partners_heading_2') }}
               p {{ $t('impact.partners_school_2') }}
-        a.btn.btn-lg.btn-primary(href="/images/pages/impact/pdf/CodeCombat_CaseStudy_BobbyDukeMS.pdf" target="_blank") {{ $t('impact.read_full_story') }}
+        a.btn.btn-lg.btn-primary(href="/images/pages/impact/pdf/CodeCombat_CaseStudy_RiverRidgeHS.pdf" target="_blank") {{ $t('impact.read_full_story') }}
       .col-md-4.partner-story-tile.col-xs-12
         .row
           .row
@@ -80,7 +80,7 @@
             .col-xs-9
               h4.text-teal {{ $t('impact.partners_heading_3') }}
               p {{ $t('impact.partners_school_3') }}
-        a.btn.btn-lg.btn-primary(href="/images/pages/impact/pdf/CodeCombat_CaseStudy_BobbyDukeMS.pdf" target="_blank") {{ $t('impact.read_full_story') }}
+        a.btn.btn-lg.btn-primary(href="/images/pages/impact/pdf/CodeCombat_CaseStudy_RiverdaleHS.pdf" target="_blank") {{ $t('impact.read_full_story') }}
 
   //- html structure copied and modified from home page
   #teachers-love-codecombat.width-container.row.text-center
