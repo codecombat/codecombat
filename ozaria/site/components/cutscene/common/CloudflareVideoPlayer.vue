dccfacc3c73473f83411c3972cc0459fd84be575
Andrew Jakubowicz <spyr1014@gmail.com>
2019-11-06 13:37:02 -0800

    Cutscenes infrastructure for China support and Vimeo alternative (#214)

---
+++ ozaria/site/components/cutscene/common/CloudflareVideoPlayer.vue
@@ -0,0 +1,83 @@
+<script>
+import LayoutAspectRatioContainer from '../../common/LayoutAspectRatioContainer'
+import LayoutCenterContent from '../../common/LayoutCenterContent'
+
+export default {
+  components: {
+    LayoutAspectRatioContainer,
+    LayoutCenterContent
+  },
+
+  props: {
+    cutscene: {
+      type: Object,
+      required: true
+    },
+
+    cloudflareID: {
+      type: String,
+      required: true
+    }
+  },
+
+  data: () => ({
+    cloudflareCaptionUrl: null
+  }),
+
+  mounted () {
+    const cutscene = this.cutscene
+    /**
+     * Create and attach the script that streams the video.
+     */
+    const cloudflareScript = document.createElement('script')
+    cloudflareScript.setAttribute('src', `https://embed.videodelivery.net/embed/r4xu.fla9.latest.js?video=${this.cloudflareID}`)
+    cloudflareScript.defer = true
+    cloudflareScript.setAttribute('type', 'text/javascript')
+    cloudflareScript.setAttribute('data-cfasync', 'false')
+    document.body.appendChild(cloudflareScript)
+
+    /**
+     * Cloudflare video player only supports a single caption track.
+     * Thus pick out the specific caption file for the user based on preferred language.
+     */
+    let captionSrc = (cutscene.captions || {}).src
+    const localizedCaption = (((cutscene.i18n || {})[me.get('preferredLanguage')] || {}).captions || {}).src
+
+    if (localizedCaption) {
+      captionSrc = localizedCaption
+    }
+
+    if (captionSrc) {
+      this.cloudflareCaptionUrl = `/file/${captionSrc}`
+    }
+  },
+
+  updated () {
+    const video = this.$refs["cloudflareVideo"];
+    if (video) {
+      video.addEventListener('ended', () => this.onCompleted())
+    }
+  },
+
+  methods: {
+    onCompleted() {
+      this.$emit('completed')
+    },
+  }
+}
+</script>
+
+<template>
+  <layout-center-content>
+    <layout-aspect-ratio-container
+      class="cutscene-container"
+      :aspect-ratio="16 / 9"
+    >
+      <div class="cutscene">
+        <stream ref="cloudflareVideo" :src="cloudflareID" controls>
+          <track v-if="cloudflareCaptionUrl" kind="captions" :src="cloudflareCaptionUrl" default />
+        </stream>
+      </div>
+    </layout-aspect-ratio-container>
+  </layout-center-content>
+</template>
