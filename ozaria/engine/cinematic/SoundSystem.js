906d739ca7d88d0d001b6ea059100a1df20aa62b
Joe Mifsud <joe@mifsud.me>
2019-11-20 14:19:48 -0800

    Audio v2: Cinematic integration (#236)

--- ozaria/engine/cinematic/SoundSystem.js
+++ ozaria/engine/cinematic/SoundSystem.js
@@ -2,9 +2,10 @@
  * System for orchestrating the various sound commands.
  * An abstraction around the `howler.js` sound library.
  */
-import { Howl } from 'howler'
 import { getSetupMusic, getSoundEffects } from '../../../app/schemas/models/selectors/cinematic'
 import { SyncFunction, Sleep, SequentialCommands } from './commands/commands'
+
+import store from 'app/core/store'
 
 /**
  * @param {import('../../../app/schemas/models/selectors/cinematic').Sound} music
@@ -16,42 +17,8 @@ const getMusicKey = (music) => {
   }
   return key
 }
+
 export class SoundSystem {
-  constructor () {
-    // Map of key value pairs <string, Howl>
-    this.loadedSounds = new Map()
-    // Map of sounds currently playing <integer, Howl>
-    this.playingSound = new Map()
-  }
-
-  /**
-   * @param {string} key
-   * @param {import('../../../app/schemas/models/selectors/cinematic').Sound} music
-   */
-  preload (key, music) {
-    if (this.loadedSounds.has(key)) {
-      console.warn(`Sound with key: '${key}' is already loaded`)
-      return
-    }
-    if (!music) {
-      return
-    }
-    const sources = []
-    const extensions = []
-    for (const key of Object.keys(music)) {
-      sources.push(`/file/${music[key]}`)
-      extensions.push(key)
-    }
-    this.loadedSounds.set(key, new Howl({
-      src: sources,
-      format: extensions,
-      preload: true,
-      onloaderror: function (...args) {
-        console.log(`Failed to load sound`, args)
-      }
-    }))
-  }
-
   /**
    * System interface method that CinematicParser calls with
    * each shot.
@@ -61,10 +28,17 @@ export class SoundSystem {
   parseSetupShot (shot) {
     const commands = []
     const music = getSetupMusic(shot)
+
     if (music) {
-      const key = getMusicKey(music)
-      this.preload(key, music)
-      commands.push(new SyncFunction(() => { this.stopAllSounds(); this.playSound(key) }))
+      commands.push(new SyncFunction(() => {
+        store.dispatch('audio/stopAll')
+
+        store.dispatch('audio/playSound', {
+          track: 'background',
+          src: Object.values(music.files).map(f => `/file/${f}`),
+          loop: music.loop
+        })
+      }))
     }
     return commands
   }
@@ -78,51 +52,23 @@ export class SoundSystem {
   parseDialogNode (dialogNode) {
     const soundEffects = getSoundEffects(dialogNode) || []
     return soundEffects.map(({ sound, triggerStart }) => {
-      const key = getMusicKey(sound)
-      this.preload(key, sound)
+      const soundIdPromise = store.dispatch('audio/playSound', {
+        track: 'soundEffects',
+        autoplay: false,
+        src: Object.values(sound).map(f => `/file/${f}`)
+      })
+
       return new SequentialCommands([
         new Sleep(triggerStart),
-        new SyncFunction(() => this.playSound(key))
+        new SyncFunction(async () => {
+          const soundId = await soundIdPromise
+          store.dispatch('audio/playSound', soundId)
+        })
       ])
     })
   }
 
-  /**
-   * Finds and plays the sound associated to the given key, hooking the sound up
-   * to various event handlers and tracking the sound in the `this.playingSound`
-   * map.
-   * @param {Howl} key
-   */
-  playSound (key) {
-    const sound = this.loadedSounds.get(key)
-    if (!sound) {
-      console.warn(`Tried to play music '${key}' that doesn't exist.`)
-      return
-    }
-    const soundInstanceId = sound.play()
-
-    const cleanupSound = (isStop = false) => () => {
-      const sound = this.playingSound.get(soundInstanceId)
-      if (!sound) {
-        return
-      }
-      if (sound.loop() && !isStop) {
-        return
-      }
-      this.playingSound.delete(soundInstanceId)
-
-      sound.off('stop', cleanupSound(true), soundInstanceId)
-      sound.off('end', cleanupSound(), soundInstanceId)
-    }
-
-    sound.once('stop', cleanupSound(true), soundInstanceId)
-    sound.once('end', cleanupSound(), soundInstanceId)
-    this.playingSound.set(soundInstanceId, sound)
-  }
-
   stopAllSounds () {
-    for (const key of this.playingSound.keys()) {
-      this.playingSound.get(key).stop(key)
-    }
+    store.dispatch('audio/stopAll', { unload: true })
   }
 }
