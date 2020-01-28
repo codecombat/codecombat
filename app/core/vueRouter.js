5fbd350e82251067d9e3dd02d38fd8e5c59b10d3
Andrew Jakubowicz <spyr1014@gmail.com>
2020-01-28 14:59:37 -0800

    Search dialogue in cinematic editor feature (#317)

--- app/core/vueRouter.js
+++ app/core/vueRouter.js
@@ -8,6 +8,8 @@ import TeacherClassView from 'app/views/courses/TeacherClassView.vue'
 import TeacherStudentView from 'app/views/teachers/classes/TeacherStudentView.vue'
 
 import PageCinematicEditor from '../../ozaria/site/components/cinematic/PageCinematicEditor'
+import BaseCinematicList from '../../ozaria/site/components/cinematic/PageCinematicEditor/BaseCinematicList'
+
 import PageCutsceneEditorList from '../../ozaria/site/components/cutscene/PageCutsceneEditorList'
 import PageCutsceneEditor from '../../ozaria/site/components/cutscene/PageCutsceneEditor'
 import PageInteractiveEditor from '../../ozaria/site/components/interactive/PageInteractiveEditor'
@@ -24,9 +26,13 @@ export default function getVueRouter () {
 
       routes: [
         {
+          path: '/editor/cinematic',
+          component: BaseCinematicList
+        },
+        {
           // TODO: The cinematic editor route should use vue guards to check for admin access.
           // TODO: Once we have a base editor component, use the nested route structure.
-          path: '/editor/cinematic/:slug?',
+          path: '/editor/cinematic/:slug',
           component: PageCinematicEditor,
           props: true
         },
