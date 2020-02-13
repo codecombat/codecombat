f2dec84d4aac3f142e9d8e13061dd799ceec6f1c
jmif <joe@mifsud.me>
2020-02-13 15:53:39 -0500

    Split user queries into batches with max length 100 to fix timeouts

--- app/core/store/modules/schoolAdministrator.js
+++ app/core/store/modules/schoolAdministrator.js
@@ -30,18 +30,28 @@ export default {
 
       commit('toggleLoading', 'teachers')
 
-      return usersApi
-        .fetchByIds({
-          fetchByIds: administratedTeachers,
-          includeTrialRequests: true
-        })
-        .then(res =>  {
-          if (res) {
-            commit('addTeachers', res)
-          } else {
-            throw new Error('Unexpected response from teachers by ID API.')
-          }
-        })
+      const groups = Math.ceil(administratedTeachers.length / 100.0)
+      const userPromises = []
+      for (let i = 0; i < groups; i++) {
+        userPromises.push(
+          usersApi
+            .fetchByIds({
+              fetchByIds: administratedTeachers,
+              includeTrialRequests: true
+            })
+            .then(res => {
+              if (res) {
+                return res
+              } else {
+                throw new Error('Unexpected response from teachers by ID API.')
+              }
+            })
+        )
+      }
+
+      return Promise.all(userPromises)
+        .then(groupResults => groupResults.flat())
+        .then(combinedResults => commit('addTeachers', combinedResults))
         .catch((e) => noty({ text: 'Fetch teachers failure' + e, type: 'error' }))
         .finally(() => commit('toggleLoading', 'teachers'))
     },
