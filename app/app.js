29bd80971a09850595d54c521d5a371ecc49f014
LanceWu <Lance@wjllance-mbp.local>
2020-03-26 12:29:21 +0800

    remove google font china mirror(can't connect this mirror any more)

--- app/app.js
+++ app/app.js
@@ -6,11 +6,7 @@ import 'babel-polyfill';
 /**
  * Fonts dynamically imported based on infra location
  */
-if (window.features && window.features.chinaUx) {
-  import(/* webpackChunkName: "ChinaFont" */ 'app/styles/common/fontChina.sass');
-} else {
-  import(/* webpackChunkName: "UsFont" */ 'app/styles/common/fontUS.sass');
-}
+import(/* webpackChunkName: "UsFont" */ 'app/styles/common/fontUS.sass');
 
 require('app/vendor.js');
 
