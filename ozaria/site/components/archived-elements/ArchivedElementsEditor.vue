2a96f124880cf47d5629c2ee93068f2e7dce4e4a
Matias Forbord <matias.forbord@gmail.com>
2020-06-04 20:33:15 +0800

    Make it look okish

--- ozaria/site/components/archived-elements/ArchivedElementsEditor.vue
+++ ozaria/site/components/archived-elements/ArchivedElementsEditor.vue
@@ -1,52 +1,48 @@
 <template>
-  <div>
+  <div id="archived-elements">
     <div id="type-select">
-      <label>
-      </label>
-
-      <div class="form-group">
-        <label>Show archived</label>
-        <div class="radio-inline">
-          <label>
-            Only unarchived
-            <input
-                type="radio"
-                name="displayArchived"
-                value="none"
-                :checked="displayArchived === 'none'"
-                @change="setDisplayArchived('none')"
-            />
-          </label>
-          <label>
-            Both
-            <input
-                type="radio"
-                name="displayArchived"
-                value="both"
-                :checked="displayArchived === 'both'"
-                @change="setDisplayArchived('both')"
-            />
-          </label>
-          <label>
-            Only archived
-            <input
-                type="radio"
-                name="displayArchived"
-                value="only"
-                :checked="displayArchived === 'only'"
-                @change="setDisplayArchived('only')"
-            />
-          </label>
-        </div>
-      </div>
-
       <button @click="changeType('ThangType')" :class="elementType === 'ThangType' ? 'selected' : ''">ThangTypes</button>
       <button @click="changeType('Level')" :class="elementType === 'Level' ? 'selected' : ''">Levels</button>
       <button @click="changeType('LevelComponent')" :class="elementType === 'LevelComponent' ? 'selected' : ''">Components</button>
     </div>
+
     <div id="controls">
       <input id="search" placeholder="Search" v-model:value="searchingFor" v-on:keyup.enter="setSearchTerm(searchingFor)">
     </div>
+
+    <form class="radio-group">
+      <div class="form-group">
+        <label>Only unarchived </label>
+        <input
+            type="radio"
+            name="displayArchived"
+            value="none"
+            :checked="displayArchived === 'none'"
+            @change="setDisplayArchived('none')"
+        />
+      </div>
+      <div class="form-group">
+        <label>Both</label>
+        <input
+            type="radio"
+            name="displayArchived"
+            value="both"
+            :checked="displayArchived === 'both'"
+            @change="setDisplayArchived('both')"
+        />
+      </div>
+      <div class="form-group">
+        <label>Only archived</label>
+        <input
+            type="radio"
+            name="displayArchived"
+            value="only"
+            :checked="displayArchived === 'only'"
+            @change="setDisplayArchived('only')"
+        />
+      </div>
+    </form>
+
     <ArchiveSearchView
       :model="viewMap[elementType].model"
       :model-name="viewMap[elementType].modelName"
@@ -105,15 +101,30 @@
       changeType (elementType) {
         this.searchingFor = ''
         this.setElementType(elementType)
-      },
-      // updateDisplayArchived (displayArchived) {
-      //   this.setDisplayArchived(displayArchived)
-      // }
+      }
     }
   }
 </script>
 
 <style lang="sass" scoped>
+  .radio-group
+    float: left
+    padding: 5px
+
+  .form-group
+    display: flex
+    align-items: center
+    flex-direction: row
+    justify-content: space-between
+
+    label
+      margin-bottom: 0
+      font-weight: 400
+      font-size: 18px
+      letter-spacing: 0.2px
+      line-height: 24px
+      padding-right: 5px
+
   #type-select
     padding: 2% 25% 2% 25%
     display: flex
