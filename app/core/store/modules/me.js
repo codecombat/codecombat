c7ae8b491d794c6d9a3ae4a3b2c23fdb3cd39128
jmif <joe@mifsud.me>
2020-06-17 13:49:30 -0700

    feat(user): add premium sub getter to me vuex module

--- app/core/store/modules/me.js
+++ app/core/store/modules/me.js
@@ -10,6 +10,10 @@ export default {
   state: _.cloneDeep(emptyUser),
 
   getters: {
+    isInGodMode (state) {
+      return ((state != null ? state.permissions : undefined) || []).indexOf('godmode') > -1
+    },
+
     isAnonymous (state) { return state.anonymous === true },
 
     isStudent (state) {
@@ -60,6 +64,26 @@ export default {
 
     isSmokeTestUser (state) {
       return utils.isSmokeTestEmail(state.email)
+    },
+
+    hasSubscription (state) {
+      if (state.payPal && state.payPal.billingAgreementID) {
+        return true
+      }
+
+      if (state.stripe && (state.stripe.sponsorID || state.stripe.subscriptionID || state.stripe.free === true)) {
+        return true
+      }
+
+      if (state.stripe && typeof state.stripe.free === 'string') {
+        return new Date() < new Date(state.stripe.free)
+      }
+
+      return false
+    },
+
+    isPremium (state, getters) {
+      return getters.isAdmin || getters.hasSubscription || getters.isInGodMode
     }
   },
 
