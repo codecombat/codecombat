0de504a049aa575e2c0edb6688ec682aa60fe8db
Andrew Jakubowicz <spyr1014@gmail.com>
2020-10-26 18:34:35 -0700

    Complete the Ozaria Encouragement Modal

--- app/views/teachers/EducatorSignupOzariaEncouragementModal.vue
+++ app/views/teachers/EducatorSignupOzariaEncouragementModal.vue
@@ -37,8 +37,29 @@ export default Vue.extend({
         </div>
         <div class="modal-contents-container">
           <div class="ozaria-encourage-container">
-            <h2>Ozaria</h2>
-            <button>Try Ozari</button>
+            <img class="oz-logo" alt="Ozaria logo" src="/images/pages/home/logo_ozaria.png"/>
+            <div class="ozaria-row">
+              <img src="/images/ozaria/modal/encouragement-modal/Icon_StandardsAlignment.svg" />
+              <div>
+                <h4>Standards Aligned Core Curriculum</h4>
+                <p>Immersive, story based curriculum that meets all 6th-8th grade CSTA standards.</p>
+              </div>
+            </div>
+            <div class="ozaria-row">
+              <img src="/images/ozaria/modal/encouragement-modal/Icon_Turnkey.svg" />
+              <div>
+                <h4>Turnkey Lesson Plans</h4>
+                <p>In-depth presentations and worksheets for teachers to guide students through learning objectives.</p>
+              </div>
+            </div>
+            <div class="ozaria-row">
+              <img src="/images/ozaria/modal/encouragement-modal/Icon_NewTeacherDashboard.svg" />
+              <div>
+                <h4>Turnkey Lesson Plans</h4>
+                <p>All the actionable insights educators need at a glance, such as student progress and concept understanding.</p>
+              </div>
+            </div>
+            <a class="primary-oz-button" href="https://www.ozaria.com/sign-up/educator">Try Ozaria</a>
           </div>
           <div class="codecombat-continue-container">
             <img class="coco-logo" alt="CodeCombat logo" src="/images/pages/base/logo.png">
@@ -50,7 +71,7 @@ export default Vue.extend({
               <li>Educators who want to continue using CodeCombat in their class</li>
             </ul>
 
-            <button class="continue-codecombat">Continue with CodeCombat</button>
+            <button @click="close" class="continue-codecombat">Continue with CodeCombat</button>
           </div>
         </div>
       </div>
@@ -87,6 +108,12 @@ export default Vue.extend({
   position: absolute;
 }
 
+img.oz-logo {
+  height: 78.75px;
+  width: auto;
+  margin-bottom: 40px;
+}
+
 img.coco-logo {
   height: 50px;
   width: auto;
@@ -106,13 +133,70 @@ img.coco-logo {
   flex-basis: 2;
   flex: 2;
 
-  background-color: yellow;
-  border-radius: 6px 0 0 6px;
+  background: white;
+  background: url(/images/ozaria/modal/encouragement-modal/SpritLandReveal_Gradient.png) left bottom no-repeat, url(/images/ozaria/modal/encouragement-modal/Hero.png) left bottom no-repeat; /* Fallback */
+  background: url(/images/ozaria/modal/encouragement-modal/Hero.png) 22px bottom no-repeat, url(/images/ozaria/modal/encouragement-modal/SpritLandReveal_Gradient.png) left bottom no-repeat, linear-gradient(66.45deg, #FFFEF0 15.94%, #FFFDEA 29.45%, #FFF8CF 46.45%, #FDF7D7 76.99%, #FFFCE9 83.48%);
 
+  border-radius: 6px 0 0 6px;
   display: flex;
   flex-direction: column;
-  justify-content: center;
+  justify-content: space-between;
   align-items: center;
+  padding: 25px 0;
+
+}
+
+.ozaria-row {
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  width: 90%;
+  max-width: 520px;
+}
+
+.ozaria-row img {
+  width: 80px;
+  height: 80px;
+  margin-right: 30px;
+}
+
+.ozaria-row p {
+  font-family: Open Sans;
+  font-style: normal;
+  font-weight: 300;
+  font-size: 18px;
+  line-height: 25px;
+  color: #232323;
+}
+
+.ozaria-row h4 {
+  font-family: Open Sans;
+  font-style: normal;
+  font-weight: bold;
+  font-size: 24px;
+  line-height: 33px;
+  color: black;
+}
+
+a.primary-oz-button {
+  margin-top: 40px;
+
+  font-family: Open Sans;
+  font-style: normal;
+  font-weight: bold;
+  font-size: 18px;
+  line-height: 38px;
+
+  color: black;
+  text-transform: capitalize;
+  background-color: #1FBAB4;
+  border: 0;
+  padding: 5px 15px;
+  border-radius: 4px;
+  text-align: center;
+
+  min-width: 250px;
+  min-height: 50px;
 }
 
 .codecombat-continue-container {
@@ -146,6 +230,12 @@ img.coco-logo {
   font-size: 18px;
   line-height: 25px;
   color: white;
+
+  margin: 10px 0;
+}
+
+.codecombat-continue-container ul {
+  margin: 10px 0 40px;
 }
 
 .codecombat-continue-container button {
