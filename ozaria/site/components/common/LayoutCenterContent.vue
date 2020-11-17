579087f2beebecae25b93f6a4a87b0d430076b3d
Andrew Jakubowicz <spyr1014@gmail.com>
2020-11-17 14:01:20 -0800

    Handle portrait on ipad better (#654)
    * Pin portrait ipad to top of screen
    
    * Fix comment issue
    
    * Add a bit of padding to the top
    
    * Don't scale cinematic text as aggressively

--- ozaria/site/components/common/LayoutCenterContent.vue
+++ ozaria/site/components/common/LayoutCenterContent.vue
@@ -17,4 +17,10 @@
   align-items: center
   justify-content: center
 
+@media only screen and (min-device-width: 481px) and (max-device-width: 1024px) and (orientation:portrait)
+  // On ipad or smaller devices shifts view to top making space for the virtual keyboard.
+  #layout-center
+    justify-content: flex-start
+    padding-top: 30px
+
 </style>
