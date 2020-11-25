fb00fe1d7b15e65be5f7d9528875f933b77c4013
Matias Forbord <codeluggage@users.noreply.github.com>
2020-11-26 00:16:49 +0800

    Polish changes, new assets and animations (#6059)

--- app/views/landing-pages/parents/PageParentsJumbotron.vue
+++ app/views/landing-pages/parents/PageParentsJumbotron.vue
@@ -10,18 +10,50 @@ export default {
     onCtaClick () {
       this.$emit('cta-clicked')
     }
+  },
+  mounted () {
+    const animatingSvg = $('.animated-griffin')
+    const totalFrames = 20
+    const animationDuration = 1200
+    const timePerFrame = animationDuration / totalFrames
+    let timeWhenLastUpdate
+    let timeFromLastUpdate
+    let frameNumber = 1
+
+    function step (startTime) {
+      if (!timeWhenLastUpdate) {
+        timeWhenLastUpdate = startTime
+      }
+
+      timeFromLastUpdate = startTime - timeWhenLastUpdate
+
+      if (timeFromLastUpdate > timePerFrame) {
+        animatingSvg.attr('src', `/images/pages/parents/Griffin_and_Alejandro${frameNumber}.svg`)
+        timeWhenLastUpdate = startTime
+        frameNumber = (frameNumber >= totalFrames) ? 1 : frameNumber + 1
+      }
+
+      window.requestAnimationFrame(step)
+    }
+
+    step()
   }
 }
 </script>
 
 <template>
   <div class="top-jumbotron">
+    <img class="animated-griffin"
+         src="/images/pages/parents/Griffin_and_Alejandro1.svg"
+         alt="blinking eye animation"/>
     <div class="row">
       <div class="col-lg-12">
         <h1>Live Online Coding Classes</h1>
         <h1>Your Child Will Love</h1>
       </div>
     </div>
+
+
     <div class="row">
       <div class="col-lg-12">
         <button-schedule-free-class
@@ -49,7 +81,6 @@ export default {
     min-height: 628px;
 
     background-image: url(/images/pages/parents/parent_hero_image.png),
-      url(/images/pages/parents/griffin_alejandro_feedback.png),
       url(/images/pages/parents/image_cloud_3.svg),
       url(/images/pages/parents/image_cloud_4.svg),
       url(/images/pages/parents/image_cloud_3.svg),
@@ -59,18 +90,15 @@ export default {
       no-repeat,
       no-repeat,
       no-repeat,
-      no-repeat,
       no-repeat;
 
     background-position: bottom left 5%,
-      center right 5%,
       top 50px left 30px,
       top 35px right 280px,
       top 360px right 300px,
       bottom 52px right 475px;
 
     background-size: 500px,
-      350px,
       260px,
       90px,
       260px,
@@ -81,7 +109,6 @@ export default {
     .top-jumbotron {
       /* Moves images out of the way of the heading to keep it legible */
       background-position: bottom -74% left -5%,
-        top -50% right -30%,
         top 50px left 30px,
         top 35px right 280px,
         top 360px right 300px,
@@ -96,6 +123,12 @@ export default {
   .top-jumbotron .button {
     margin-top: 30px;
     max-width: 320px;
+  }
+
+  .animated-griffin {
+    position: absolute;
+    top: 30%;
+    right: 6%;
   }
 
 </style>
