5fa8ce81d9f2cb2a70b99f0afafa03158e3f987f
smallst_wsl <yuqiang_tong@163.com>
2020-12-02 08:18:45 +0800

    fix $.t to Vue.t

--- ozaria/site/components/teacher-dashboard/modals/ModalNewClass/index.vue
+++ ozaria/site/components/teacher-dashboard/modals/ModalNewClass/index.vue
@@ -28,7 +28,7 @@
         if (this.showInviteStudentsModal) {
           return 'Invite Students by Email'
         } else {
-          return $.t('teachers.add_a_class')
+          return Vue.t('teachers.add_a_class')
         }
       }
     },
