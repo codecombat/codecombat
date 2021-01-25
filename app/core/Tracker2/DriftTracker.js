17b4d14a69b837644bc74e888df9df135dc8c2d2
Matias Forbord <codeluggage@users.noreply.github.com>
2021-01-25 17:44:29 +0800

    Split contact modal routing (#6165)

--- app/core/Tracker2/DriftTracker.js
+++ app/core/Tracker2/DriftTracker.js
@@ -90,7 +90,7 @@ export default class DriftTracker extends BaseTracker {
 
     // Show when a message is received
     window.drift.on('message', (e) => {
-      if (!e.data.sidebarOpen) {
+      if (!e?.data?.sidebarOpen) {
         if (this.isChatEnabled) {
           this.driftApi.widget.show()
         }
