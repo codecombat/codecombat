db81d2fa5f299cfbcfaf3351e5f2dab40f98eb2d
Saurabh Patel <saurabh.finch@gmail.com>
2021-03-02 12:23:05 +0530

    add sort option of firstName and lastName in class (#717)
    * add sort option of firstName and lastName in class
    
    * Fix error if user is missing firstName/lastName fields
    
    Co-authored-by: Nick Winter <livelily@gmail.com>

--- ozaria/site/components/teacher-dashboard/BaseSingleClass/ViewAndManage.vue
+++ ozaria/site/components/teacher-dashboard/BaseSingleClass/ViewAndManage.vue
@@ -53,7 +53,7 @@
       <dropdown
         :label-text="$t('teacher.sort_by')"
         class="dropdowns"
-        :options="['Name', 'Progress (High to Low)', 'Progress (Low to High)']"
+        :options="['Last Name', 'First Name', 'Progress (High to Low)', 'Progress (Low to High)']"
 
         @change="changeSortBy"
       />
