364bc33953bee802990d7c480d9f0d1ec263f781
Andrew Jakubowicz <spyr1014@gmail.com>
2021-03-16 17:28:24 -0700

    PageClanSearch page done

--- app/views/admin/PageClanSearch.vue
+++ app/views/admin/PageClanSearch.vue
@@ -1,6 +1,5 @@
 <script>
 import { getClanBySchool, getClanByDistrict } from 'core/api/clans'
-import Clan from 'app/models/Clan'
 const algolia = require('core/services/algolia')
 
 export default {
@@ -8,62 +7,91 @@ export default {
     foundClans: []
   }),
   mounted() {
-      $(this.$refs.organizationControl).algolia_autocomplete({ hint: false }, [{
-        source: function (query, callback) {
-          algolia.schoolsIndex.search(query, { hitsPerPage: 5, aroundLatLngViaIP: false }).then((answer) =>
-            callback(answer.hits)
-          , function () {
-            return callback([])
-          })
-        },
-        displayKey: 'name',
-        templates: {
-          suggestion: function (suggestion) {
-            const hr = suggestion._highlightResult
-            return `<div class='school'> ${hr.name.value} </div>` +
-              `<div class='district'>${hr.district.value}, ` +
-                `<span>${hr.city?.value}, ${hr.state.value}</span></div>`
-          }
-        },
-      }]).on('autocomplete:selected', async (event, suggestion, dataset) => {
-        console.log(suggestion)
-        const districtId = suggestion.district_id
-        const schoolId = suggestion.id
+    if (!me.isAdmin()) {
+      alert('You must be logged in as an admin to use this page.')
+      return application.router.navigate('/', { trigger: true })
+    }
+    $(this.$refs.organizationControl).algolia_autocomplete({ hint: false }, [{
+      source: function (query, callback) {
+        algolia.schoolsIndex.search(query, { hitsPerPage: 5, aroundLatLngViaIP: false }).then((answer) =>
+          callback(answer.hits)
+        , function () {
+          return callback([])
+        })
+      },
+      displayKey: 'name',
+      templates: {
+        suggestion: function (suggestion) {
+          const hr = suggestion._highlightResult
+          return `<div class='school'> ${hr.name.value} </div>` +
+            `<div class='district'>${hr.district.value}, ` +
+              `<span>${hr.city?.value}, ${hr.state.value}</span></div>`
+        }
+      },
+    }]).on('autocomplete:selected', async (event, suggestion, dataset) => {
+      const districtId = suggestion.district_id
+      const schoolId = suggestion.id
 
-        try {
-          const school = await getClanBySchool(schoolId)
-          this.foundClans = [...this.foundClans, school]
-        } catch (e) {}
+      try {
+        const school = await getClanBySchool(schoolId)
+        this.foundClans = [...this.foundClans, school]
+      } catch (e) {
+        noty({
+          text: e.message,
+          layout: 'topCenter',
+          type: 'error',
+          timeout: 5000,
+          killer: false
+        })
+      }
 
-        try {
-          const district = await getClanByDistrict(districtId)
-          this.foundClans = [...this.foundClans, district]
-        } catch (e) {}
+      try {
+        const district = await getClanByDistrict(districtId)
+        this.foundClans = [...this.foundClans, district]
+      } catch (e) {
+        noty({
+          text: e.message,
+          layout: 'topCenter',
+          type: 'error',
+          timeout: 5000,
+          killer: false
+        })
+      }
+    });
+
+  $(this.$refs.districtControl).algolia_autocomplete({ hint: false }, [{
+    source: function (query, callback) {
+      algolia.schoolsIndex.search(query, { hitsPerPage: 5, aroundLatLngViaIP: false }).then((answer) =>
+        callback(answer.hits)
+      , function () {
+        return callback([])
       })
-    // TODO
-    // $("#district-control").algolia_autocomplete({hint: false}, [
-    //   source: (query, callback) ->
-    //     algolia.schoolsIndex.search(query, { hitsPerPage: 5, aroundLatLngViaIP: false }).then (answer) ->
-    //       callback answer.hits
-    //     , ->
-    //       callback []
-    //   displayKey: 'district',
-    //   templates:
-    //     suggestion: (suggestion) ->
-    //       hr = suggestion._highlightResult
-    //       "<div class='district'>#{hr.district.value}, " +
-    //         "<span>#{hr.city?.value}, #{hr.state.value}</span></div>"
-    // ]).on 'autocomplete:selected', (event, suggestion, dataset) =>
-    //   @$('input[name="organization"]').val '' # TODO: does not persist on tabbing: back to school, back to district
-    //   @$('input[name="city"]').val suggestion.city
-    //   @$('input[name="state"]').val suggestion.state
-    //   @$('select[name="state"]').val suggestion.state
-    //   @$('select[name="country"]').val 'United States'
-    //   @state.set({showUsaStateDropdown: true})
-    //   @state.set({stateValue: suggestion.state})
-    //   for key in DISTRICT_NCES_KEYS
-    //     @$('input[name="nces_' + key + '"]').val suggestion[key]
-    //   @onChangeForm()
+    },
+    displayKey: 'district',
+    templates: {
+      suggestion: function (suggestion) {
+        const hr = suggestion._highlightResult
+        return `<div class='district'>${hr.district.value}, ` +
+        `<span>${hr.city?.value}, ${hr.state.value}</span></div>`
+      }
+    }
+  }]).on('autocomplete:selected', async (event, suggestion, dataset) => {
+    console.log(suggestion)
+    const districtId = suggestion.district_id
+
+    try {
+      const district = await getClanByDistrict(districtId)
+      this.foundClans = [...this.foundClans, district]
+    } catch (e) {
+      noty({
+        text: e.message,
+        layout: 'topCenter',
+        type: 'error',
+        timeout: 5000,
+        killer: false
+      })
+    }
+    })
   }
 }
 </script>
@@ -72,6 +100,19 @@ export default {
   <div class="form-group">
     <label>School search</label>
     <input id="organization-control" class="form-control" ref="organizationControl"/>
+    <label>District search</label>
+    <input id="district-control" class="form-control" ref="districtControl"/>
+
+    <div>
+      <h2>List of found clans/teams</h2>
+      <ul>
+        <li
+          v-for="child in foundClans"
+          :key="child._id">
+          <span>{{child.displayName || child.name}} - </span><span>{{child.id}}</span><a :href="`/league/${child.slug}`">Esports Page</a>
+          </li>
+      </ul>
+    </div>
   </div>
 </template>
 
