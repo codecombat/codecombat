5308a0401ff70ac6811b042173024e97a4a3d276
Saurabh Patel <saurabh.finch@gmail.com>
2021-04-27 20:30:08 +0530

    add validation on licenseNum

--- app/views/payment/PaymentStudentLicensePurchaseView.vue
+++ app/views/payment/PaymentStudentLicensePurchaseView.vue
@@ -1,8 +1,12 @@
 <template>
-	<form style="width: 60%; padding-left: 40%">
+	<form class="purchase-form">
 		<div class="form-group">
 			<label for="licenseType">Select License</label>
-			<select class="form-control" id="licenseType">
+			<select
+					class="form-control"
+					id="licenseType"
+					@change="updateSelectedPrice"
+			>
 				<option
 						v-for="price in priceData"
 						:value="price.id"
@@ -14,9 +18,20 @@
 		</div>
 		<div class="form-group">
 			<label for="licenseNum">Number of Licenses</label>
-			<input type="text" class="form-control" id="licenseNum">
+			<input type="text" class="form-control" id="licenseNum" @keydown="updateLicenseNum" @keyup="updateLicenseNum">
+			<p v-if="licenseNum && !errMsg">Total price: {{totalPrice}}</p>
+			<p class="error">{{errMsg}}</p>
 		</div>
-		<button type="submit" class="btn btn-primary">Purchase Now</button>
+		<div class="form-group">
+			<button
+					type="submit"
+					class="btn btn-primary btn-lg purchase-btn"
+					:class="licenseNum && !errMsg ? '' : 'disabled'"
+					@click="onPurchaseNow"
+			>
+				Purchase Now
+			</button>
+		</div>
 	</form>
 </template>
 
@@ -26,12 +41,50 @@ export default {
 	props: {
 		priceData: Array,
 	},
+	data() {
+		return {
+			licenseNum: 0,
+			selectedPrice: this.priceData[0].id,
+			errMsg: '',
+		}
+	},
 	methods: {
 		getCurrency(price) {
 			return price.currency === 'usd' ? '$' : price.currency;
 		},
 		getUnitPrice(price) {
 			return price.unit_amount / 100;
+		},
+		updateSelectedPrice(e) {
+			this.selectedPrice = e.target.value;
+		},
+		updateLicenseNum(e) {
+			this.errMsg = '';
+			const licenseNum = parseInt(e.target.value);
+			if (isNaN(licenseNum)) {
+				this.errMsg = 'Invalid number';
+				return
+			}
+			const price = this.getSelectedPrice();
+			const licenseCap = price.metadata.licenseCap
+			if (licenseNum > licenseCap) {
+				this.errMsg = `Sorry, you cannot purchase more than ${licenseCap} licenses`;
+				return;
+			}
+			this.licenseNum = licenseNum;
+		},
+		getSelectedPrice() {
+			return this.priceData.find((p) => p.id === this.selectedPrice)
+		},
+		onPurchaseNow(e) {
+			e.preventDefault();
+			console.log('redirect to stripe');
+		}
+	},
+	computed: {
+		totalPrice() {
+			const price = this.getSelectedPrice();
+			return this.getUnitPrice(price) * this.licenseNum
 		},
 	}
 }
