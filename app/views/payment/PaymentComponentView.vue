be0e17960bf1385d71ed3445bc7b55fff3bbd104
Saurabh Patel <saurabh.finch@gmail.com>
2021-04-27 06:52:40 +0530

    integrate vuex store for payment group

--- app/views/payment/PaymentComponentView.vue
+++ app/views/payment/PaymentComponentView.vue
@@ -1,12 +1,24 @@
 <template>
   <div>
-    <h2>Hello</h2>
+		<h2 v-if="loading">Loading</h2>
+		<h2 v-else>{{paymentGroup._id}}</h2>
   </div>
 </template>
 
 <script>
 export default {
-name: "PaymentComponentView"
+	name: "PaymentComponentView",
+	created() {
+		this.$store.dispatch('paymentGroups/fetch', this.$route.params.slug);
+	},
+	computed: {
+		paymentGroup() {
+			return this.$store.getters['paymentGroups/paymentGroup'];
+		},
+		loading() {
+			return this.$store.getters['paymentGroups/loading'];
+		}
+	}
 }
 </script>
 
