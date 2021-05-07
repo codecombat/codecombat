fa59acd3363f9898b50d0b84259063a0c605afd9
Saurabh Patel <saurabh.finch@gmail.com>
2021-05-07 07:45:53 +0530

    add buy now button in online-classes page

--- app/views/payment/PaymentOnlineClassesView.vue
+++ app/views/payment/PaymentOnlineClassesView.vue
@@ -9,6 +9,17 @@
 				<payment-online-classes-plans-view
 						:price-data="priceData"
 					/>
+				<div class="offer-view text-center">
+					<h3>OFFER: Get extra 15% off on purchase of sibling accounts</h3>
+					<p>Select for more than one student after clicking Buy Now to automatically get 15% discount</p>
+				</div>
+			</div>
+		</div>
+		<div class="buy-now-view text-center">
+			<button type="button" class="btn btn-success btn-lg" @click="">Buy Now</button>
+			<div class="info-view">
+				<p>*All plans are automatically renewed at the same level and billing cycle unless otherwise changed or cancelled.</p>
+				<p>**Your purchase is 100% risk free within the first 30 days. If for any reason you decide not to continue, simply <a>Contact Us</a> within 30 days of purchase and we will promptly refund 100% of your payment, no questions asked.</p>
 			</div>
 		</div>
 	</div>
@@ -39,13 +50,27 @@ export default {
 	background-color: aliceblue;
 }
 .header {
-	padding-bottom: 25px;
+	padding-bottom: 20px;
 	padding-top: 10px;
 	h2 {
 		font-weight: bolder;
 	}
 	h4 {
 		font-weight: bold;
+	}
+}
+.buy-now-view {
+	padding-top: 10px;
+
+	.info-view {
+		padding-top: 5px;
+		padding-left: 25%;
+		padding-right: 25%;
+		font-size: small;
+
+		p {
+			margin: 0;
+		}
 	}
 }
 </style>
