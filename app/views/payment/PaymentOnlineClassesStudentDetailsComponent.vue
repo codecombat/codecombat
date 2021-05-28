6b3ee56e847f387d682749b96dd3b0342b61f2d3
Saurabh Patel <saurabh.finch@gmail.com>
2021-05-28 12:36:38 +0530

    add class level and programming lang in student details

--- app/views/payment/PaymentOnlineClassesStudentDetailsComponent.vue
+++ app/views/payment/PaymentOnlineClassesStudentDetailsComponent.vue
@@ -18,8 +18,42 @@
 			<div class="form-group">
 				<label :for="`student-email-${index}`">Email</label>
 				<input type="text" class="form-control" :id="`student-email-${index}`" placeholder="Enter Email" @keydown="updateEmail($event, index)" @keyup="updateEmail($event, index)" />
-				<div class="small-tip">*Enter student's CodeCombat account email if they have an account on CodeCombat</div>
+				<div class="small-tip">*Enter student's CodeCombat account email if they have created an account</div>
 			</div>
+      <div class="form-group">
+        <label :for="`levels-${index}`">Class Level</label>
+        <select
+            class="form-control"
+            id="`levels-${index}`"
+            @change="updateClassLevel($event, index)"
+        >
+          <option value="" selected disabled>Select level for the class</option>
+          <option
+              v-for="level in getClassLevels()"
+              :key="level"
+              :value="level"
+          >
+            {{level}}
+          </option>
+        </select>
+      </div>
+      <div class="form-group">
+        <label :for="`language-${index}`">Programming Language</label>
+        <select
+            class="form-control"
+            id="`language-${index}`"
+            @change="updateLanguage($event, index)"
+        >
+          <option value="" selected disabled>Select desired language for the class</option>
+          <option
+              v-for="lang in getLanguages()"
+              :key="lang"
+              :value="lang"
+          >
+            {{lang}}
+          </option>
+        </select>
+      </div>
 		</div>
 		<p class="error">{{errorMsg}}</p>
 	</div>
@@ -60,6 +94,19 @@ export default {
 			this.students[index - 1] = student;
 			this.updateStudentDetails();
 		},
+    updateClassLevel(e, index) {
+		  const student = this.getStudent(index);
+		  student.classLevel = e.target.value;
+		  this.updateStudentDetails();
+    },
+    updateLanguage(e, index) {
+      const student = this.getStudent(index);
+      student.programmingLanguage = e.target.value;
+      this.updateStudentDetails();
+    },
+    getStudent(index) {
+		  return this.students[index - 1];
+    },
 		updateStudentDetails() {
 			// validate all data is filled properly
 			let err = null;
@@ -86,6 +133,12 @@ export default {
 			const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 			return re.test(String(email).toLowerCase());
 		},
+    getClassLevels() {
+		  return ['Beginner', 'Intermediate', 'Advanced'];
+    },
+    getLanguages() {
+		  return ['JavaScript', 'Python'];
+    },
 	},
 }
 </script>
@@ -106,7 +159,7 @@ export default {
 	color: red;
 }
 .small-tip {
-	font-size: x-small;
+	font-size: small;
 	color: grey;
 }
 </style>
