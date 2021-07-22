c0862b58de9b22e1ec4a119cf371938450e4d073
Saurabh Patel <saurabh.finch@gmail.com>
2021-07-22 14:51:35 +0530

    payment UI changes for classroom and schools (#6347)
    * refactor student licenses top to head component
    
    * move student UI logic to its own view
    
    * add classroom and district tab heads
    
    * add classrooms and districts tab body
    
    * add small classroom payment body view
    
    * integrate buy now with payment-session
    
    * add minimum license purchase option

---
+++ app/views/payment/PaymentStudentLicensesComponent.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <template v-if="this.paymentGroup.subType === 'classroom-and-school-district'">
+      <payment-student-license-classroom-district-view
+        :price-info="paymentGroup.priceData[0]"
+        :payment-group-id="paymentGroup._id"
+      />
+    </template>
+    <template v-else>
+      <payment-student-licenses-view
+        :price-data="paymentGroup.priceData"
+        :payment-group-id="paymentGroup._id"
+      />
+    </template>
+  </div>
+</template>
+
+<script>
+import PaymentStudentLicensesView from './PaymentStudentLicensesView'
+import PaymentStudentLicenseClassroomDistrictView from './PaymentStudentLicenseClassroomDistrictView'
+export default {
+  name: "PaymentStudentLicensesComponent",
+  components: {
+    PaymentStudentLicensesView,
+    PaymentStudentLicenseClassroomDistrictView,
+  },
+  props: {
+    paymentGroup: {
+      type: Object,
+      required: true,
+    },
+  },
+}
+</script>
+
+<style scoped>
+
+</style>
