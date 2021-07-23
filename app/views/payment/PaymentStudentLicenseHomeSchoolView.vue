a1cf76f63164511f1bba8b0fff43fed59861feca
Saurabh Patel <saurabh.finch@gmail.com>
2021-07-23 11:55:09 +0530

    add home school UI (#6354)
    * add home school UI
    refactor small classroom to purchase component for re-use
    
    * refactor to add box-shadow
    
    * add online-classes widget
    
    * fix alignment by using vertical-align: top
    
    * move online-classes info to its own cmpt
    
    * fix alignment in school-district view

---
+++ app/views/payment/PaymentStudentLicenseHomeSchoolView.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <payment-student-license-head-component
+      :i18n-heading-name="priceInfo.metadata.i18nName"
+    />
+    <payment-student-license-home-school-body-component
+      :price-info="priceInfo"
+      :payment-group-id="paymentGroupId"
+    />
+  </div>
+</template>
+
+<script>
+import PaymentStudentLicenseHeadComponent from "./PaymentStudentLicenseHeadComponent";
+import PaymentStudentLicenseHomeSchoolBodyComponent from "./PaymentStudentLicenseHomeSchoolBodyComponent";
+export default {
+  name: "PaymentStudentLicenseHomeSchoolView",
+  components: {
+    PaymentStudentLicenseHeadComponent,
+    PaymentStudentLicenseHomeSchoolBodyComponent,
+  },
+  props: {
+    priceInfo: {
+      type: Object,
+      required: true,
+    },
+    paymentGroupId: {
+      type: String,
+      required: true,
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
