c5220ed3b173cbeb34ff1e6eb566dc09072cfb01
Saurabh Patel <saurabh.finch@gmail.com>
2021-07-23 11:32:37 +0530

    move online-classes info to its own cmpt

--- app/views/payment/PaymentStudentLicenseHomeSchoolBodyComponent.vue
+++ app/views/payment/PaymentStudentLicenseHomeSchoolBodyComponent.vue
@@ -20,15 +20,7 @@
           />
         </div>
         <div class="online-classes-widget same-row">
-          <div class="text-center learning">
-            <img src="/images/pages/parents/personal_learning.png" class="learning-image" />
-          </div>
-          <div class="learning-text">
-            <p class="bold">Online Classes Available</p>
-            <p class="small">Combining the power of gameplay and personalized instruction, our live online classes deliver an experience your student will love.
-            Our dedicated instructors offer individualized attention with a structured curriculum that teacher real Python and JavaScript. Try a 60 min trial class for free!</p>
-          </div>
-          <button type="button" class="btn btn-default more-info-btn" @click="moreInfo">More info</button>
+          <payment-student-licenses-online-classes-component />
         </div>
       </div>
     </div>
@@ -37,10 +29,12 @@
 
 <script>
 import PaymentStudentLicensePurchaseComponent from "./PaymentStudentLicensePurchaseComponent";
+import PaymentStudentLicensesOnlineClassesComponent from "./PaymentStudentLicensesOnlineClassesComponent";
 export default {
   name: "PaymentStudentLicenseHomeSchoolBodyComponent",
   components: {
     PaymentStudentLicensePurchaseComponent,
+    PaymentStudentLicensesOnlineClassesComponent,
   },
   data() {
     return {
@@ -60,11 +54,6 @@ export default {
       type: String,
       required: true,
     }
-  },
-  methods: {
-    moreInfo() {
-      window.open('https://codecombat.com/parents', '_blank')
-    },
   },
 }
 </script>
@@ -107,30 +96,6 @@ export default {
   background-color: white;
   vertical-align: top;
   padding: 5px 5px 15px;
-}
-.learning {
-  width: 60%;
-  display: table-cell;
-}
-.learning-image {
-  max-width: 100%;
-  max-height: 100%;
-  width: 65%;
-}
-.small {
-  font-size: small;
-  color: grey;
-  margin-bottom: 5px;
-}
-.bold {
-  font-weight: bold;
-}
-.learning-text {
-  padding: 10px;
-}
-.more-info-btn {
-  font-weight: bold;
-  border: 1px solid black;
 }
 .body-content {
   padding-top: 20px;
---
+++ app/views/payment/PaymentStudentLicensesOnlineClassesComponent.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="online-classes">
+    <div class="text-center learning">
+      <img src="/images/pages/parents/personal_learning.png" class="learning-image" />
+    </div>
+    <div class="learning-text">
+      <p class="bold">Online Classes Available</p>
+      <p class="small">Combining the power of gameplay and personalized instruction, our live online classes deliver an experience your student will love.
+        Our dedicated instructors offer individualized attention with a structured curriculum that teacher real Python and JavaScript. Try a 60 min trial class for free!</p>
+    </div>
+    <button type="button" class="btn btn-default more-info-btn" @click="moreInfo">More info</button>
+  </div>
+</template>
+
+<script>
+// online classes info shown in student classes payment page
+export default {
+  name: "PaymentStudentLicensesOnlineClassesComponent",
+  methods: {
+    moreInfo() {
+      window.open('https://codecombat.com/parents', '_blank')
+    },
+  }
+}
+</script>
+
+<style scoped lang="scss">
+.learning {
+  width: 60%;
+  display: table-cell;
+}
+.learning-image {
+  max-width: 100%;
+  max-height: 100%;
+  width: 65%;
+}
+.small {
+  font-size: small;
+  color: grey;
+  margin-bottom: 5px;
+}
+.bold {
+  font-weight: bold;
+}
+.learning-text {
+  padding: 10px;
+}
+.more-info-btn {
+  font-weight: bold;
+  border: 1px solid black;
+}
+</style>
