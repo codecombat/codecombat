a942bdda9229ba7c70d538417b453fddaaf1856f
Saurabh Patel <saurabh.finch@gmail.com>
2022-05-05 14:18:16 +0530

    reindent file

--- app/core/store/modules/paymentGroups.js
+++ app/core/store/modules/paymentGroups.js
@@ -1,43 +1,43 @@
 import { getPaymentGroup } from '../../api/payment-group'
 
 export default {
-	namespaced: true,
-	state: {
-		loading: true,
-		paymentGroup: {}
-	},
-	mutations: {
-		setPaymentGroup(state, paymentGroup) {
-			state.paymentGroup = { ...paymentGroup };
-		},
-		setLoading(state, loading) {
-			state.loading = loading;
-		},
-	},
-	getters: {
-		paymentGroup(state) {
-			return state.paymentGroup;
-		},
-		loading(state) {
-			return state.loading;
-		},
-	},
-	actions: {
-		async fetch({ commit,state }, slug) {
-			commit('setLoading', true)
-			if (state.paymentGroup.slug === slug) {
-				commit('setLoading', false)
-				return
-			}
-			let paymentGroup;
-			try {
-				paymentGroup = await getPaymentGroup(slug);
-			} catch (err) {
-				console.error('GET paymentGroup failed', err);
-				return
-			}
-			commit('setPaymentGroup', paymentGroup.data);
-			commit('setLoading', false);
-		}
-	}
+  namespaced: true,
+  state: {
+    loading: true,
+    paymentGroup: {}
+  },
+  mutations: {
+    setPaymentGroup(state, paymentGroup) {
+      state.paymentGroup = { ...paymentGroup };
+    },
+    setLoading(state, loading) {
+      state.loading = loading;
+    },
+  },
+  getters: {
+    paymentGroup(state) {
+      return state.paymentGroup;
+    },
+    loading(state) {
+      return state.loading;
+    },
+  },
+  actions: {
+    async fetch({ commit,state }, slug) {
+      commit('setLoading', true)
+      if (state.paymentGroup.slug === slug) {
+        commit('setLoading', false)
+        return
+      }
+      let paymentGroup;
+      try {
+        paymentGroup = await getPaymentGroup(slug);
+      } catch (err) {
+        console.error('GET paymentGroup failed', err);
+        return
+      }
+      commit('setPaymentGroup', paymentGroup.data);
+      commit('setLoading', false);
+    }
+  }
 }
