4e1e7997e9a25131dcd8e21f3406e4dbd6302371
Adam Kecskes <adam.k@powerednow.com>
2022-06-10 09:30:26 +0200

    /partners page implementation

--- app/views/partners/PagePartners/index.vue
+++ app/views/partners/PagePartners/index.vue
@@ -1,28 +1,31 @@
 <template lang="pug">
 .container-fluid#partners-page
-  .width-container.text-center.row
-    .col-lg-12
-      h1.text-h1.text-navy(data-i18n="partners.headline")
-  .width-container.text-center.row
-    .col-lg-12
-      a.btn.btn-primary.btn-lg(href="https://form.typeform.com/to/loZIh0I9" target="_blank") {{ $t('general.contact_us') }}
-  .width-container.text-center.row
-    .col-lg-12
-      h5.text-h5.text-teal(data-i18n="partners.below_headline_blurb")
+  .header
+    .width-container.text-center.row
+      .col-lg-12
+        h1.text-h1.text-navy(data-i18n="partners.headline")
 
-  .width-container.row.justify-content-center
-    .col-sm-offset-1.col-sm-6.col-xs-12
+    .width-container.text-center.row
+      .col-lg-12
+        a.btn.btn-primary.btn-lg(href="https://form.typeform.com/to/loZIh0I9" target="_blank") {{ $t('general.contact_us') }}
+
+    .width-container.text-center.row
+      .col-lg-12
+        h5.text-h5.text-teal(data-i18n="partners.below_headline_blurb")
+
+  .width-container.row.image-row
+    .col-sm-9
       p.text-navy
         strong(data-i18n="partners.coding_title")
         | &nbsp;
         span(data-i18n="partners.coding_blurb")
-    .col-sm-3.col-xs-8.col-xs-offset-2.col-sm-offset-0
+    .col-sm-3
       img.img-responsive(src="/images/pages/partners/coding.jpg")
 
-  .width-container.row.justify-content-center
-    .col-sm-offset-1.col-sm-3.col-xs-8.col-xs-offset-2
+  .width-container.row.image-row
+    .col-sm-3
       img.img-responsive(src="/images/pages/partners/powered-by-hyper.png")
-    .col-sm-6
+    .col-sm-9
       p.text-navy
         strong(data-i18n="partners.esports_title")
         | &nbsp;
@@ -43,12 +46,30 @@
         | &nbsp;
         span(data-i18n="[html]partners.equity_social_blurb" :data-i18n-options="`${JSON.stringify(i18nData)}`")
 
-  .row.width-container
-    .col-md-4.col-md-offset-1.col-sm-12.teacher-quote
-      img(src="/images/pages/home/teal_quote.png")
+  .width-container.row
+    .col-lg-12
+      p.text-navy
+        span(data-i18n="[html]partners.latin_superstar_blurb" :data-i18n-options="`${JSON.stringify(i18nData)}`")
+
+  .row.width-container.image-row
+    .col-md-3.col-md-offset-3.col-sm-12.teacher-quote.col-sm-offset-0
       h3.text-h3.text-navy(data-i18n="partners.eugenio_derbez_blurb")
-    .col-md-6.col-sm-12
+    .col-md-3.col-sm-12
       img.img-responsive(src="/images/pages/partners/Eugenio-Derbez.png" alt="Eugenio Derbez")
+
+  .width-container.row
+    .col-sm-12
+      p.text-navy
+        span(data-i18n="[html]partners.seek_relationships_blurb" :data-i18n-options="`${JSON.stringify(i18nData)}`")
+
+  .width-container.row.image-row
+    .col-sm-3
+      img.img-responsive(src="/images/pages/partners/corporate.png")
+    .col-sm-9
+      p.text-navy
+        strong(data-i18n="partners.corporate_team_building_title")
+        | &nbsp;
+        span(data-i18n="[html]partners.corporate_team_building_blurb" :data-i18n-options="`${JSON.stringify(i18nData)}`")
 
 
   .width-container.row
@@ -70,6 +91,8 @@ export default Vue.extend({
         respawn: `<a href='/league/#winners' target='_blank'>${$.i18n.t('partners.respawn')}</a>`,
         forty_languages: `<a href='/contribute/diplomat' target='_blank'>${$.i18n.t('partners.forty_languages')}</a>`,
         reach_latinx: `<a href='https://www.yahoo.com/now/mexican-superstar-eugenio-derbez-partners-200100110.html' target='_blank'>${$.i18n.t('partners.reach_latinx')}</a>`,
+        seeds_of_light: `<a href='https://www.accesswire.com/702186/Seeds-of-Light-and-CodeCombat-Expand-Partnership-with-Launch-of-Glow-Academies-for-Middle-School-Students' target='_blank'>${$.i18n.t('partners.seeds_of_light')}</a>`,
+        eugenio_derbez: "<a href='https://www.businesswire.com/news/home/20210316006024/en/Mexican-Superstar-Eugenio-Derbez-Partners-with-CodeCombat-to-Get-the-Latinx-Community-Coding' target='_blank'>Eugenio Derbez</a>",
         interpolation: { escapeValue: false }
       }
     }
@@ -96,8 +119,8 @@ export default Vue.extend({
 .text-h3 {
   font-family: $headline-font;
   font-weight: bold;
-  font-size: 24px;
-  line-height: 32px;
+  font-size: 16px;
+  line-height: 24px;
   letter-spacing: 0.52px;
 }
 
@@ -116,6 +139,7 @@ p, .text-p {
   font-weight: 300;
   letter-spacing: 0.75px;
   line-height: 26px;
+  text-align: justify;
 }
 
 .btn-primary, .btn-navy, .btn-teal {
@@ -141,9 +165,15 @@ p, .text-p {
     margin: 0 auto;
   }
 
+  .header .row {
+    padding: 20px 0;
+  }
+
   & > .row {
-    padding-top: 31px;
-    padding-bottom: 31px;
+    padding: 5px 0 0;
+    &.image-row {
+      padding-bottom: 15px;
+    }
   }
 
   overflow: hidden;
