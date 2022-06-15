6cc6842f676e717da30cf5f970f8798e7bf4b7ff
smallst_wsl <yuqiang_tong@163.com>
2022-06-15 15:21:08 +0800

    fix china footer

---
+++ app/views/test/A.vue
@@ -0,0 +1,26 @@
+<style>
+</style>
+<template>
+  <div>
+    {{A}}
+    <div @click="() => go()">go</div>
+  </div>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        A: 'A'
+}
+},
+    methods: {
+      go() {
+        application.router.navigate("/testb", {trigger: true})
+}
+},
+    mounted() {
+
+}
+}
+</script>
