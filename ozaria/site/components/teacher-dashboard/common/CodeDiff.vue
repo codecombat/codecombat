e3154ac65960d1446f0eded72a01cd4cae3417b0
smallst_wsl <yuqiang_tong@163.com>
2022-07-08 07:08:42 +0800

    introduce ace-diff to ozaria

---
+++ ozaria/site/components/teacher-dashboard/common/CodeDiff.vue
@@ -0,0 +1,96 @@
+<script>
+  const ace = require('lib/aceContainer')
+  const AceDiff = require('ace-diff')
+
+  const aceEditModes = {
+    javascript: 'ace/mode/javascript',
+    coffeescript: 'ace/mode/coffee',
+    python: 'ace/mode/python',
+    lua: 'ace/mode/lua',
+    java: 'ace/mode/java',
+    html: 'ace/mode/html'
+  }
+
+  export default {
+    props: {
+      codeLeft: {
+        type: String,
+        required: true
+      },
+      codeRight: {
+        type: String,
+        required: true
+      },
+      language: {
+        type: String,
+        required: true
+      }
+    },
+
+    data: () => ({
+      codeDiff: null
+    }),
+
+    watch: {
+      codeLeft (val, oldVal) {
+        if (val !== oldVal && this.codeDiff) {
+          this.codeDiff.editors.left.ace.setValue(val.trim(), -1)
+        }
+      },
+      codeRight (val, oldVal) {
+        if (val !== oldVal && this.codeDiff) {
+          this.codeDiff.editors.right.ace.setValue(val.trim(), -1)
+        }
+      }
+    },
+
+    mounted () {
+      this.codeDiff = this.createAceDiff('.code-diff-component')
+    },
+
+    beforeDestroy () {
+      this.codeDiff.destroy()
+    },
+
+    methods: {
+      createAceDiff (el) {
+        const diffView = new AceDiff({
+          element: el,
+          mode: aceEditModes[this.language],
+          theme: 'ace/theme/textmate',
+          left: {
+            content: this.codeLeft,
+            editable: false,
+            copyLinkEnabled: false
+          },
+          right: {
+            content: this.codeRight,
+            editable: false,
+            copyLinkEnabled: false
+          }
+        })
+        return diffView
+      }
+    }
+  }
+</script>
+
+<template>
+  <div
+    ref="code"
+    class="code-diff-component"
+  >
+    <!-- {{ trimmedCode }} -->
+  </div>
+</template>
+
+<style lang="sass">
+  @import 'app/styles/common/ace-diff.sass'
+</style>
+
+<style lang="scss" scoped>
+  .code-area-component {
+    width: 100%;
+  }
+
+</style>
