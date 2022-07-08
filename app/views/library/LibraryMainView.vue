0905762b0c918404740752233427335159f5ade0
Saurabh Patel <saurabh.finch@gmail.com>
2022-07-08 15:57:47 +0530

    library design changes requested

--- app/views/library/LibraryMainView.vue
+++ app/views/library/LibraryMainView.vue
@@ -1,22 +1,23 @@
 <template>
   <div>
-    <div class="library container">
+    <div class="library">
+      <div class="container">
       <section class="row library__heading">
-        <h2 class="library__heading-text">
-          We support equity in computer science by partnering with libraries
+        <h2 class="library__heading-text pixel-text">
+          Give your members access to the most engaging way to learn coding
         </h2>
         <button class="btn library__heading-btn btn-moon" @click="toggleLicenseModal">
-          Talk with our Team
+          Contact Us
         </button>
       </section>
 
       <section class="library__sub">
 
         <div class="row library__sub-1">
-          <div class="library__sub-text col-md-5">
+          <div class="library__sub-text col-md-6">
             Libraries play an important role in advancing educational equity in their communities. Many young learners do not have access or exposure to computer science, and libraries can provide important resources to these underserved groups. That is why we are partnering with libraries across the country to provide access to our game-based learning tools and educational resources.
           </div>
-          <div class="library__sub-image col-md-offset-1 col-md-6">
+          <div class="library__sub-image col-md-6">
             <img src="/images/pages/library/library-students.png" alt="Students in library" class="library__sub-image-img img-responsive">
           </div>
         </div>
@@ -25,7 +26,7 @@
           <div class="library__sub-image col-md-6">
             <img src="/images/pages/library/teach-real-code.png" alt="Teach real code" class="library__sub-image-img img-responsive">
           </div>
-          <div class="library__sub-text col-md-offset-1 col-md-5">
+          <div class="library__sub-text col-md-6">
             Our games teach real typed Python, JavaScript and C++ coding languages through the power of play. The gameplay is paired with a scaffolded curriculum to allow self-paced learners the ability to master foundational programming concepts before moving on to the more complex. Each level challenges the player to type the code necessary to advance their hero and achieve the level goal.
           </div>
         </div>
@@ -33,11 +34,11 @@
       </section>
 
       <section class="library__needs">
-        <div class="library__needs__heading">
+        <div class="library__needs__heading pixel-text">
           Our platform is built to address the specific needs of libraries and their patrons:
         </div>
         <div class="row library__needs__info">
-          <div class="col-md-5 library__needs__info__text">
+          <div class="col-md-6 library__needs__info__text">
             <div class="library__needs__info__text-heading">
               Accessible
             </div>
@@ -45,8 +46,8 @@
               A basic computer with internet access is all you need to sign-in and start playing. The content is intended for beginners with no prior coding experience.
             </div>
           </div>
-          <div class="col-md-offset-1 col-md-6 library__needs__info-img">
-            <img src="/images/pages/home-v2/pd-image.png" alt="Students with computer" class="img-responsive">
+          <div class="col-md-6 library__needs__info-img">
+            <img src="/images/pages/library/kids-in-library.jpeg" alt="Students with computer" class="img-responsive">
           </div>
         </div>
 
@@ -54,7 +55,7 @@
           <div class="col-md-6 library__needs__info-img">
             <img src="/images/pages/library/self-paced-intuitive.png" alt="Self paced and intuitive" class="img-responsive">
           </div>
-          <div class="col-md-offset-1 col-md-5 library__needs__info__text">
+          <div class="col-md-6 library__needs__info__text">
             <div class="library__needs__info__text-heading">
               Self-Paced and Intuitive
             </div>
@@ -64,7 +65,7 @@
           </div>
         </div>
         <div class="row library__needs__info">
-          <div class="col-md-5 library__needs__info__text">
+          <div class="col-md-6 library__needs__info__text">
             <div class="library__needs__info__text-heading">
               Engaging Content
             </div>
@@ -72,7 +73,7 @@
               Game-based learning drives engagement from the start but perhaps even more importantly keeps students engaged when the concepts get more challenging. In a game environment failure and success are expected and embraced as part of the play experience.
             </div>
           </div>
-          <div class="col-md-offset-1 col-md-6 library__needs__info-img">
+          <div class="col-md-6 library__needs__info-img">
             <img src="/images/pages/parents/power_of_play_capstone.gif" alt="Engaging" class="img-responsive">
           </div>
         </div>
@@ -80,7 +81,7 @@
           <div class="col-md-6 library__needs__info-img">
             <img src="/images/pages/library/calgary-home.png" alt="Calgary library page" class="img-responsive">
           </div>
-          <div class="col-md-offset-1 col-md-5 library__needs__info__text">
+          <div class="col-md-6 library__needs__info__text">
             <div class="library__needs__info__text-heading">
               API integration
             </div>
@@ -89,8 +90,38 @@
             </div>
           </div>
         </div>
-        <div class="row library__needs__info">
-          <div class="col-md-5 library__needs__info__text">
+        <div class="row library__needs__info library__needs-only-text">
+          <div class="col-md-11 library__needs__info__text">
+            <div class="library__needs__info__text-heading">
+              Privacy
+            </div>
+            <div class="library__needs__info__text-sub">
+              We take user safety, cybersecurity, and data privacy seriously, following many stringent privacy regulations, like COPPA, FERPA, SOPIPA, California AB 1584, CalOPPA, Fair Information Practices, CAN-SPAM, and GDPR. These policies and protective measures have let us securely serve over twenty million users around the world. See <a href="/privacy" target="_blank">our full privacy policy.</a>
+            </div>
+          </div>
+        </div>
+        <div class="row library__needs__info library__needs-only-text">
+          <div class="col-md-11 library__needs__info__text">
+            <div class="library__needs__info__text-heading">
+              Communication Limits
+            </div>
+            <div class="library__needs__info__text-sub">
+              To ensure the security of your members, we do not offer in-game chat or other communication features between players, however we do allow members to safely share their coding projects with friends, family and the CodeCombat community. We restrict any upsell or marketing messaging to provide an uninterrupted experience for your patrons.
+            </div>
+          </div>
+        </div>
+        <div class="row library__needs__info library__needs-only-text">
+          <div class="col-md-11 library__needs__info__text">
+            <div class="library__needs__info__text-heading">
+              Usage Stats
+            </div>
+            <div class="library__needs__info__text-sub">
+              Gain insight to your members’ usage with our snapshot reporting tool. It provides access to usage stats at any time, including monthly active users, licensed days used and more valuable data points.
+            </div>
+          </div>
+        </div>
+        <div class="row library__needs__info library__needs-only-text">
+          <div class="col-md-11 library__needs__info__text">
             <div class="library__needs__info__text-heading">
               Academic Esports
             </div>
@@ -98,56 +129,27 @@
               Our esports program offers an opportunity to further engage your members through a fun and exciting competition. The AI League allows members to showcase their understanding of coding concepts in a new environment and practice problem solving strategies as they go head to head in battle against the global community.
             </div>
           </div>
-          <div class="col-md-offset-1 col-md-6 library__needs__info-img">
-            <img src="/images/pages/home-v2/league-image.png" alt="AI League" class="img-responsive">
-          </div>
         </div>
-        <div class="row library__needs__info">
-          <div class="col-md-11 library__needs__info__text">
-            <div class="library__needs__info__text-heading">
-              Privacy
-            </div>
-            <div class="library__needs__info__text-sub">
-              We take user safety, cybersecurity, and data privacy seriously, follow many stringent privacy regulations, like COPPA, FERPA, SOPIPA, California AB 1584, CalOPPA, Fair Information Practices, CAN-SPAM, as well as GDPR, and these policies and protective measures have let us serve 20M+ users around the world without incident.
-            </div>
-          </div>
+        <div class="library__continue">
+          <img class="img-responsive" src="/images/pages/parents/graphic_07.svg" style="margin: 0 auto; transform: translate(-47%, 0);" loading="lazy"/>
         </div>
-        <div class="row library__needs__info">
-          <div class="col-md-11 library__needs__info__text">
-            <div class="library__needs__info__text-heading">
-              Communication Limits
-            </div>
-            <div class="library__needs__info__text-sub">
-              While members do have the ability to share their coding projects with friends, family and the CodeCombat community, there are no in-game chat or other communication features. Marketing and any upsell messaging is disabled for this execution.
+        <section class="library__footer">
+          <div class="library__footer-content row">
+            <div class="col-md-offset-2 col-md-8">
+              <h2 class="library__footer-text pixel-text">
+                Work with us to unlock the next generation of engineers, programmers and CS professionals in your community.
+              </h2>
+              <div class="library__footer-cta">
+                <button class="btn library__footer-cta-btn btn-moon" @click="toggleLicenseModal">
+                  Schedule a Demo
+                </button>
+              </div>
             </div>
           </div>
-        </div>
-        <div class="row library__needs__info">
-          <div class="col-md-11 library__needs__info__text">
-            <div class="library__needs__info__text-heading">
-              Usage Stats
-            </div>
-            <div class="library__needs__info__text-sub">
-              Snapshot reporting tool where you can see usage stats at any time, including stats like monthly active users.
-            </div>
-          </div>
-        </div>
+        </section>
       </section>
     </div>
-    <section class="library__footer container-fluid">
-      <div class="library__footer-content row">
-        <div class="col-md-offset-2 col-md-8">
-          <h2 class="library__footer-text">
-            Work with us to unlock the next generation of engineers, programmers and CS professionals in your community.
-          </h2>
-          <div class="library__footer-cta">
-            <button class="btn library__footer-cta-btn btn-moon" @click="toggleLicenseModal">
-              Contact Us
-            </button>
-          </div>
-        </div>
-      </div>
-    </section>
+    </div>
     <modal-get-licenses
       v-if="showLicenseModal"
       subtitle="Please complete this short form and one of our library specialists will reach out to provide a demo and custom pricing options!"
@@ -190,15 +192,20 @@ export default {
 
 <style scoped lang="scss">
 @import "app/styles/common/button";
+@import "app/styles/style-flat-variables";
 
 .library {
   font-size: 62.5%;
   font-family: "Work Sans", "Open Sans", sans-serif;
 
   padding-top: 3rem;
+  background: linear-gradient(262.39deg, #D7EFF2 -1.56%, #FDFFFF 95.05%);
   &__heading {
     text-align: center;
     margin-bottom: 5rem;
+
+    background-image: url(/images/pages/parents/image_cloud_3.svg);
+    background-repeat: no-repeat;
 
     &-text {
       font-weight: 700;
@@ -210,10 +217,14 @@ export default {
   }
 
   &__sub {
+    background-image: url(/images/pages/parents/image_cloud_4.svg);
+    background-repeat: no-repeat;
+    background-position: center top;
+
     margin-bottom: 5rem;
     &-text {
       font-size: 2.4rem;
-      line-height: 3.4rem;
+      line-height: 3rem;
       letter-spacing: 0.44px;
     }
 
@@ -227,6 +238,16 @@ export default {
   }
 
   &__needs {
+    background-image: url(/images/pages/parents/image_cloud_1.svg);
+    background-repeat: no-repeat;
+    background-position: right top;
+
+    &-only-text:nth-child(even) {
+      background-image: url(/images/pages/parents/image_cloud_4.svg);
+      background-repeat: no-repeat;
+      background-position: right top;
+    }
+
     &__heading {
       font-weight: 700;
       font-size: 4rem;
@@ -265,39 +286,24 @@ export default {
   }
 
   &__footer {
-    height: 56.4vw;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    position: relative;
+    border-radius: 38px;
+    border: 4px solid #6AE8E3;
+    padding: 3rem;
 
-    color: white;
-
-    &::before {
-      content: "";
-      background: url(/images/ozaria/home/background_cta_1.png) no-repeat center;
-      background-size: cover;
-      position: absolute;
-      top: 0;
-      right: 0;
-      bottom: 0;
-      left: 0;
-      filter: brightness(0.6);
-    }
-
-    &-content {
-      z-index: 1;
-      text-align: center;
-    }
+    text-align: center;
 
     &-text {
       font-size: 2.6rem;
       line-height: 4rem;
       letter-spacing: 0.444px;
-      color: #fff;
       font-weight: 400;
       margin-bottom: 2rem;
     }
   }
+}
+
+.pixel-text {
+  font-family: $pixel-headline-font;
+  color: $navy;
 }
 </style>
