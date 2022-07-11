7a727b1337cbae5bd9c4b227cd3dd74a9b7c80b1
Saurabh Patel <saurabh.finch@gmail.com>
2022-07-11 16:14:02 +0530

    add download attribute in mp3 audio file instead of opening in new tab

--- app/views/podcast/single-podcast/EpisodeComponent.vue
+++ app/views/podcast/single-podcast/EpisodeComponent.vue
@@ -27,14 +27,16 @@
           <div class="episode__info-description" v-html="formatDescription"></div>
         </div>
         <div class="col-md-4 episode__function">
-          <div
+          <a
+            :href="`/file/${podcast.audio.mp3}`"
+            target="_blank"
+            download
             v-if="podcast.audio && podcast.audio.mp3"
-            class="episode__btn-info episode__function-download"
-            @click="() => onDownloadClick(podcast)"
+            class="episode__btn-info episode__btn-anchor episode__function-download"
           >
             <img src="/images/pages/podcast/IconDownload.svg" alt="Download Icon" class="episode__download-icon episode__icon">
             <span class="episode__listen episode__btn-text episode__btn-text-hor">{{ $t('podcast.download') }}</span>
-          </div>
+          </a>
           <div
             v-if="podcast.transcript"
             class="episode__btn-info"
@@ -193,6 +195,7 @@ export default {
   &__btn-text {
     font-weight: 700;
     font-size: 1.8rem;
+    color: black;
 
     &-play {
       font-size: 2.2rem;
@@ -212,6 +215,11 @@ export default {
 
   &__frame {
     padding-bottom: 1rem;
+  }
+
+  &__btn-anchor {
+    text-decoration: none;
+    display: block;
   }
 
 }
