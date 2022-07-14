478d2d0ec27ae6b5fe475c88fc0b400616f1895f
Tong Yuqiang <tongsmallst@gmail.com>
2022-07-14 08:13:29 +0800

    better maintenance ui (#6681)
    Co-authored-by: smallst_wsl <yuqiang_tong@163.com>

--- app/views/admin/components/MaintenanceModal.vue
+++ app/views/admin/components/MaintenanceModal.vue
@@ -1,15 +1,27 @@
 <script>
   const fetchJson = require("../../../core/api/fetch-json")
   export default Vue.extend({
+    props: ['hide'],
     data() {
      return {
-       hours: 1,
+       hours: 0,
+       pending: 10,
      }
     },
     methods: {
       submit: async function(){
         await fetchJson('/admin/maintenance-time', {json: {hours: this.hours}, method: 'POST' })
+      },
+      countDown () {
+        this.pending -= 1
+        if(this.pending) {
+          setTimeout(this.countDown, 1000)
+        }
       }
+    },
+    mounted() {
+      this.pending = 10
+      this.countDown()
     }
   })
 </script>
@@ -17,21 +29,27 @@
 #modal-base-flat
   .body
     .alert
-      p MAINTENANCE MODE MAKES WEBSITE INACCESSIBLE!
-      p MAINTENANCE MODE MAKES WEBSITE INACCESSIBLE!
-      p MAINTENANCE MODE MAKES WEBSITE INACCESSIBLE!
+      p MAINTENANCE MODE MAKES CODECOMBAT INACCESSIBLE!
+      p MAINTENANCE MODE MAKES CODECOMBAT INACCESSIBLE!
+      p MAINTENANCE MODE MAKES CODECOMBAT INACCESSIBLE!
       p ARE YOU SURE YOU WANT TO CONTINUE ?
     .input
+      p if you're not sure, ask the #eng in slack
       p set the maintenance hours:
       div
-        input(type="number", v-model="hours", min="0.1", step="0.1")
+        input(type="number", v-model="hours", min="0", step="0.1")
         span Hours
       br
-      a.btn.btn-primary.btn-lg(@click="submit") I'm Sure, Go Ahead!
+      .buttons
+        a.btn.btn-danger.btn-lg(@click="submit" :class="{disabled: pending}") I'm Sure, Go Ahead!
+          span(v-if="pending") ({{pending}})
+        a.btn.btn-primary.btn-lg(@click="hide") I dunno
 </template>
 
 <style scoped lang="scss">
   .body {
+    width: 800px;
+    margin-left: -100px;
     background: #F4FAFF;
     box-shadow: 2px 2px 2px 2px #777;
     padding: 5px;
@@ -48,5 +66,10 @@
     display: flex;
     flex-direction: column;
     align-items: center;
-}
+  }
+  .buttons {
+    width: 600px;
+    display: flex;
+    justify-content: space-between;
+  }
 </style>
