d838f56e723c9a6ee233ba1e3aeb66a88a9e0cbf
Saurabh Patel <saurabh.finch@gmail.com>
2022-07-15 15:02:14 +0530

    changes for tecmilenio page

--- app/views/payment/student-license/HeadComponent.vue
+++ app/views/payment/student-license/HeadComponent.vue
@@ -3,14 +3,32 @@
     <div class="container">
       <div class="top-section">
         <div class="heading-row">
-          <h1>{{ title || $t(`payments.${this.i18nHeadingName}`)}}</h1>
+          <h1 v-if="!isTecmilenioPartner">{{ title || $t(`payments.${this.i18nHeadingName}`)}}</h1>
+          <h1 v-else>Licencias de Estudiantes</h1>
           <p class="dsh-info" v-if="isDshPartner"><b>Digital Schoolhouse</b> has partnered with CodeCombat to offer the entirety of the Ozaria story-based computer science adventure game (all 4 Chapters, and over 70 hours of comprehensive instruction) as well as our original game CodeCombat (11 courses spanning Computer Science, Game Development and Web Development) for the <b>discounted rate of $25 USD per annual license (normally $30 USD)</b>. Happy coding!</p>
-          <h5>{{ subTitle || $t('payments.great_courses')}}</h5>
+          <div class="tecmilenio-info" v-else-if="isTecmilenioPartner">
+            <p class="tecmilenio-info-txt">
+              Tecmilenio se asoció con CodeCombat para ofrecer educación en codificación programación en Python  basada en juegos por el precio con descuento de $12.52 USD. Todos los estudiantes de Tecmilenio que reciban este enlace deben comprar el software CodeCombat.
+            </p>
+            <p class="tecmilenio-info-txt">
+              En siguiente página, se le pedirá que ingrese el correo del estudiante de Tecmilenio. Ejemplo: al02962166@tecmilenio.mx. Asegúrese de que esta información sea correcta, ya que nos permite saber que ha pagado por su licencia el estudiante.
+            </p>
+          </div>
+          <h5 v-if="!isTecmilenioPartner">{{ subTitle || $t('payments.great_courses')}}</h5>
+          <h5 v-else>La licencia del estudiante incluye:</h5>
         </div>
         <div class="row info-row">
           <template v-if="isDshPartner">
             <ozaria-head-info-component />
             <coco-head-info-component />
+          </template>
+          <template v-else-if="isTecmilenioPartner">
+            <ozaria-head-info-component
+              :is-tecmilenio-partner="isTecmilenioPartner"
+            />
+            <coco-head-info-component
+              :is-tecmilenio-partner="isTecmilenioPartner"
+            />
           </template>
           <template v-else>
             <coco-head-info-component />
@@ -39,6 +57,10 @@ export default {
     isDshPartner: {
       type: Boolean,
       default: false
+    },
+    isTecmilenioPartner: {
+      type: Boolean,
+      default: false
     }
   },
 }
@@ -65,5 +87,12 @@ export default {
 .dsh-info {
   color: white;
   font-size: small;
+}
+
+.tecmilenio-info {
+  &-txt {
+    color: white;
+    font-size: small;
+  }
 }
 </style>
