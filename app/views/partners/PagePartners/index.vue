888fdd9ca39bd5026d132948b0389a5fec4a2e0e
Adam Kecskes <adam.k@powerednow.com>
2022-07-18 10:34:07 +0200

    Polishing the /partners page. (borders, backgrounds, images)

--- app/views/partners/PagePartners/index.vue
+++ app/views/partners/PagePartners/index.vue
@@ -1,64 +1,103 @@
 <template lang="pug">
 .container-fluid#partners-page
   .header
+    img.animated-griffin(src="/images/pages/parents/Griffin_and_Alejandro1.svg" alt="flying griffin")
     .width-container.text-center.row
       .col-lg-12
-        h1.text-h1.text-navy {{ $t("partners.headline") }}
+        h1.text-h1.text-navy.pixelated {{ $t("partners.headline") }}
 
     .width-container.text-center.row
       .col-lg-12
         a.btn.btn-primary.btn-lg(href="https://form.typeform.com/to/loZIh0I9" target="_blank") {{ $t('general.contact_us') }}
 
-  .width-container.row.image-row
-    .col-md-8
+  .width-container.row.image-row.row-eq-height.container-coding
+    .col-md-7.text-column
       h3.text-h3 {{ $t("partners.coding_title") }}
       p.text-navy {{ $t("partners.coding_blurb") }}
-    .col-md-4
-      img.img-responsive(src="/images/pages/partners/coding.jpg")
+    .col-md-5.pixel-background.teal-pixel-background
+      img.img-responsive.teal-border(src="/images/pages/partners/coding.jpg")
 
-  .width-container.row.image-row
-    .col-md-4
-      img.img-responsive(src="/images/pages/partners/powered-by-hyper.png")
-    .col-md-8
+  .container-graphic-spacer
+    .container
+      .row
+        .col-xs-12.graphic-spacer-column
+          img.img-responsive-height.native-height(src="/images/pages/parents/graphic_03_speech.svg" alt="hero moving along a path" loading="lazy")
+
+  .width-container.row.image-row.row-eq-height
+    .col-md-5.pixel-background.yellow-pixel-background-2
+      img.img-responsive.teal-border(src="/images/pages/partners/powered-by-hyper.png")
+    .col-md-7.text-column
       h3.text-h3 {{ $t("partners.esports_title") }}
       p.text-navy(v-html="$t('partners.esports_blurb', i18nData)")
+
+  .container-graphic-spacer
+    .container
+      .row
+        .col-xs-12.graphic-spacer-column
+          img.img-responsive-height(src="/images/pages/parents/graphic_06.svg" alt="hero moving along a path" loading="lazy")
 
   .width-container.row.image-row
     .col-sm-12
       h3.text-h3 {{ $t("partners.distribution_title") }}
       p.text-navy(v-html="$t('partners.distribution_blurb', i18nData)")
 
-  .width-container.row.image-row
-    .col-md-8
+
+  .container-graphic-spacer
+    .container
+      .row
+        .col-xs-12.graphic-spacer-column
+          img.img-responsive-height(src="/images/pages/parents/graphic_05.svg" alt="hero moving along a path" loading="lazy")
+
+
+  .width-container.row.image-row.row-eq-height
+    .col-md-7.text-column
       h3.text-h3 {{ $t("partners.equity_social_title") }}
       p.text-navy
         span(v-html="$t('partners.equity_social_blurb', i18nData)")
         span(v-html="$t('partners.seek_relationships_blurb', i18nData)")
-    .col-md-4
-      img.img-responsive(src="/images/pages/partners/selina.jpg" alt="Seeds of Light")
+    .col-md-5.pixel-background.teal-pixel-background-2
+      img.img-responsive.teal-border(src="/images/pages/partners/selina.jpg" alt="Seeds of Light")
 
-  .width-container.row
+  .container-graphic-spacer
+    .container
+      .row
+        .col-xs-12.graphic-spacer-column
+          img.img-responsive-height(src="/images/pages/parents/graphic_04.svg" alt="hero moving along a path" loading="lazy")
+
+  .width-container.row.row-top-margin
     .col-lg-12
       p.text-navy
         span(v-html="$t('partners.latin_superstar_blurb', i18nData)")
-
-
 
   .row.width-container.image-row.quote-row
     .col-md-5.col-md-offset-1.col-sm-12.teacher-quote.col-sm-offset-0
       h3.text-h3.text-navy {{ $t("partners.eugenio_derbez_blurb") }}
     .col-md-5.col-sm-12
-      img.img-responsive(src="/images/pages/partners/Eugenio-Derbez.png" alt="Eugenio Derbez")
+      img.img-responsive.teal-border(src="/images/pages/partners/Eugenio-Derbez.png" alt="Eugenio Derbez")
 
-  .width-container.row.image-row
-    .col-md-4
-      img.img-responsive(src="/images/pages/partners/corporate.png")
-    .col-md-8
+
+  .container-graphic-spacer
+    .container
+      .row
+        .col-xs-12.graphic-spacer-column
+          img.img-responsive-height.native-height(src="/images/pages/parents/graphic_08.svg" alt="hero moving along a path" loading="lazy")
+
+  .width-container.row.image-row.row-eq-height
+    .col-md-5.pixel-background.yellow-pixel-background
+      img.img-responsive.teal-border(src="/images/pages/partners/corporate.png")
+    .col-md-7.text-column
       h3.text-h3 {{ $t("partners.corporate_team_building_title") }}
       p.text-navy(v-html="$t('partners.corporate_team_building_blurb', i18nData)")
 
 
-  .width-container.row
+  .container-graphic-spacer
+    .container
+      .row
+        .col-xs-12.graphic-spacer-column
+          img.img-responsive-height.native-height(src="/images/pages/parents/graphic_07.svg" alt="hero moving along a path" loading="lazy")
+
+
+  .width-container.row.row-top-margin
     .col-lg-12
       p.text-navy {{ $t("partners.footer_blurb") }}
 
@@ -66,10 +105,17 @@
     .col-lg-12
       a.btn.btn-primary.btn-lg(href="https://form.typeform.com/to/loZIh0I9" target="_blank") {{ $t("partners.talk_with_us") }}
 
-  .width-container.text-center.row.logos-row
+  .container-graphic-spacer
+    .container
+      .row
+        .col-xs-12.graphic-spacer-column
+          img.img-responsive-height(src="/images/pages/parents/graphic_09.png" alt="hero moving along a path based on code commands" loading="lazy")
+
+
+  .width-container.text-center.row.brands-row
     .col-lg-12
       h3.text-h3 {{ $t("partners.our_partners") }}
-    .col-lg-12
+    .col-lg-12.brands-column
       ul.brands
         li.item
           img(src="/images/pages/base/code-ninjas-logo-right.png")
@@ -171,15 +217,57 @@ p, .text-p {
 }
 
 #partners-page {
+  background: linear-gradient(262.39deg, #D7EFF2 -1.56%, #FDFFFF 95.05%);
   .width-container {
     max-width: 1170px;
     float: unset;
     margin: 0 auto;
   }
 
-  .header .row {
-    padding: 30px 0;
+  .header {
+    min-height: 628px;
+    background-image: url(/images/pages/parents/parent_hero_image.png), url(/images/pages/parents/image_cloud_3.svg), url(/images/pages/parents/image_cloud_4.svg), url(/images/pages/parents/image_cloud_3.svg), url(/images/pages/parents/image_cloud_1.svg);
+    background-repeat: no-repeat, no-repeat, no-repeat, no-repeat, no-repeat;
+    background-position: bottom left 5%, top 50px left 30px, top 35px right 280px, top 360px right 300px, bottom 52px right 475px;
+    background-size: 500px, 260px, 90px, 260px, 250px;
+    .row {
+      padding: 30px 0;
+      .pixelated {
+        font-family: "lores12ot-bold", "VT323";
+      }
+    }
+    .animated-griffin {
+      position: absolute;
+      top: 30%;
+      right: 6%;
+    }
   }
+
+
+
+  .row-top-margin {
+    margin-top: 30px;
+  }
+
+
+  .img-responsive-height {
+    display: block;
+    max-width: 100%;
+    max-height: 200px;
+    &.native-height {
+      max-height: none;
+    }
+  }
+
+  .container-graphic-spacer {
+    pointer-events: none;
+    min-height: 200px;
+    .graphic-spacer-column {
+      display: flex;
+      justify-content: center;
+    }
+  }
+
 
   & > .row {
     padding: 5px 0 0;
@@ -190,7 +278,7 @@ p, .text-p {
         padding: 40px 0;
       }
     }
-    &.logos-row,
+    &.brands-row,
     &.quote-row {
       padding: 40px 0;
     }
@@ -199,10 +287,63 @@ p, .text-p {
       padding: 30px 0;
     }
 
+    &.container-coding {
+      position: relative;
+      padding-top: 100px;
+      &::before{
+        content: "";
+        background-image: url(/images/pages/parents/graphic_01_anya.svg);
+        background-repeat: no-repeat;
+        position: absolute;
+        background-size: 99%;
+        background-position: center;
+        width: 151px;
+        height: 227px;
+        top: -50px;
+      }
+    }
+
+    .brands-column {
+      border: 4px dashed $teal;
+      border-radius: 20px;
+      margin-top: 20px;
+    }
+  }
+
+  .row-eq-height {
+    display: flex;
+    flex-wrap: wrap;
   }
 
   .img-responsive {
     width: 100%;
+  }
+
+  .text-column {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: stretch;
+  }
+
+  .pixel-background {
+    padding: 80px;
+    background-size: contain;
+    background-repeat: no-repeat;
+    background-position: center center;
+    &.teal-pixel-background {
+      background-image: url(/images/pages/partners/pixel-bg-light-teal.svg);
+    }
+    &.teal-pixel-background-2 {
+      background-image: url(/images/pages/partners/pixel-bg-light-teal-2.svg);
+    }
+    &.yellow-pixel-background {
+      background-image: url(/images/pages/partners/pixel-bg-yellow.svg);
+      padding-bottom: 25px;
+    }
+    &.yellow-pixel-background-2 {
+      background-image: url(/images/pages/partners/pixel-bg-yellow-2.svg);
+    }
   }
 }
 
@@ -228,6 +369,13 @@ p, .text-p {
   }
 }
 
+.teal-border {
+  border: 10px solid $teal;
+  box-sizing: border-box;
+  border-radius: 20px;
+}
+
+
 
 .container-footer-mountains {
   width: 100%;
@@ -251,6 +399,7 @@ p, .text-p {
     justify-content: center;
     align-items: center;
     height: 100%;
+    mix-blend-mode: multiply;
     img {
       place-self: center;
       object-fit: contain;
