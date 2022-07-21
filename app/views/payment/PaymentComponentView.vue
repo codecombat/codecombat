13d672888703e2a9c8659f9c3be7f40fc7ab612c
Saurabh Patel <saurabh.finch@gmail.com>
2022-07-21 14:58:08 +0530

    tem

--- app/views/payment/PaymentComponentView.vue
+++ app/views/payment/PaymentComponentView.vue
@@ -32,7 +32,7 @@ export default {
   name: "PaymentComponentView",
   components: {
     PaymentOnlineClassesView,
-    PaymentStudentLicensesComponent
+    PaymentStudentLicensesComponent,
   },
   data() {
     return {
--- app/views/payment/online-class/PageView.vue
+++ app/views/payment/online-class/PageView.vue
@@ -57,7 +57,7 @@ export default {
   data () {
     return {
       showPurchaseView: false,
-    }
+    };
   },
   props: {
     priceData: {
