18d88ddec7cbd874980d3456b38772b593a607fe
Nick Winter <livelily@gmail.com>
2022-12-01 13:30:29 -0800

    Fix leaderboard clan/team column formatting to not be so wide

--- app/views/ladder/components/Leaderboard.vue
+++ app/views/ladder/components/Leaderboard.vue
@@ -195,7 +195,7 @@ export default Vue.extend({
       if (slug === 'name') {
         return { 'name-col-cell': 1, ai: /(Bronze|Silver|Gold|Platinum|Diamond) AI/.test(item) }
       }
-      if (slug === 'team') {
+      if (slug === 'team' || slug === 'clan') {
         return { capitalize: 1, 'clan-col-cell': 1 }
       }
       if (slug === 'language') {
