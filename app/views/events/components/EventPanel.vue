3b6bf57886417ae95cfe1ef3f920e2c1373146cc
smallst_wsl <yuqiang_tong@163.com>
2022-12-12 16:37:50 +0800

    make side panel a vue template

--- app/views/events/components/EventPanel.vue
+++ app/views/events/components/EventPanel.vue
@@ -1,30 +1,29 @@
 <template>
-  <div>
-    <transition name="slide">
-      <div
-        v-if="isVisible"
-        id="event-panel"
-        class="transition-side-panel"
-      >
-        <div class="header">
-          <div class="header-right">
-            <img
-              class="close-btn"
-              src="/images/ozaria/teachers/dashboard/svg_icons/Icon_Exit.svg"
-              @click="close"
-            >
-          </div>
-        </div>
-        <div>Panel</div>
-      </div>
-    </transition>
-  </div>
+  <side-panel
+    :is-visible="isVisible"
+    :block-background="false"
+    id="event-panel"
+    @close-panel="close"
+  >
+    <template #header>
+      Event Panel
+    </template>
+
+    <template #body>
+      Body
+    </template>
+  </side-panel>
 </template>
 
 <script>
 import { mapGetters, mapMutations } from 'vuex'
+import SidePanel from '../../../components/common/SidePanel'
+
 export default {
   name: 'EventPanel',
+  components: {
+    SidePanel
+  },
   computed: {
     ...mapGetters({
       isVisible: 'events/eventPanelVisible'
@@ -39,20 +38,4 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-@use 'app/styles/common/transition' with (
-  $side-panel-width: min(40vw, 800px)
-);
-
-#event-panel {
-  height: 100vh;
-}
-
-.header-right{
-  float: right;
-  .close-btn {
-    cursor: pointer;
-    margin-left: 30px;
-    padding: 10px;
-  }
-}
 </style>
