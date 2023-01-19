e0bc736f919f6b196c79c1323b8b8917129e77d5
Saurabh Patel <saurabh.finch@gmail.com>
2023-01-19 13:22:26 -0800

    add workflow for lighthouse ci

---
+++ lighthouserc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  ci: {
+    collect: {
+      url: ['https://codecombat.com', 'https://www.ozaria.com']
+    },
+    upload: {
+      target: 'temporary-public-storage'
+    }
+  }
+}
