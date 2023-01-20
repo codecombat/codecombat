5c626a5b3ef1bfa0e1e0fd6545df836e58a790c7
Saurabh Patel <saurabh.finch@gmail.com>
2023-01-19 16:27:41 -0800

    integrate to lhci server

--- lighthouserc.js
+++ lighthouserc.js
@@ -4,7 +4,9 @@ module.exports = {
       url: ['https://codecombat.com', 'https://www.ozaria.com']
     },
     upload: {
-      target: 'temporary-public-storage'
+      target: 'lhci',
+      serverBaseUrl: 'https://warm-dawn-24831.herokuapp.com/',
+      token: '7dbd57bd-0b67-491a-a4dd-cb35e6a95a86'
     }
   }
 }
