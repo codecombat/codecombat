50e8c0870cb8540ec91d55459eda831dc0d8b3eb
Justin Hilliard <justin.a.hilliard@gmail.com>
2023-07-20 09:46:53 -0700

    Schema updates for AI (#7041)
    * updates docs
    
    * got api returning for single endpoint in the integrated surface
    
    * chat message still not working
    
    * learn mode working
    
    * updates for scenario editor
    
    * updates for scenario editor
    
    * feat: add ai model class
    
    * fix: simplify aimodel
    
    * pushing
    
    * adds name to scenario schema
    
    * copy and pasting missing code snippets
    
    * fix: wrap tailwind with a class, so we can do it multiple places
    
    * adds back necessary plugins
    
    * removes empty line
    
    * feat: make chat messages polymorhpic to scenario or project
    
    * fixes bugs
    
    * Fix slugify in utils for backend (#7043)
    
    ---------
    
    Co-authored-by: Jake Correa <jcorrea257@gmail.com>
    Co-authored-by: adamkecskes <adam.k@codecombat.com>

---
+++ app/schemas/models/ai_chat_message.schema.js
@@ -0,0 +1,39 @@
+const c = require('./../schemas')
+const _ = require('lodash')
+
+const AIChatMessageSchema = c.object({
+  title: 'AI Interaction',
+  description: 'A generative AI interaction',
+  required: ['actor', 'parent', 'parentKind']
+})
+
+_.extend(AIChatMessageSchema.properties, {
+  actor: { type: 'string', title: 'Actor', enum: ['model', 'user', 'teacher'] },
+  parent: c.objectId({ refPath: 'parentKind', title: 'Parent', description: 'The parent chat of this message' }),
+  parentKind: { type: 'string', title: 'Kind', enum: ['scenario', 'project'], description: 'Whether this message is part of a scenario or project chat' },
+  sentAt: c.date({ title: 'Sent', description: 'The time the message started being sent' }),
+  text: {
+    type: 'string',
+    title: 'Chat Message Text',
+    description: 'The content text of the chat message'
+  },
+  document: c.objectId(),
+  preview: {
+    type: 'string',
+    title: 'Preview',
+    maxLength: 300,
+    description: 'A preview of the document in the message discussed'
+  },
+  created: c.date({ title: 'Created' }),
+  actionData: {
+    type: 'object',
+    title: 'Data',
+    description: 'Data associated with the message action'
+  }
+})
+
+c.extendBasicProperties(AIChatMessageSchema, 'ai_chat_message')
+c.extendSearchableProperties(AIChatMessageSchema)
+// c.extendPermissionsProperties(AIInteractionSchema, 'ai_interaction')
+
+module.exports = AIChatMessageSchema
