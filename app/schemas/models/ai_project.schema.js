26b23efafa89f8bb9f14160048737d36c7058d59
Saurabh Patel <saurabh.finch@gmail.com>
2023-09-01 11:46:09 +0530

    output of build schema in ai project

--- app/schemas/models/ai_project.schema.js
+++ app/schemas/models/ai_project.schema.js
@@ -1,5 +1,10 @@
-const c = require('./../schemas')
+// WARNING: This file is auto-generated from within AI HackStack. Do not edit directly.
+// Instead, edit the corresponding Zod schema in the HackStack repo and run `npm run build` or `npm run build:schemas
+//
+// Last updated: 2023-09-01T06:15:18.648Z
+
 const _ = require('lodash')
+const c = require('./../schemas')
 
 const AIProjectSchema = c.object({
   title: 'AI Project',
@@ -7,32 +12,40 @@ const AIProjectSchema = c.object({
 })
 
 _.extend(AIProjectSchema.properties, {
-  name: {
+  name: { title: 'Name', type: 'string', maxLength: 100 },
+  description: {
+    title: 'Description',
     type: 'string',
-    description: 'The name of the project'
+    description: 'A short explanation of what this project is about',
+    maxLength: 2000,
   },
-  user: c.objectId({
-    links: [{ rel: 'db', href: '/db/user/{($)}' }],
-    title: 'User ID',
-    description: 'The user ID of the project owner'
-  }),
-  scenario: c.objectId({
-    links: [{ rel: 'db', href: '/db/ai_scenario/{($)}' }],
-    title: 'SCENARIO ID',
-    description: 'The scenario ID of the project'
-  }),
-  created: c.date({ title: 'Created', readOnly: true }),
   visibility: {
-    type: 'string',
-    enum: ['private', 'public', 'published'],
     title: 'Visibility',
-    description: 'Whether this project is private, public but unlisted, or public and published'
+    type: 'string',
+    description: 'Whether this project is private, public but unlisted, or public and published',
+    enum: ['private', 'public', 'published'],
   },
-  actionQueue: c.array({
+  user: {
+    title: 'User',
+    type: ['object', 'string'],
+    description: 'The user ID of the project owner',
+    links: [{ rel: 'db', href: '/db/user/{($)}' }],
+  },
+  scenario: {
+    title: 'Scenario',
+    type: ['object', 'string'],
+    description: 'The scenario ID of the project',
+    links: [{ rel: 'db', href: '/db/ai_scenario/{($)}' }],
+  },
+  actionQueue: {
     title: 'Action Queue',
-    description: 'Actions left to perform in this project'
-  }, c.objectId({format: 'chat-message-link'})),
+    type: 'array',
+    description: 'Actions left to perform in this project, represented as AI Chat Messages',
+    items: { type: ['object', 'string'], format: 'chat-message-link' },
+  },
 })
+
+AIProjectSchema.required = ['visibility', 'user', 'scenario', 'actionQueue']
 
 c.extendBasicProperties(AIProjectSchema, 'ai_project')
 c.extendPermissionsProperties(AIProjectSchema, 'ai_project')
