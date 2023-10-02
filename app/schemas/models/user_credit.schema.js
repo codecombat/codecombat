c90ac60d60c32d507dfc1ed76023b29766c73159
Saurabh Patel <saurabh.finch@gmail.com>
2023-10-02 12:19:18 +0530

    user credit schema changes

--- app/schemas/models/user_credit.schema.js
+++ app/schemas/models/user_credit.schema.js
@@ -13,7 +13,18 @@ const UserCreditSchema = c.object({
           }
         ]
     }),
-    credits: c.int(),
+    credits: c.array({
+      title: 'Credit by different types like hackstack, level_chat' // making it an array of objects so that we can field like endDate in future if needed
+    }, c.object({
+      title: 'Single credit type with its value'
+    }, {
+      key: c.shortString({
+        title: 'Credit type: level_chat, hackstack'
+      }),
+      value: c.int({
+        title: 'Number of credits available to use'
+      })
+    })),
     history: c.array(
       {
         title: 'History of credit transactions'
@@ -25,7 +36,12 @@ const UserCreditSchema = c.object({
           operation: c.shortString({
             enum: ['add', 'sub']
           }),
-          credit: c.int(),
+          credit: c.object({
+            title: 'Type and value of credit'
+          }, {
+            type: c.shortString(),
+            value: c.int()
+          }),
           transactionDate: c.date(),
           source: c.object({
             title: 'source of transaction'
