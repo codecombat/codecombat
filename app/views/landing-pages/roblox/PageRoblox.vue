cfe3c28529adb4bef23751e24eb474108c49c4c6
adamkecskes <adam.k@codecombat.com>
2023-10-10 12:55:35 +0200

    /roblox page update (#7131)
    * /roblox page update
    
    * responsive fixes for /roblox page
    
    * optimize asset images
    
    * optimize asset images
    
    * optimize Play Now button

--- app/views/landing-pages/roblox/PageRoblox.vue
+++ app/views/landing-pages/roblox/PageRoblox.vue
@@ -1,32 +1,74 @@
 <template>
   <div id="roblox-page">
     <div class="container-fluid headline-container">
-      <div class="video-background">
-        <iframe
-          src="https://customer-burj9xtby325x4f1.cloudflarestream.com/a4946ec5affa5a4ff315255487379661/iframe?muted=true&preload=true&loop=true&autoplay=true&poster=https%3A%2F%2Fcustomer-burj9xtby325x4f1.cloudflarestream.com%2Fa4946ec5affa5a4ff315255487379661%2Fthumbnails%2Fthumbnail.jpg%3Ftime%3D%26height%3D600&controls=false"
-          style="border: none; position: absolute; top: 0; left: 0; height: 100%; width: 100%;"
-          allow="accelerometer; gyroscope; autoplay; encrypted-media; picture-in-picture;"
-          allowfullscreen="true"></iframe>
+      <div class="responsive-background">
+        <picture>
+          <source media="(max-width: 600px)" srcset="/images/pages/roblox/header-bg/bg_600.webp" type="image/webp">
+          <source media="(max-width: 600px)" srcset="/images/pages/roblox/header-bg/bg_600.png" type="image/png">
+
+          <source media="(min-width: 601px) and (max-width: 800px)" srcset="/images/pages/roblox/header-bg/bg_800.webp"
+            type="image/webp">
+          <source media="(min-width: 601px) and (max-width: 800px)" srcset="/images/pages/roblox/header-bg/bg_800.png"
+            type="image/png">
+
+          <source media="(min-width: 801px) and (max-width: 991px)" srcset="/images/pages/roblox/header-bg/bg_991.webp"
+            type="image/webp">
+          <source media="(min-width: 801px) and (max-width: 991px)" srcset="/images/pages/roblox/header-bg/bg_991.png"
+            type="image/png">
+
+          <source media="(min-width: 992px) and (max-width: 993px)" srcset="/images/pages/roblox/header-bg/bg_993.webp"
+            type="image/webp">
+          <source media="(min-width: 992px) and (max-width: 993px)" srcset="/images/pages/roblox/header-bg/bg_993.png"
+            type="image/png">
+
+          <source media="(min-width: 994px) and (max-width: 1199px)" srcset="/images/pages/roblox/header-bg/bg_1199.webp"
+            type="image/webp">
+          <source media="(min-width: 994px) and (max-width: 1199px)" srcset="/images/pages/roblox/header-bg/bg_1199.png"
+            type="image/png">
+
+          <source media="(min-width: 1200px) and (max-width: 1300px)" srcset="/images/pages/roblox/header-bg/bg_1300.webp"
+            type="image/webp">
+          <source media="(min-width: 1200px) and (max-width: 1300px)" srcset="/images/pages/roblox/header-bg/bg_1300.png"
+            type="image/png">
+
+          <source media="(min-width: 1301px) and (max-width: 1600px)" srcset="/images/pages/roblox/header-bg/bg_1600.webp"
+            type="image/webp">
+          <source media="(min-width: 1301px) and (max-width: 1600px)" srcset="/images/pages/roblox/header-bg/bg_1600.png"
+            type="image/png">
+
+          <source media="(min-width: 1601px)" srcset="/images/pages/roblox/header-bg/bg_full.webp" type="image/webp">
+          <source media="(min-width: 1601px)" srcset="/images/pages/roblox/header-bg/bg_full.png" type="image/png">
+
+          <!-- Fallback for browsers that don't support the picture element -->
+          <img src="/images/pages/roblox/header-bg/bg_600.png">
+        </picture>
       </div>
       <div class="container">
         <div class="row headline-row">
-          <div class="col col-md-8">
+          <div class="col col-sm-8">
             <img class="coco-worlds-logo" src="/images/pages/roblox/coco-worlds.png">
-            <h1 class="text-headline">
-              {{ $t('roblox_landing.headline') }}
-            </h1>
-            <h2 class="text-subhead">
-              {{ $t('roblox_landing.subhead') }}
-            </h2>
+            <div class="text-container">
+              <h1 class="text-headline">
+                {{ $t('roblox_landing.headline') }}
+              </h1>
+              <h2 class="text-subhead">
+                {{ $t('roblox_landing.subhead') }}
+              </h2>
+            </div>
 
-            <button-main @click="openModal" :buttonText="$t('roblox_landing.join_the_beta')" class="button-main" />
+            <a href="https://www.roblox.com/games/11704713454/CodeCombat-Worlds" target="_blank">
+              <picture>
+                <source srcset="/images/pages/roblox/play_now.webp" type="image/webp">
+                <img src="/images/pages/roblox/play_now.png" class="header-button">
+              </picture>
+            </a>
           </div>
         </div>
       </div>
     </div>
     <div class="container-fluid container-fluid-boxes">
-      <div v-if="role" class="container container-boxes">  
-        <robloxButton class="roblox-connect"/>
+      <div v-if="role" class="container container-boxes">
+        <robloxButton class="roblox-connect" />
         <h3>{{ $t('roblox_landing.boxes_title') }}</h3>
         <div v-for="boxType in boxesByRole[role]" class="row" :class="`row-type-${boxType}`">
           <div class="col col-md-6 box-content ">
@@ -39,7 +81,16 @@
             </p>
           </div>
           <div class="col col-md-6">
-            <img :src="`/images/pages/roblox/${boxType}.png`">
+            <picture>
+              <source media="(max-width: 600px)" :srcset="`/images/pages/roblox/${boxType}_600.webp`" type="image/webp">
+              <source media="(max-width: 600px)" :srcset="`/images/pages/roblox/${boxType}_600.png`" type="image/png">
+
+              <source media="(min-width: 1601px)" :srcset="`/images/pages/roblox/${boxType}.webp`" type="image/webp">
+              <source media="(min-width: 1601px)" :srcset="`/images/pages/roblox/${boxType}.png`" type="image/png">
+
+              <img :src="`/images/pages/roblox/${boxType}_600.png`">
+            </picture>
+
           </div>
         </div>
         <div class="row">
@@ -62,7 +113,9 @@
 
         <div class="row row-faq">
           <div class="col-md-12">
-            <button-main :href="false" :buttonText="$t('contact.faq')" class="button-main" />
+            <button-main
+              href="https://codecombat.zendesk.com/hc/en-us/categories/18084251471383-CodeCombat-Worlds-Roblox-"
+              target="_blank" :buttonText="$t('contact.faq')" class="button-main" />
 
             <div class="item">
               <p class="question">{{ $t('roblox_landing.question_1') }}</p>
@@ -84,6 +137,8 @@
                 <li>{{ $t('roblox_landing.answer_3') }}</li>
               </ul>
             </div>
+
+            <p class="faq-bottom" v-html="$t('roblox_landing.faq_bottom')"></p>
           </div>
         </div>
       </div>
@@ -108,7 +163,9 @@
               Success
             </span>
             <span v-else>
-              You're in! First, <a href="https://www.roblox.com/groups/14987146/CodeCombat#!/about" target="_blank">join the CodeCombat Roblox group</a>, then <a href="https://www.roblox.com/games/11704713454/CodeCombat-Worlds" target="_blank">play CodeCombat Worlds</a>!
+              You're in! First, <a href="https://www.roblox.com/groups/14987146/CodeCombat#!/about" target="_blank">join
+                the CodeCombat Roblox group</a>, then <a href="https://www.roblox.com/games/11704713454/CodeCombat-Worlds"
+                target="_blank">play CodeCombat Worlds</a>!
             </span>
           </span>
           <button v-if="!isSuccess" class="btn btn-success btn-lg" type="submit" :disabled="inProgress">
@@ -176,25 +233,25 @@ export default {
     }
   },
   computed: {
-    isMobile() {
+    isMobile () {
       const userAgent = window.navigator.userAgent;
       const mobileDevices = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i;
-  
+
       return mobileDevices.test(userAgent);
     },
   },
 
   methods: {
-    openModal() {
+    openModal () {
       this.modalShown = true
     },
-    closeModal() {
+    closeModal () {
       this.modalShown = false
     },
-    validate() {
+    validate () {
       this.isValidEmail = this.email && forms.validateEmail(this.email)
     },
-    async onFormSubmit() {
+    async onFormSubmit () {
       this.validate()
       if (!this.isValidEmail) {
         return
@@ -236,16 +293,26 @@ $body-font: "Work Sans", "Open Sans", "sans serif";
 $box-content-margin: min(6vw, 90px);
 
 .asset {
-  content: '';
+  content: "";
   position: absolute;
   display: block;
-  width: 55%;
-  aspect-ratio: 16/9;
-  background-image: url('/images/pages/roblox/play-assets.png');
+  width: 30%;
+  aspect-ratio: 1/1;
+  background-image: url(/images/pages/roblox/play-assets.png);
+
+  @supports (background-image: url('/images/pages/roblox/play-assets.webp')) {
+    background-image: url('/images/pages/roblox/play-assets.webp')
+  }
+
   background-size: contain;
+  background-repeat: no-repeat;
   z-index: 1;
-  top: 37%;
-  left: -27.5%;
+  top: 27%;
+  left: -18.5%;
+
+  @media only screen and (max-width: 768px) {
+    display: none;
+  }
 }
 
 #roblox-page {
@@ -263,10 +330,11 @@ $box-content-margin: min(6vw, 90px);
   }
 
   h1.text-headline {
+    color: black;
     font-size: 40px;
     line-height: 1em;
     font-weight: 600;
-    text-shadow: 0em 0.0375em 0.28125em rgb(0 0 0 / 60%);
+    text-shadow: 0em 0.0375em 0.28125em rgb(255 255 255 / 90%);
 
     @media (max-width: $screen-md-min) {
       font-size: 20px;
@@ -274,9 +342,9 @@ $box-content-margin: min(6vw, 90px);
   }
 
   h2.text-subhead {
-    color: #e1dede;
+    color: black;
     font-size: 29px;
-    text-shadow: 0em 0.0375em 0.28125em rgb(0 0 0 / 60%);
+    text-shadow: 0em 0.0375em 0.28125em rgb(255 255 255 / 90%);
     line-height: 1.13em;
     font-weight: 600;
 
@@ -287,7 +355,8 @@ $box-content-margin: min(6vw, 90px);
     margin: 5px 0;
   }
 
-  >.container>.row, >.container-fluid-boxes>.container>.row {
+  >.container>.row,
+  >.container-fluid-boxes>.container>.row {
     margin-bottom: min(6.66vw, 100px);
 
     &:last-child {
@@ -316,6 +385,10 @@ $box-content-margin: min(6vw, 90px);
     p {
       margin: 0 $box-content-margin 14px;
     }
+
+    +div>img {
+      width: 100%;
+    }
   }
 
   .row-type-play,
@@ -329,10 +402,14 @@ $box-content-margin: min(6vw, 90px);
 
   .row-type-create {
     &:before {
-      top: -31%;
+      top: -18%;
       left: auto;
-      right: -30.7%;
+      right: -16.7%;
       background-image: url(/images/pages/roblox/create-assets.png);
+
+      @supports (background-image: url('/images/pages/roblox/create-assets.webp')) {
+        background-image: url('/images/pages/roblox/create-assets.webp')
+      }
     }
   }
 
@@ -351,6 +428,7 @@ $box-content-margin: min(6vw, 90px);
     margin-top: -50px;
     padding-top: 50px;
     overflow: hidden;
+    clear: both;
   }
 
   .container-boxes {
@@ -372,7 +450,7 @@ $box-content-margin: min(6vw, 90px);
       >.row {
         display: flex;
 
-        &:nth-child(even) {
+        &:nth-child(odd) {
           >.col:first-child {
             order: 1
           }
@@ -385,7 +463,8 @@ $box-content-margin: min(6vw, 90px);
     }
   }
 
-  >.container, >.container-fluid-boxes>.container {
+  >.container,
+  >.container-fluid-boxes>.container {
     >.row-video {
       margin-bottom: min(3vw, 40px);
 
@@ -401,6 +480,17 @@ $box-content-margin: min(6vw, 90px);
 
       @media (max-width: $screen-md-min) {
         font-size: 13px;
+      }
+
+      .faq-bottom {
+        margin-top: 60px;
+        margin-bottom: 30px;
+
+        ::v-deep {
+          a {
+            color: #FF9406;
+          }
+        }
       }
 
       .item {
@@ -422,18 +512,108 @@ $box-content-margin: min(6vw, 90px);
 
   .headline-container {
     margin-bottom: min(3.33vw, 50px);
-    background: black;
     position: relative;
-    overflow: hidden;
+    overflow: visible;
     position: relative;
+
+    @media screen and (max-width: 768px) {
+      display: flex;
+      flex-direction: column;
+    }
+
+    .container {
+      z-index: 2;
+      position: relative;
+
+      @media screen and (max-width: 768px) {
+        margin: 0;
+        padding: 0;
+
+        >* {
+          padding: 0 20px;
+        }
+      }
+    }
+
+
+    .responsive-background {
+      position: absolute;
+      overflow: hidden;
+      width: 100%;
+      height: 100%;
+
+      @media screen and (max-width: 768px) {
+        position: relative;
+        height: 275px;
+      }
+
+      &:before {
+        content: "";
+        position: absolute;
+        height: 100%;
+        aspect-ratio: 283 / 844;
+        background-image: url(/images/pages/roblox/tree.png);
+
+        @supports (background-image: url('/images/pages/roblox/tree.webp')) {
+          background-image: url('/images/pages/roblox/tree.webp')
+        }
+
+        background-size: contain;
+        left: 0;
+        z-index: 1;
+      }
+
+    }
+
+    .responsive-background img {
+      position: absolute;
+      top: 0;
+      right: 0;
+      bottom: 0;
+      left: 0;
+      width: 100%;
+      height: 100%;
+      object-fit: cover;
+      object-position: right center;
+    }
   }
 
   .headline-row {
     position: relative;
     padding-bottom: 0px;
 
+    @media screen and (max-width: 768px) {
+      background: white;
+    }
+
+
+    .text-container {
+      background: rgb(255 255 255 / 55%);
+      border-radius: 20px;
+      padding: 10px;
+      margin: 10px 0;
+    }
+
+
+    .header-button {
+      width: 190px;
+      margin-bottom: 30px;
+
+      &:hover {
+        filter: brightness(1.2);
+      }
+    }
+
     .col {
       padding-top: min(1.25vw, 20px);
+
+      @media screen and (max-width: 768px) {
+        text-align: center;
+        display: flex;
+        flex-direction: column;
+        justify-content: space-evenly;
+        align-items: center;
+      }
     }
 
   }
@@ -467,6 +647,7 @@ $box-content-margin: min(6vw, 90px);
       background-color: #fcd200;
     }
   }
+
   .roblox-connect {
     margin-bottom: 20px;
   }
