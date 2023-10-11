8c709b93902cb22bc28be160ea11905dced601f9
Saurabh Patel <saurabh.finch@gmail.com>
2023-10-11 18:51:08 +0530

    schema changes

--- app/schemas/models/user_credit.schema.js
+++ app/schemas/models/user_credit.schema.js
@@ -13,52 +13,22 @@ const UserCreditSchema = c.object({
           }
         ]
     }),
-    credits: c.array({
-      title: 'Credit by different types like hackstack, level_chat' // making it an array of objects so that we can field like endDate in future if needed
-    }, c.object({
-      title: 'Single credit type with its value'
-    }, {
-      key: c.shortString({
-        title: 'Credit type: level_chat, hackstack'
-      }),
-      value: c.int({
-        title: 'Number of credits available to use'
-      })
-    })),
     history: c.array(
       {
-        title: 'History of credit transactions'
+        title: 'History of transactions'
       },
       c.object(
         {
-          title: 'Credit transaction'
+          title: 'transaction'
         }, {
-          operation: c.shortString({
-            enum: ['add', 'sub']
-          }),
-          credit: c.object({
-            title: 'Type and value of credit'
-          }, {
-            type: c.shortString(),
-            value: c.int()
-          }),
-          transactionDate: c.date(),
-          source: c.object({
-            title: 'source of transaction'
-          }, {
-            info: c.shortString({
-              enum: ['webhook', 'admin']
-            }),
-            id: c.shortString({
-              title: 'objectid in case of webhook or any unique string to stop double credits'
-            })
-          })
+          action: c.shortString(),
+          transactionDate: c.date()
         }
       )
     )
   }
 })
 
-UserCreditSchema.required = ['userId', 'credits']
+UserCreditSchema.required = ['userId']
 
 module.exports = UserCreditSchema
