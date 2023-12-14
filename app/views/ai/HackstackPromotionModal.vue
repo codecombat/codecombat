cd6f2f499f9240f91eb10a431978afa6a84588ea
Saurabh Patel <saurabh.finch@gmail.com>
2023-12-14 11:26:11 +0530

    hackstack promotion modal

---
+++ app/views/ai/HackstackPromotionModal.vue
@@ -0,0 +1,23 @@
+<template>
+  <Modal
+    title="AI Hackstack"
+    :backbone-dismiss-modal="true"
+  >
+    <div class="hackstack-modal">
+      Hackstack
+    </div>
+  </Modal>
+</template>
+
+<script>
+import Modal from 'app/components/Modal.vue'
+export default {
+  name: 'HackstackPromotionModal',
+  components: {
+    Modal
+  }
+}
+</script>
+
+<style scoped lang="scss">
+</style>
