224a434b01e0bb2e529d60ba0ebc5bfd3f9c8744
Tong Yuqiang <tongsmallst@gmail.com>
2024-03-22 17:30:39 +0800

    adjust ai league stats to fit outcomes-report (#7325)
    * adjust ai league stats to fit outcomes-report
    
    * clean code
    
    * reorder sections
    
    ---------
    
    Co-authored-by: smallst_wsl <yuqiang_tong@163.com>

--- app/locale/en.js
+++ app/locale/en.js
@@ -4462,7 +4462,9 @@ module.exports = {
       schools: 'Schools',
       top_teacher_info: 'Teacher __A__, __B__ and __n__ other teachers use our platform',
       top_teacher_info_1: 'Teacher __A__ and __B__ use our platform',
-      top_teacher_info_2: 'Teacher __A__ uses our platform'
+      top_teacher_info_2: 'Teacher __A__ uses our platform',
+      ai_league: 'AI League',
+      ai_league_stats: '__n__ players played in AI League arenas'
     },
 
     efficacy: {
