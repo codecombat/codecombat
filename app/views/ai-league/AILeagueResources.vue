bb4a0ec0142f3161ea04c40070f96d3c98d0028c
adamkecskes <adam.k@codecombat.com>
2024-04-12 06:07:25 +0200

    ENG-336 - Add AI League to teacher dashboard & move navigation to left (#7334)
    * Add AI League to teacher dashboard & move navigation to left
    
    * remove unnecessary /ai-league route
    
    * hide ai-league from teacher navbar
    
    * make collapse button darker
    
    * move dashboard toggle to the bottom of navbar to save space in title bar
    
    * redirect to classes if new class was created on some other page
    
    * export current arena from seasonalLeague
    
    * new component for remaining time
    
    * new component for help tooltip
    
    * extract resources into a separate component
    
    * review fixes
    
    * make dashboard header fit better with left navbar
    
    * make sidebar edge softer

---
+++ app/views/ai-league/AILeagueResources.vue
@@ -0,0 +1,89 @@
+<script>
+
+import { currentRegularArena } from 'app/core/store/modules/seasonalLeague'
+
+export default {
+  name: 'AILeagueResources',
+  data () {
+    return {
+      regularArenaSlug: currentRegularArena ? currentRegularArena.slug : null,
+    }
+  }
+}
+</script>
+
+<template>
+  <div class="resources">
+    <a
+      class="resources__item screen"
+      href="https://docs.google.com/presentation/d/1ouDOu2k-pOxkWswUKuik7CbrUCkYXF7N_jNjGO0II6o/edit#slide=id.gb06b5c7fa4_0_10"
+      target="_blank"
+    >
+      {{ $t('league.teacher_getting_started_guide') }}
+    </a>
+    <a
+      class="resources__item view-exemplar"
+      :href="`https://codecombat.com/play/ladder/${regularArenaSlug ? regularArenaSlug : ''}`"
+      target="_blank"
+    >
+      {{ $t('league.try_ai_league_as_a_teacher') }}
+    </a>
+    <a
+      class="resources__item view-exemplar"
+      href="https://codecombat.zendesk.com/hc/en-us/categories/1500000915842-AI-League"
+      target="_blank"
+    >
+      {{ $t('nav.faqs') }}
+    </a>
+  </div>
+</template>
+
+<style scoped lang="scss">
+
+.resources {
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+
+  &__item {
+    display: flex;
+    align-items: center;
+    justify-content: flex-start;
+    border-radius: 8px;
+    border: 1px solid #476FB1;
+    background: white;
+    padding: 0 20px;
+    height: 50px;
+
+    color: #000;
+
+    font-feature-settings: 'clig' off, 'liga' off;
+    font-family: "Work Sans";
+    font-size: 16px;
+    font-style: normal;
+    font-weight: 600;
+    line-height: 17px;
+    position: relative;
+
+    &:after {
+      position: absolute;
+      content: '';
+      display: block;
+      width: 30px;
+      height: 30px;
+      background-image: url(/images/ozaria/teachers/dashboard/svg_icons/Icon_ViewExemplar.svg);
+      background-color: #F7D047;
+      background-repeat: no-repeat;
+      background-position: 4px 2px;
+      top: -15px;
+      right: -15px;
+      border-radius: 5px;
+    }
+
+    &.screen:after {
+      background-image: url(/images/ozaria/teachers/dashboard/svg_icons/Icon_Screen.svg);
+      background-color: #157A6C;
+    }
+  }
+}
+</style>
