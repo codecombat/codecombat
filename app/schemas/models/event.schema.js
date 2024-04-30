b70917b0503b308ce8a162baf137d21a46444d87
smallst_wsl <yuqiang_tong@163.com>
2024-04-30 16:52:41 +0800

    add additionalproperties in events

--- app/schemas/models/event.schema.js
+++ app/schemas/models/event.schema.js
@@ -37,6 +37,10 @@ const EventSchema = schema.object(
       description: 'meeting link of the event, i.e. zoom link'
     },
     state: { type: 'string', description: 'state of the event', enum: ['pending', 'active'] },
+    properties: {
+      type: 'object',
+      additionalProperties: true,
+    }
   }
 )
 
