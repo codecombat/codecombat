28bed1e658b1dc079fb1e78b2d9a5a20b6193459
adamkecskes <adam.k@codecombat.com>
2024-05-10 12:54:07 +0200

    Allow locking for unassigned chapters (#7409)
    * Allow locking for unassigned chapters
    
    * show error message if content was not assigned
    
    * update message text
    
    * update noty type to error

--- app/locale/en.js
+++ app/locale/en.js
@@ -4567,7 +4567,8 @@ module.exports = {
       no_modifiers_changed_unlocks_skipped: 'It appears you attempted to unlock levels that aren\'t currently locked. Please note, if your selected level appears as locked, it might be due to a preceding level being locked. To proceed, ensure to unlock the prior level first.',
       view_custom_league: 'View Custom League',
       view_global_league: 'View Global League',
-      refresh_progress: 'Refresh Progress'
+      refresh_progress: 'Refresh Progress',
+      assign_content_first: 'Please ensure that content is assigned to students before adjusting access levels',
     },
 
     outcomes: {
