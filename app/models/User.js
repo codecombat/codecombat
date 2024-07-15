6bc637c2055d2fa038b9c6ecce3f145512a752d3
adamkecskes <adam.k@codecombat.com>
2024-07-15 15:11:45 +0200

    ENG-894 - AI hackstack beta release and modal promotion (#7562)
    * AI hackstack beta release and modal promotion
    
    * review fixes
    
    * review fixes
    
    * reduce header size

--- app/models/User.js
+++ app/models/User.js
@@ -659,6 +659,17 @@ module.exports = (User = (function () {
       return 'expired'
     }
 
+    getSeenPromotion (key) {
+      const seenPromotions = this.get('seenPromotions') || {}
+      return seenPromotions[key]
+    }
+
+    setSeenPromotion (key) {
+      const seenPromotions = this.get('seenPromotions') || {}
+      Object.assign(seenPromotions, { [key]: true })
+      this.set('seenPromotions', seenPromotions)
+    }
+
     activeProducts (type) {
       const now = new Date()
       return _.filter(this.get('products'), p => (p.product === type) && ((new Date(p.endDate) > now) || !p.endDate))
---
+++ ozaria/site/components/teacher-dashboard/modals/ModalDynamicPromotion.vue
@@ -0,0 +1,111 @@
+<script>
+import Modal from '../../common/Modal'
+import utils from 'core/utils'
+import trackable from 'app/components/mixins/trackable.js'
+
+export default Vue.extend({
+  components: {
+    Modal,
+  },
+  mixins: [trackable],
+  props: {
+    title: {
+      type: String,
+      default: null,
+      required: false
+    },
+    seenPromotionsProperty: {
+      type: String,
+      required: true
+    },
+    cocoOnly: {
+      type: Boolean,
+      default: false
+    },
+    ozarOnly: {
+      type: Boolean,
+      default: false
+    }
+  },
+
+  data: () => {
+    return {
+      showModal: true
+    }
+  },
+
+  computed: {
+    isCodeCombat () {
+      return utils.isCodeCombat
+    },
+    isOzaria () {
+      return utils.isOzaria
+    }
+  },
+
+  watch: {
+    showModal (newVal) {
+      if (this.cocoOnly && this.isOzaria && newVal) {
+        this.hideModal()
+      }
+      if (this.ozarOnly && this.isCodeCombat && newVal) {
+        this.hideModal()
+      }
+    }
+  },
+
+  created () {
+    if (this.seenPromotionsProperty) {
+      this.showModal = !me.getSeenPromotion(this.seenPromotionsProperty)
+    }
+  },
+
+  methods: {
+    close () {
+      if (this.seenPromotionsProperty) {
+        me.setSeenPromotion(this.seenPromotionsProperty)
+        me.save()
+      }
+      this.hideModal()
+    },
+    hideModal () {
+      this.showModal = false
+      this.$emit('close')
+    },
+    onClose () {
+      this.close()
+    },
+  }
+})
+</script>
+
+<template>
+  <div>
+    <modal
+      v-if="showModal"
+      :title="title"
+      role="dialog"
+      aria-modal="true"
+      aria-labelledby="modal-title"
+      @close="onClose"
+    >
+      <slot name="content" />
+    </modal>
+    <slot
+      name="opener"
+      @click="showModal = true"
+    />
+  </div>
+</template>
+
+<style lang="scss" scoped>
+$header-height: 71px;
+::v-deep {
+  .modal-mask-fade {
+    padding-top: $header-height
+  }
+  .ozaria-modal-content {
+    max-height: calc(95vh - #{$header-height});
+  }
+}
+</style>
