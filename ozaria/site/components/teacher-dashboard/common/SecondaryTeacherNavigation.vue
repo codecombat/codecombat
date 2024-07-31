f5bc307dee76b0d0b3b8dfbd57814251ebff95b9
gabriellui1 <164280260+gabriellui1@users.noreply.github.com>
2024-07-31 11:31:16 -0400

    Curriculum sidebar promotion (#7593)
    * move curriculum button into a tab
    
    * promotion modal for curriculum tab in sidebar
    
    * fix overlapping bug with hackstack beta modal
    
    * convert image into webp and cleanup

--- ozaria/site/components/teacher-dashboard/common/SecondaryTeacherNavigation.vue
+++ ozaria/site/components/teacher-dashboard/common/SecondaryTeacherNavigation.vue
@@ -4,10 +4,12 @@ import utils from 'core/utils'
 import DashboardToggle from 'ozaria/site/components/teacher-dashboard/common/DashboardToggle'
 import sortClassroomMixin from '../mixins/sortClassroomMixin.js'
 import ModalHackStackBeta from 'ozaria/site/components/teacher-dashboard/modals/ModalHackStackBeta.vue'
+import ModalCurriculumPromotion from 'ozaria/site/components/teacher-dashboard/modals/ModalCurriculumPromotion.vue'
 
 export default {
   components: {
     DashboardToggle,
+    ModalCurriculumPromotion,
     ModalHackStackBeta
   },
 
@@ -19,6 +21,14 @@ export default {
     classrooms: {
       type: Array,
       default: () => []
+    }
+  },
+
+  data: () => {
+    return {
+      showCurriculumPromotion: false,
+      curriculumPromoClicked: false,
+      hackstackModalVisibility: false,
     }
   },
 
@@ -97,6 +107,14 @@ export default {
       this.$store.commit('teacherDashboard/setSelectedCourseIdCurrentClassroom', { courseId: utils.courseIDs.HACKSTACK })
     },
 
+    onCurriculumClicked (e) {
+      if (this.showCurriculumPromotion) {
+        this.curriculumPromoClicked = true
+        this.unhighlightCurriculumPromotion()
+      }
+      this.trackEvent(e)
+    },
+
     trackEvent (e) {
       const eventName = e.target.dataset.action
       const eventLabel = e.target.dataset.label
@@ -112,6 +130,18 @@ export default {
       if (this.hackStackClassrooms.length === 0) {
         noty({ text: $.i18n.t('teacher_dashboard.create_class_hackstack'), type: 'warning', layout: 'center', timeout: 5000 })
       }
+    },
+    highlightCurriculum () {
+      this.showCurriculumPromotion = true
+    },
+    unhighlightCurriculumPromotion () {
+      this.showCurriculumPromotion = false
+    },
+    hackstackModalShowing () {
+      this.hackstackModalVisibility = true
+    },
+    hackstackModalClose () {
+      this.hackstackModalVisibility = false
     }
   }
 }
@@ -225,6 +255,18 @@ export default {
           </a>
         </li>
       </ul>
+    </li>
+    <li :class="{ 'modal-highlight': showCurriculumPromotion && !hackstackModalVisibility }">
+      <router-link
+        id="CurriculumAnchor"
+        to="/teachers/curriculum"
+        :class="{ 'current-route': isCurrentRoute('/teachers/curriculum') || (showCurriculumPromotion && !hackstackModalVisibility) }"
+        data-action="Curriculum Guide: Nav Clicked"
+        @click.native="onCurriculumClicked"
+      >
+        <div id="IconCurriculum" />
+        {{ $t('teacher_dashboard.curriculum') }}
+      </router-link>
     </li>
     <li>
       <router-link
@@ -408,10 +450,17 @@ export default {
         reload-location="/teachers/classes"
       />
     </li>
+    <ModalCurriculumPromotion
+      :curriculum-clicked="curriculumPromoClicked"
+      @show="highlightCurriculum"
+      @close="unhighlightCurriculumPromotion"
+    />
     <ModalHackStackBeta
       v-if="showHackStack"
       :href="hackStackClassrooms.length>0 ? `/teachers/hackstack-classes/${hackStackClassrooms[0]._id}` : '#'"
       @tryClicked="hackstackClicked"
+      @show="hackstackModalShowing"
+      @close="hackstackModalClose"
     />
   </ul>
 </template>
@@ -726,7 +775,12 @@ li.open > #AILeague,
       color: #979797;
       cursor: default;
     }
+
   }
+}
+
+.modal-highlight {
+  z-index: 10000;
 }
 
 .dashboard-toggle {
