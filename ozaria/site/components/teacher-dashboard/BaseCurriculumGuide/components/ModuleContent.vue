1f3da1cc723121828c140eefa16f36240d370a45
Tong Yuqiang <tongsmallst@gmail.com>
2024-08-28 15:07:31 +0800

    hide practice levels for junior (#7652)
    * hide practice levels for junior
    
    * update pr
    
    * final fix comment
    
    ---------
    
    Co-authored-by: smallst_wsl <yuqiang_tong@163.com>

--- ozaria/site/components/teacher-dashboard/BaseCurriculumGuide/components/ModuleContent.vue
+++ ozaria/site/components/teacher-dashboard/BaseCurriculumGuide/components/ModuleContent.vue
@@ -69,6 +69,10 @@ export default {
       return this.getCurrentCourse?.name || ''
     },
 
+    isJunior () {
+      return this.courseName === 'Junior'
+    },
+
     getContentTypes () {
       return getCurriculumGuideContentList({
         introLevels: this.getModuleIntroLevels,
@@ -83,10 +87,16 @@ export default {
       const levels = this.getContentTypes
         .map(({ original, assessment, icon, fromIntroLevelOriginal }) => ({ original, key: (original || fromIntroLevelOriginal), assessment, practice: icon === 'practicelvl' }))
       return utils.createLevelNumberMap(levels)
-    }
+    },
   },
 
   methods: {
+    relatedLevels (level) {
+      const levelNumber = this.getLevelNumber(level.original)
+      const regex = new RegExp(`^${levelNumber}[a-z]?$`)
+      const levelOriginals = Object.entries(this.levelNumberMap).filter(([_k, value]) => regex.test(value.toString()))
+      return levelOriginals.map(([key, _v]) => this.getModuleInfo[this.moduleNum].find(l => l.original === key))
+    },
     getLevelNumber (original, index) {
       if (utils.isCodeCombat && this.classroomId) {
         const levelNumber = this.classroomInstance.getLevelNumber(original, index, this.getCurrentCourse?._id)
@@ -105,18 +115,31 @@ export default {
       event.stopPropagation()
       event.preventDefault()
       const level = this.getModuleInfo?.[this.moduleNum].find(l => l.slug === identifier)
-      if (hideCode) {
-        this.showCodeLevelSlugs = _.without(this.showCodeLevelSlugs, identifier)
-        return
+      const relatedLevels = this.relatedLevels(level)
+      for (const relatedLevel of relatedLevels) {
+        const identifier = relatedLevel.slug
+        if (hideCode) {
+          this.showCodeLevelSlugs = _.without(this.showCodeLevelSlugs, identifier)
+          continue
+        }
+        this.showCodeLevelSlugs = this.showCodeLevelSlugs.concat([identifier])
+        this.solutionCodeByLevel[identifier] = getSolutionCode(level, { lang: this.getSelectedLanguage }) || ''
+        this.sampleCodeByLevel[identifier] = getSampleCode(level, { lang: this.getSelectedLanguage }) || ''
       }
-      this.showCodeLevelSlugs = this.showCodeLevelSlugs.concat([identifier])
-      this.solutionCodeByLevel[identifier] = getSolutionCode(level, { lang: this.getSelectedLanguage }) || ''
-      this.sampleCodeByLevel[identifier] = getSampleCode(level, { lang: this.getSelectedLanguage }) || ''
     },
     onClickedCodeDiff (event) {
       // Stop it from triggering its parent <a> to start the level
       event.stopPropagation()
       event.preventDefault()
+    },
+    calculateLevelDescription (description, slug) {
+      const level = this.getModuleInfo?.[this.moduleNum].find(l => l.slug === slug)
+      const relatedLevels = this.relatedLevels(level)
+      const practiceNumber = relatedLevels.length - 1
+      if (!this.isJunior || practiceNumber === 0) {
+        return description
+      }
+      return `${description}. ${$.i18n.t('teacher_dashboard.practice_levels')}: ${practiceNumber}`
     }
   }
 }
@@ -142,19 +165,21 @@ export default {
           :icon-type="icon"
           :display-name="name"
         />
-        <module-row
-          v-else
-          :icon-type="icon"
-          :name-type="assessment ? null : icon"
-          :level-number="getLevelNumber(original, key + 1 )"
-          :display-name="name"
-          :description="description"
-          :is-part-of-intro="isPartOfIntro"
-          :show-code-btn="icon !== 'cutscene'"
-          :identifier="slug"
-          @click.native="trackEvent('Curriculum Guide: Individual content row clicked')"
-          @showCodeClicked="onShowCodeClicked"
-        />
+        <template v-else>
+          <module-row
+            v-if="!isJunior || icon !== 'practicelvl' || showCodeLevelSlugs.includes(slug)"
+            :icon-type="icon"
+            :name-type="assessment ? null : icon"
+            :level-number="getLevelNumber(original, key + 1 )"
+            :display-name="name"
+            :description="calculateLevelDescription(description, slug)"
+            :is-part-of-intro="isPartOfIntro"
+            :show-code-btn="icon !== 'cutscene' && !(isJunior && icon === 'practicelvl')"
+            :identifier="slug"
+            @click.native="trackEvent('Curriculum Guide: Individual content row clicked')"
+            @showCodeClicked="onShowCodeClicked"
+          />
+        </template>
         <code-diff
           v-if="showCodeLevelSlugs.includes(slug)"
           :key="slug"
@@ -169,20 +194,20 @@ export default {
 </template>
 
 <style lang="scss" scoped>
-  .content-rows {
-    background-color: white;
-    box-shadow: 2px 2px 4px rgba(0, 0, 0, 0.12);
+.content-rows {
+  background-color: white;
+  box-shadow: 2px 2px 4px rgba(0, 0, 0, 0.12);
 
-    margin-bottom: 29px;
+  margin-bottom: 29px;
 
-    // Supports both locked and unlocked views.
-    & a:nth-child(odd), & > div:nth-child(odd) {
-      background-color: #f2f2f2;
-    }
-
-    a {
-      display: block;
-      text-decoration: none;
-    }
+  // Supports both locked and unlocked views.
+  & a:nth-child(odd), & > div:nth-child(odd) {
+    background-color: #f2f2f2;
   }
+
+  a {
+    display: block;
+    text-decoration: none;
+  }
+}
 </style>
