5284d325b8572d9008e236b402d981fbacf5dafb
adamkecskes <adam.k@codecombat.com>
2024-09-02 11:12:10 +0200

    Coco JR Banner in teacher dashboard

---
+++ ozaria/site/components/teacher-dashboard/BaseTeacherDashboard/TopBanner.vue
@@ -0,0 +1,47 @@
+<template>
+  <div
+    v-if="showBanner"
+    id="top-banner"
+  >
+    <mixed-color-label
+      text="Try **CodeCombat Junior:** Our new K-5 curriculum, tablet-ready and perfect for all learners. **[https://codecombat.com/teachers/curriculum]Click to start now!**"
+    />
+  </div>
+</template>
+
+<script>
+import MixedColorLabel from 'app/components/common/labels/MixedColorLabel'
+
+export default {
+  components: {
+    'mixed-color-label': MixedColorLabel
+  },
+  computed: {
+    showBanner () {
+      const currentDate = new Date()
+      const start = new Date('2024-09-05')
+      const end = new Date('2024-09-25')
+      return currentDate >= start && currentDate <= end
+    }
+  }
+}
+</script>
+
+<style scoped lang="scss">
+@import "app/styles/component_variables.scss";
+
+#top-banner {
+    background: $teal-light-2;
+    padding: 10px 0px;
+    text-align: center;
+    font-size: 18px;
+    font-style: normal;
+    line-height: 20px;
+    ::v-deep {
+        .mixed-color-label__highlight {
+            color: var(--color-dark-grey);
+            font-weight: bold;
+        }
+    }
+}
+</style>
