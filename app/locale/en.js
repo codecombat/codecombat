4cec5ebe72dc216fb216b1952a9dee8fbf07e475
gabriellui1 <164280260+gabriellui1@users.noreply.github.com>
2024-09-19 14:26:18 -0400

    Add AI leauge to ozaria using modal popup (#7701)
    * add ai leauge to ozaria with modal popup
    
    * set component name
    
    * fixed typo
    
    * use component tag to avoid code duplication

--- app/locale/en.js
+++ app/locale/en.js
@@ -4687,7 +4687,9 @@ module.exports = {
       test_student_only: '(Test Student Only)',
       ready_to_review: 'Ready to Review',
       ozaria_hs_modal_title: 'Access AI Hackstack Exclusively on CodeCombat',
-      ozaria_hs_modal_blurb: 'Educators can now access AI Hackstack directly on **[https://codecombat.com/teachers/classes]CodeCombat** using the same credentials as Ozaria. For added convenience, you can easily import your classes from Ozaria into CodeCombat.'
+      ozaria_hs_modal_blurb: 'Educators can now access AI Hackstack directly on **[https://codecombat.com/teachers/ai-league]CodeCombat** using the same credentials as Ozaria. For added convenience, you can easily import your classes from Ozaria into CodeCombat.',
+      ozaria_ai_league_modal_title: 'Access AI League Exclusively on CodeCombat',
+      ozaria_ai_league_modal_blurb: 'Educators can now access AI League directly on **[https://codecombat.com/teachers/ai-league]CodeCombat** using the same credentials as Ozaria. For added convenience, you can easily import your classes from Ozaria into CodeCombat.'
     },
 
     outcomes: {
