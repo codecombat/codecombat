f65f4c2d287dcf330c15baba5a4faf9e36aa9443
gabriellui1 <164280260+gabriellui1@users.noreply.github.com>
2024-09-24 14:03:55 -0400

    added status property for license requests (#7675)

--- app/schemas/models/license_request.schema.js
+++ app/schemas/models/license_request.schema.js
@@ -17,7 +17,8 @@ _.extend(LicenseRequestSchema.properties, {
   phone: c.shortString({ title: 'Phone number' }),
   district: c.shortString({ title: 'District' }),
   role: c.shortString({ title: 'Teacher, principal etc' }),
-  school: c.shortString({ title: 'School' })
+  school: c.shortString({ title: 'School' }),
+  status: { type: 'string', enum: ['CREATED', 'SLACK_SENT', 'FAILED', 'PROCESSING'] },
 })
 
 c.extendBasicProperties(LicenseRequestSchema, 'LicenseRequest')
