8ba51b922ae12c3ec0f0f94cc20954d83fb6538b
gabriel lui <gabriel.lui314@gmail.com>
2024-09-29 17:51:13 -0400

    fix typo

--- ozaria/site/components/teacher-dashboard/BaseCurriculumGuide/components/ModalCapstoneProject.vue
+++ ozaria/site/components/teacher-dashboard/BaseCurriculumGuide/components/ModalCapstoneProject.vue
@@ -16,7 +16,7 @@ export default Vue.extend({
       },
       requirements: {
         'introduction-to-computer-science': ['Your dragon must be at least half the width and height of your viewing window.', 'You must meaningfully use two variables in your program.'],
-        'computer-science-2': ['Your game includes at least 3 questions that a user can answer.', 'Different output is generated depending on the input provided by the user.', 'You meaningfully define and call a function that uses at least one paramater.'],
+        'computer-science-2': ['Your game includes at least 3 questions that a user can answer.', 'Different output is generated depending on the input provided by the user.', 'You meaningfully define and call a function that uses at least one parameter.'],
         'computer-science-3': ['At least one input must be randomly generated.', 'Output must change depending on the input randomly generated.', 'Define and call a function that uses at least one parameter and one return statement.'],
         'computer-science-4': ['Users should be able to provide input in order to play the game, and this input should influence game play in some way.', 'Use at least one list in your program.', 'Define and call a function that uses at least one parameter and one return statement.'],
       }
