e18d779fc909974bdda3e56c332017f1bc8a7305
adamkecskes <adam.k@codecombat.com>
2024-11-19 14:46:23 +0100

    Chapter 2 student navigation on map

--- ozaria/site/components/play/PageUnitMap/common/UnitMapBackground.vue
+++ ozaria/site/components/play/PageUnitMap/common/UnitMapBackground.vue
@@ -10,57 +10,57 @@ export default Vue.extend({
   components: {
     'level-dot': levelDot,
     'modal-char-customization': ModalCharCustomization,
-    UnitMapNav
+    UnitMapNav,
   },
   props: {
     campaignData: {
       type: Object,
       required: true,
-      default: () => {}
+      default: () => {},
     },
     levels: {
       type: Object,
       required: true,
-      default: () => {}
+      default: () => {},
     },
     campaignPage: {
       type: Number,
-      default: 1
+      default: 1,
     },
     courseId: {
       type: String,
-      default: undefined
+      default: undefined,
     },
     courseInstanceId: {
       type: String,
-      default: undefined
+      default: undefined,
     },
     codeLanguage: {
       type: String,
-      default: undefined
+      default: undefined,
     },
     classroomId: {
       type: String,
-      default: ''
-    }
+      default: '',
+    },
   },
   data: () => ({
     showCharCx: false,
-    heroName: undefined
+    heroName: undefined,
   }),
   computed: {
     ...mapGetters({
       isAnonymous: 'me/isAnonymous',
       isTeacher: 'me/isTeacher',
       isStudent: 'me/isStudent',
-      getLevelNumber: 'gameContent/getLevelNumber'
+      getLevelNumber: 'gameContent/getLevelNumber',
     }),
     backgroundImage: function () {
       if (this.campaignData.backgroundImage) {
         // Fetch the background relevant for current campaign page
         const background = this.campaignData.backgroundImage.find((b) => b.campaignPage === this.campaignPage) || {}
         return {
-          'background-image': 'url(/file/' + background.image + ')'
+          'background-image': 'url(/file/' + background.image + ')',
         }
       }
       return undefined
@@ -85,7 +85,7 @@ export default Vue.extend({
       } else {
         return '/'
       }
-    }
+    },
   },
   mounted () {
     // heroName is not working as computed property for some reason
@@ -96,7 +96,7 @@ export default Vue.extend({
   },
   methods: {
     ...mapActions({
-      generateLevelNumberMap: 'gameContent/generateLevelNumberMap'
+      generateLevelNumberMap: 'gameContent/generateLevelNumberMap',
     }),
 
     customizeHero () {
@@ -116,12 +116,22 @@ export default Vue.extend({
           courseInstanceId: this.courseInstanceId,
           campaignPage: page,
           campaignId: this.campaignData._id,
-          codeLanguage: this.codeLanguage
+          codeLanguage: this.codeLanguage,
         })
         return application.router.navigate(url, { trigger: true })
       }
-    }
-  }
+    },
+    previousPage () {
+      if (this.campaignPage > 1) {
+        this.clickPageNav(this.campaignPage - 1)
+      }
+    },
+    nextPage () {
+      if (this.campaignPage < this.totalPages) {
+        this.clickPageNav(this.campaignPage + 1)
+      }
+    },
+  },
 })
 </script>
 
@@ -148,15 +158,25 @@ export default Vue.extend({
       v-if="showNavDots"
       id="dot-nav"
     >
+      <div
+        class="arrow left"
+        :class="{ inactive: campaignPage === 1 }"
+        @click="previousPage"
+      />
       <ul>
         <li
           v-for="page in totalPages"
           :key="page"
-          :class="{ active: page === campaignPage }"
+          :class="{ selected: page === campaignPage }"
           :title="page"
           @click="clickPageNav(page)"
         />
       </ul>
+      <div
+        class="arrow right"
+        :class="{ inactive: campaignPage === totalPages }"
+        @click="nextPage"
+      />
     </div>
     <modal-char-customization
       v-if="showCharCx"
@@ -190,11 +210,55 @@ export default Vue.extend({
     position: absolute
     z-index: 999
     bottom: 15px
+    display: flex
+    background-color: rgba(0, 0, 0, 0.6)
+    padding: 10px
+    border-radius: 20px
+
+    .arrow
+      width: 0
+      height: 0
+      border-style: solid
+      position: relative
+      cursor: pointer
+
+      &::before
+        content: ""
+        position: absolute
+        width: 0
+        height: 0
+        border-style: solid
+        z-index: 1
+
+      &.inactive
+        opacity: 0.5
+        cursor: default
+
+      &.left
+        border-width: 20px 30px 20px 0
+        border-color: transparent white transparent transparent
+        &::before
+          border-width: 15px 25px 15px 0
+          border-color: transparent #bdc3c7 transparent transparent
+          left: 3px
+          top: -15px
+
+      &.right
+        border-width: 20px 0 20px 30px
+        border-color: transparent transparent transparent white
+        &::before
+          border-width: 15px 0 15px 25px
+          border-color: transparent transparent transparent #bdc3c7
+          right: 3px
+          top: -15px
 
     ul
       list-style: none
       margin: 0
       padding: 0
+      display: flex
+      justify-content: center
+      align-items: center
 
     li
       position: relative
@@ -205,11 +269,12 @@ export default Vue.extend({
       cursor: pointer
       padding: 5px
       height: 10px
-      margin: 10px 10px 0px 0px
+      margin: 10px 10px 10px 10px
       width: 10px
       vertical-align: bottom
 
-    li.active
+    li.selected
       background-color: #3c4f69
+      cursor: default
 
 </style>
