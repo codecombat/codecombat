e0851c33233aa9c06480570053533d049e23ec13
Valentin Briukhanov <bvv.mag@gmail.com>
2025-04-08 11:31:57 +0200

    Remove decaffeinate comments and clean up JuniorOriginalChoiceModal.js

--- app/views/core/JuniorOriginalChoiceModal.js
+++ app/views/core/JuniorOriginalChoiceModal.js
@@ -1,10 +1,3 @@
-// TODO: This file was created by bulk-decaffeinate.
-// Sanity-check the conversion and remove this comment.
-/*
- * decaffeinate suggestions:
- * DS206: Consider reworking classes to avoid initClass
- * Full docs: https://github.com/decaffeinate/decaffeinate/blob/main/docs/suggestions.md
- */
 let JuniorOriginalChoiceModal
 require('app/styles/modal/junior-original-choice-modal.sass')
 const ModalView = require('views/core/ModalView')
