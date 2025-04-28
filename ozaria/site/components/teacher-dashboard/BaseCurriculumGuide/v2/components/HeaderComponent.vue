75aee9cd6e9f4bf8e5be7b6e37704b0b7d6b3355
Saurabh Patel <saurabh.finch@gmail.com>
2025-04-28 14:27:22 +0700

    adding tabs and explore component for guide

---
+++ ozaria/site/components/teacher-dashboard/BaseCurriculumGuide/v2/components/HeaderComponent.vue
@@ -0,0 +1,94 @@
+<template>
+  <div class="header">
+    <div class="header__products">
+      <div
+        :class="{ header__product: true, header__product__selected: selectedTab === 'guide' }"
+        @click.prevent="() => onTabClicked('guide')"
+      >
+        {{ product }}
+      </div>
+      <div
+        :class="{ header__product: true, header__product__selected: selectedTab === 'explore' }"
+        @click.prevent="() => onTabClicked('explore')"
+      >
+        {{ $t('parents_v2.step_box_2_subtitle') }}
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HeaderComponent',
+  props: {
+    product: {
+      type: String,
+      required: true,
+    },
+    defaultTab: {
+      type: String,
+      default: 'guide',
+      required: true,
+    },
+  },
+  data () {
+    return {
+      selectedTab: this.defaultTab,
+    }
+  },
+  methods: {
+    onTabClicked (tab) {
+      this.selectedTab = tab
+      this.$emit('onSelectedTabChange', tab)
+    },
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+@import "app/styles/bootstrap/variables";
+@import "app/views/parents/css-mixins/variables";
+
+.header {
+  &__products {
+    display: flex;
+    padding-top: .5rem;
+
+    @media (max-width: $screen-lg) {
+      flex-direction: column;
+    }
+  }
+
+  &__product {
+    background: $color-green-1;
+    border: 1px solid $color-green-1;
+    box-shadow: 4px 0 7px rgba(0, 0, 0, 0.2), 0px -2px 5px rgba(0, 0, 0, 0.1);
+
+    margin-left: 1rem;
+    margin-right: 1rem;
+
+    border-top-left-radius: 5px;
+    border-top-right-radius: 5px;
+
+    padding: 2px 1rem;
+    cursor: pointer;
+
+    text-transform: uppercase;
+
+    &:not(:last-child) {
+      @media (max-width: $screen-lg) {
+        margin-bottom: 1rem;
+      }
+    }
+
+    &__selected {
+      background: $color-grey-2;
+      border: 1px solid #E6E6E6;
+    }
+  }
+
+  &__logos {
+    height: 2rem;
+  }
+}
+</style>
---
+++ ozaria/site/components/teacher-dashboard/BaseCurriculumGuide/v2/index.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="guide">
+    <header-component
+      :product="product"
+      :default-tab="selectedTab"
+      @onSelectedTabChange="onSelectedTabChange"
+    />
+    <explore-component
+      v-if="selectedTab === 'explore'"
+      :product="product"
+    />
+  </div>
+</template>
+
+<script>
+import HeaderComponent from './components/HeaderComponent.vue'
+import ExploreComponent from './components/ExploreComponent.vue'
+export default {
+  name: 'BaseCurriculumGuideV2',
+  components: {
+    HeaderComponent,
+    ExploreComponent,
+  },
+  props: {
+    product: {
+      type: String,
+      required: true,
+    },
+  },
+  data () {
+    return {
+      selectedTab: 'guide',
+    }
+  },
+  methods: {
+    onSelectedTabChange (tab) {
+      this.selectedTab = tab
+    },
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+.guide {
+
+}
+</style>
