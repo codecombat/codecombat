f251e3827a96c0a0446e8024967170652439ddc4
Chaboi3000 <24kajih@asij.ac.jp>
2025-08-26 20:04:48 +1000

    Formatting fixes + add actual functionality

--- ozaria/site/components/teacher-dashboard/modals/ModalEndOfTrial.vue
+++ ozaria/site/components/teacher-dashboard/modals/ModalEndOfTrial.vue
@@ -12,61 +12,77 @@
         <p class="schedule-demo">
           {{ $t('teachers.schedule_demo_subheading') }}
         </p>
-        <div class="purple-separator"></div>
+        <div class="purple-separator" />
         <div class="subheadings">
-            <div class="subheading-container">
-                <p class="subheading">{{ $t('schools_page.core_curriculum') }}</p>
-            </div>
-            <div class="subheading-container">
-                <p class="subheading">{{ $t('teachers.practice_and_application') }}</p>
-            </div>
+          <div class="subheading-container">
+            <p class="subheading">
+              {{ $t('schools_page.core_curriculum') }}
+            </p>
+          </div>
+          <div class="subheading-container">
+            <p class="subheading">
+              {{ $t('teachers.practice_and_application') }}
+            </p>
+          </div>
         </div>
         <div class="experiences">
-            <div class="experiences-box">
-                <div class="card-row">
-                    <div class="card">
-                        <img src="/images/common/modal/ozaria_promo.webp">
-                        <div class="card-title">{{ $t('schools_page.core_curriculum_1_title') }}</div>
-                    </div>
-                    <div class="card">
-                        <img src="/images/common/modal/coco_promo.webp">
-                        <div class="card-title">{{ $t('schools_page.core_curriculum_2_title') }}</div>
-                    </div>
+          <div class="experiences-box">
+            <div class="card-row">
+              <div class="card">
+                <img src="/images/common/modal/ozaria_promo.webp">
+                <div class="card-title">
+                  {{ $t('schools_page.core_curriculum_1_title') }}
                 </div>
-                <div class="card-row center">
-                    <div class="card">
-                        <img src="/images/common/modal/coco_junior_promo.webp">
-                        <div class="card-title">{{ $t('schools_page.young_learners_1_title_classroom') }}</div>
-                    </div>
+              </div>
+              <div class="card">
+                <img src="/images/common/modal/coco_promo.webp">
+                <div class="card-title">
+                  {{ $t('schools_page.core_curriculum_2_title') }}
                 </div>
+              </div>
             </div>
-            <div class="experiences-box">
-                <div class="card-row">
-                    <div class="card">
-                        <img src="/images/common/modal/ai_league_promo.webp">
-                        <div class="card-title">{{ $t('nav.ai_league_esports') }}</div>
-                    </div>
-                    <div class="card">
-                        <img src="/images/common/modal/ccw_promo.webp">
-                        <div class="card-title">{{ $t('schools_page.codecombat_worlds') }}</div>
-                    </div>
+            <div class="card-row center">
+              <div class="card">
+                <img src="/images/common/modal/coco_junior_promo.webp">
+                <div class="card-title">
+                  {{ $t('schools_page.young_learners_1_title_classroom') }}
                 </div>
-                <div class="card-row center">
-                    <div class="card">
-                        <img src="/images/common/modal/hackstack_promo.webp">
-                        <div class="card-title">{{ $t('schools_page.ai_hackstack') }}</div>
-                    </div>
-                </div>
+              </div>
             </div>
+          </div>
+          <div class="experiences-box">
+            <div class="card-row">
+              <div class="card">
+                <img src="/images/common/modal/ai_league_promo.webp">
+                <div class="card-title">
+                  {{ $t('nav.ai_league_esports') }}
+                </div>
+              </div>
+              <div class="card">
+                <img src="/images/common/modal/ccw_promo.webp">
+                <div class="card-title">
+                  {{ $t('schools_page.codecombat_worlds') }}
+                </div>
+              </div>
+            </div>
+            <div class="card-row center">
+              <div class="card">
+                <img src="/images/common/modal/hackstack_promo.webp">
+                <div class="card-title">
+                  {{ $t('schools_page.ai_hackstack') }}
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
         <p class="text-p">
           {{ $t('teachers.schedule_demo_description') }}
         </p>
         <CTAButton
-            class="request-demo"
-            @clickedCTA="showContactModal = true"
-          >
-            {{ $t('schools_page.request_a_demo') }}
+          class="request-demo"
+          @clickedCTA="sendToContactModal"
+        >
+          {{ $t('schools_page.request_a_demo') }}
         </CTAButton>
       </div>
     </template>
@@ -85,19 +101,22 @@ export default {
   },
   computed: {
     ...mapGetters({
-        activeClassrooms: 'teacherDashboard/getActiveClassrooms',
-        sharedClassrooms: 'teacherDashboard/getSharedClassrooms',
-        activeLicenses: 'teacherDashboard/getActiveLicenses',
-        loadingLicenses: 'teacherDashboard/getLoadingState'
+      activeClassrooms: 'teacherDashboard/getActiveClassrooms',
+      sharedClassrooms: 'teacherDashboard/getSharedClassrooms',
+      activeLicenses: 'teacherDashboard/getActiveLicenses',
+      loadingLicenses: 'teacherDashboard/getLoadingState',
     }),
     hasLicense () {
-        return !this.loadingLicenses && this.activeLicenses.length > 0
+      return !this.loadingLicenses && this.activeLicenses.length > 0
     },
     showModal () {
-        return !this.hasLicense // THIS NEEDS UPDATING: 
+      return !this.hasLicense // THIS NEEDS UPDATING:
     },
   },
   methods: {
+    sendToContactModal () {
+      window.open('/schools?openContactModal=true', '_blank', 'noopener,noreferrer')
+    },
     openModal () {
       this.$refs.modal.openModal()
     },
@@ -148,7 +167,7 @@ export default {
         margin-top: 15px;
     }
     .experiences {
-        gap: 40px; 
+        gap: 40px;
         margin-top: 20px;
         width: 100%;
         display: flex;
@@ -157,8 +176,8 @@ export default {
         flex-direction: column;
         align-items: center;
         width: 50%;
-        gap: 20px;             
-        flex: 1;                
+        gap: 20px;
+        flex: 1;
         max-width: 360px;
     }
     .subheading{
@@ -178,8 +197,8 @@ export default {
         overflow: hidden;
         box-shadow: 0 4px 12px rgba(0,0,0,0.15);
         background: var(--color-primary-1);
-        flex: 1 1 0; 
-        width: 150px; 
+        flex: 1 1 0;
+        width: 150px;
         height: 140px;
         max-height: 140px;
         max-width: 150px;
@@ -196,10 +215,10 @@ export default {
         font-size: 12px;
         font-weight: 500;
         padding: 12px;
-        display: flex;           
+        display: flex;
         align-items: flex-start;
         height: 40px;
-        justify-content: center; 
+        justify-content: center;
     }
 }
 </style>
