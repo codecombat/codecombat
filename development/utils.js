23f4605f4832651d388cebb8f95b44587b9c0069
smallst_wsl <yuqiang_tong@163.com>
2025-09-05 21:46:38 +0800

    disable bing.com to find staging

--- development/utils.js
+++ development/utils.js
@@ -1,9 +1,11 @@
 const product = process.env.COCO_PRODUCT || 'codecombat'
 const productSuffix = { codecombat: 'coco', ozaria: 'ozar' }[product]
 const publicFolderName = 'public_' + productSuffix
+const isStaging = process.env.COCO_IS_STAGING_SERVER
 
 module.exports = {
   product,
   productSuffix,
-  publicFolderName
+  publicFolderName,
+  isStaging,
 }
