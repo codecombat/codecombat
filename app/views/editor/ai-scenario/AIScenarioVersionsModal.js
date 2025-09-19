3383460399dfcaf294372b621e446b5917bcfdd5
smallst_wsl <yuqiang_tong@163.com>
2025-09-19 10:02:23 +0800

    fix comments

--- app/views/editor/ai-scenario/AIScenarioVersionsModal.js
+++ app/views/editor/ai-scenario/AIScenarioVersionsModal.js
@@ -1,18 +1,10 @@
-// TODO: This file was created by bulk-decaffeinate.
-// Sanity-check the conversion and remove this comment.
-/*
- * decaffeinate suggestions:
- * DS002: Fix invalid constructor
- * DS206: Consider reworking classes to avoid initClass
- * Full docs: https://github.com/decaffeinate/decaffeinate/blob/main/docs/suggestions.md
- */
 let AIScenarioVersionsModal
 const VersionsModal = require('views/editor/modal/VersionsModal')
 
 module.exports = (AIScenarioVersionsModal = (function () {
   AIScenarioVersionsModal = class AIScenarioVersionsModal extends VersionsModal {
     static initClass () {
-      this.prototype.id = 'editor-cenario-versions-view'
+      this.prototype.id = 'editor-scenario-versions-view'
       this.prototype.url = '/db/ai_scenario/'
       this.prototype.page = 'ai-scenario'
     }
