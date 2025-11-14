fc10ecf7d6e5aa404b24e953fc33c1895cdd240f
Saurabh Patel <saurabh.finch@gmail.com>
2025-11-14 15:47:18 +0530

    post oz merge view fixes (#8149)

--- app/core/utils.js
+++ app/core/utils.js
@@ -221,6 +221,12 @@ var titleize = s => // Turns things like 'dungeons-of-kithgard' into 'Dungeons o
     word => word.toLowerCase(),
   )
 
+const OZ_COURSE_IDS_MAP = {
+  CHAPTER_ONE: '5d41d731a8d1836b5aa3cba1',
+  CHAPTER_TWO: '5d8a57abe8919b28d5113af1',
+  CHAPTER_THREE: '5e27600d1c9d440000ac3ee7',
+  CHAPTER_FOUR: '5f0cb0b7a2492bba0b3520df',
+}
 if (isCodeCombat) {
   campaignIDs = {
     JUNIOR: '65c56663d2ca2055e65676af',
@@ -255,12 +261,7 @@ if (isCodeCombat) {
     courseIDs.GAME_DEVELOPMENT_3,
   ]
 
-  otherCourseIDs = {
-    CHAPTER_ONE: '5d41d731a8d1836b5aa3cba1',
-    CHAPTER_TWO: '5d8a57abe8919b28d5113af1',
-    CHAPTER_THREE: '5e27600d1c9d440000ac3ee7',
-    CHAPTER_FOUR: '5f0cb0b7a2492bba0b3520df',
-  }
+  otherCourseIDs = OZ_COURSE_IDS_MAP
 
   CSCourseIDs = [
     courseIDs.JUNIOR,
@@ -310,12 +311,7 @@ if (isCodeCombat) {
   freeCampaignIds = [campaignIDs.CHAPTER_ONE] // CH1 campaign
   internalCampaignIds = ['5eb34fc8dc0fd35e8eae66b0'] // CH2 playtest
 
-  courseIDs = {
-    CHAPTER_ONE: '5d41d731a8d1836b5aa3cba1',
-    CHAPTER_TWO: '5d8a57abe8919b28d5113af1',
-    CHAPTER_THREE: '5e27600d1c9d440000ac3ee7',
-    CHAPTER_FOUR: '5f0cb0b7a2492bba0b3520df',
-  }
+  courseIDs = OZ_COURSE_IDS_MAP
 
   otherCourseIDs = {
     JUNIOR: '65f32b6c87c07dbeb5ba1936',
@@ -375,6 +371,12 @@ const JUNIOR_COURSE_IDS = [
 ]
 const HACKSTACK_COURSE_IDS = [
   courseIDs.HACKSTACK,
+]
+const OZ_COURSE_IDS = [
+  OZ_COURSE_IDS_MAP.CHAPTER_ONE,
+  OZ_COURSE_IDS_MAP.CHAPTER_TWO,
+  OZ_COURSE_IDS_MAP.CHAPTER_THREE,
+  OZ_COURSE_IDS_MAP.CHAPTER_FOUR,
 ]
 
 const allCourseIDs = _.assign(courseIDs, otherCourseIDs)
@@ -1901,6 +1903,7 @@ module.exports = {
   shaTag,
   JUNIOR_COURSE_IDS,
   HACKSTACK_COURSE_IDS,
+  OZ_COURSE_IDS,
 }
 
 function __guard__ (value, transform) {
