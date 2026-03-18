54b7f42b1545e23862e62b77b27ae7e9010cfed7
Valentin Briukhanov <bvv.mag@gmail.com>
2026-03-18 15:17:43 +0100

    Refactor countLevels method to conditionally ignore practice levels based on campaign type. Update level counting logic in CampaignView.js.

--- app/views/play/CampaignView.js
+++ app/views/play/CampaignView.js
@@ -762,7 +762,8 @@ class CampaignView extends RootView {
       context.levels = _.filter(context.levels, level => ['dungeons-of-kithgard', 'gems-in-the-deep', 'shadow-guard', 'enemy-mine', 'true-names'].includes(level.slug))
     }
     this.annotateLevels(context.levels)
-    const count = this.countLevels(context.levels)
+    const dontCountPracticeLevels = context.campaign?.get('type') === 'junior' || context.campaign?.get('slug') === 'junior'
+    const count = this.countLevels(context.levels, dontCountPracticeLevels)
     if (this.courseStats) {
       context.levelsCompleted = this.courseStats.levels.numDone
       context.levelsTotal = this.courseStats.levels.size
@@ -1223,17 +1224,18 @@ class CampaignView extends RootView {
     return null
   }
 
-  countLevels (orderedLevels) {
+  countLevels (orderedLevels, ignorePracticeLevels = false) {
     const count = { total: 0, completed: 0, unlocked: 0 }
 
     if (this.campaign?.get('type') === 'hoc') {
       // HoC: Just order left-to-right instead of looking at unlocks, which we don't use for this copycat campaign
       orderedLevels = _.sortBy(orderedLevels, level => level.position.x)
       for (const level of orderedLevels) {
+        if (ignorePracticeLevels && level.practice) { continue }
         if (this.levelStatusMap[level.slug] === COMPLETE_STATUS) { count.completed++ }
         if (!level.locked) { ++count.unlocked }
+        ++count.total
       }
-      count.total = orderedLevels.length
       return count
     }
 
@@ -1242,6 +1244,7 @@ class CampaignView extends RootView {
       if (level.locked == null) { this.annotateLevels(orderedLevels) } // Annotate if we haven't already.
       if (!level.locked) { ++count.unlocked }
       if (level.disabled) { continue }
+      if (ignorePracticeLevels && level.practice) { continue }
       const completed = this.levelStatusMap[level.slug] === COMPLETE_STATUS
       const started = this.levelStatusMap[level.slug] === STARTED_STATUS
       if ((level.unlockedInSameCampaign || !level.locked) && (started || completed || !(level.locked && level.practice && /-[a-z]$/.test(level.slug)))) {
