e2bf60b66c68bfbcbcc6918cf0c109e5e8bb4327
smallst_wsl <yuqiang_tong@163.com>
2026-07-07 17:33:16 +0800

    skip hs course for export progress for now

--- app/lib/studentProgressCalculator.js
+++ app/lib/studentProgressCalculator.js
@@ -45,11 +45,10 @@ module.exports = {
       for (trimCourse of Array.from(classroom.getSortedCourses())) {
         course = courses.get(trimCourse._id)
         if (utils.HACKSTACK_COURSE_IDS.includes(trimCourse._id)) {
-          continue
+          continue // bypass the hs courses for now
         }
         const instance = courseInstances.findWhere({ courseID: course.id, classroomID: classroom.id })
         if (instance && instance.hasMember(student)) {
-
           for (trimLevel of Array.from(trimCourse.levels)) {
             level = levels.findWhere({ original: trimLevel.original })
             if (level.get('assessment')) { continue }
