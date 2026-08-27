a922550e7bb30faa67c964915c95468b84f127f9
smallst_wsl <yuqiang_tong@163.com>
2026-08-27 14:56:21 +0800

    fix ai comments

--- app/models/Classroom.js
+++ app/models/Classroom.js
@@ -350,7 +350,7 @@ module.exports = (Classroom = (function () {
     }
 
     statsForAIProjects (sessions, courseId) {
-      const emptyResults = { levels: { size: 0, left: 0, done: true, numDone: 0, pctDone: '0.0%', completedCapstones: 0 }, courseComplete: false }
+      const emptyResults = { levels: { size: 0, left: 0, done: true, numDone: 0, pctDone: '0.0%', completedCapstones: 0 }, courseComplete: false, totalMessages: 0 }
       const course = this.get('courses')?.find(c => c._id === courseId)
       if (!course) {
         return emptyResults
@@ -373,7 +373,7 @@ module.exports = (Classroom = (function () {
         }
         const isCapstone = utils.isCapstone(scenario.mode)
         const isComplete = checkIfProjectComplete(scenario, projects)
-        totalMessages += projects.reduce((acc, p) => acc + p.totalChatMessages, 0)
+        totalMessages += projects.reduce((acc, p) => acc + (p.totalChatMessages || 0), 0)
         if (isComplete) {
           levelsLeft -= 1
 
