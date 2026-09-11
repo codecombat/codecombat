8c92dc6b2832bd92d6381b0a5105028b82f9135e
smallst_wsl <yuqiang_tong@163.com>
2026-09-11 10:32:25 +0800

    fix ai comments

--- app/components/common/Navigation.vue
+++ app/components/common/Navigation.vue
@@ -255,7 +255,7 @@ export default Vue.extend({
       if (value === 'beta') {
         url += '-beta'
       }
-      window.open(url, { target: '_blank' })
+      window.open(url, '_blank')
     },
 
     getNavbarData () {
@@ -536,7 +536,7 @@ export default Vue.extend({
                   li
                     a.account-dropdown-item(href="/account/settings") {{ $t('play.settings') }}
                   li(v-if="isCodeCombat && (me.isAdmin() || me.isParentHome() || me.isRegisteredHomeUser())")
-                    a.account-dropdown-item#manage-billing(href="#", role='button' @click="manageBillingABTest") {{ $t('account.manage_billing') }}
+                    a.account-dropdown-item#manage-billing(href="#", role='button' @click.prevent="manageBillingABTest") {{ $t('account.manage_billing') }}
                   li.dropdown.dropleft.dropdown-hover(v-if="true || unread")
                     a.account-dropdown-item.dropdown-toggle(href="#", data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false" @click="readAnnouncement")
                       caret.rotate-left(v-if="this.announcements.length && useDarkMode" color="white")
