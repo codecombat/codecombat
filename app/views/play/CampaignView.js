ca3aac6d4f4ad473e23a31498a303816f332de7e
smallst_wsl <yuqiang_tong@163.com>
2026-09-15 14:24:27 +0800

    library users may not have clientCreator

--- app/views/play/CampaignView.js
+++ app/views/play/CampaignView.js
@@ -2024,9 +2024,7 @@ class CampaignView extends RootView {
     const levelName = levelElement.data('level-name')
     const level = _.find(_.values(this.getLevels()), { slug: levelSlug })
 
-    const libPaidUser = me.get('clientCreator') && me.hasSubscription()
-    // library users can have subscription when anonymous
-    if (level.requiresSignUp && me.isAnonymous() && !libPaidUser) {
+    if (level.requiresSignUp && me.isAnonymous() && !me.hasSubscription()) {
       return this.promptForSignup({ accountRequiredMessage: $.i18n.t('account.unlock_next_level_with_sign_up') })
     }
 
